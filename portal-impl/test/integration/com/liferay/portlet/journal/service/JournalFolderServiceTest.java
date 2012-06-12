b850e945699f44677186829f610e24101933226c
Sergio González <sergio.gonzalez@liferay.com>
2012-06-12 10:10:38 +0200

    Search test

--- portal-impl/test/integration/com/liferay/portlet/journal/service/JournalFolderServiceTest.java
+++ portal-impl/test/integration/com/liferay/portlet/journal/service/JournalFolderServiceTest.java
@@ -15,9 +15,14 @@
 package com.liferay.portlet.journal.service;
 
 import com.liferay.portal.kernel.dao.orm.FinderCacheUtil;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Hits;
 import com.liferay.portal.kernel.transaction.Transactional;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.ServiceContext;
@@ -27,10 +32,13 @@ import com.liferay.portal.test.ExecutionTestListeners;
 import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
 import com.liferay.portal.test.TransactionalExecutionTestListener;
 import com.liferay.portal.util.TestPropsValues;
+import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
+import com.liferay.portlet.journal.DuplicateFolderNameException;
 import com.liferay.portlet.journal.model.JournalArticle;
 import com.liferay.portlet.journal.model.JournalFolder;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -63,7 +71,7 @@ public class JournalFolderServiceTest {
 
 		JournalArticle article = _addArticle(
 			group.getGroupId(), folder.getFolderId(), "Test Article",
-			"This is a test article.");
+			"This is a test article.", false);
 
 		Assert.assertEquals(article.getFolderId(), folder.getFolderId());
 
@@ -93,8 +101,85 @@ public class JournalFolderServiceTest {
 			folder11.getFolderId(), folder111.getParentFolderId());
 	}
 
+	@Test
+	@Transactional
+	public void testMoveFolder() throws Exception {
+		Group group = ServiceTestUtil.addGroup("Test Group");
+
+		JournalFolder folder1 = _addFolder(group.getGroupId(), 0, "Test 1");
+		JournalFolder folder11 = _addFolder(
+			group.getGroupId(), folder1.getFolderId(), "Test Same Name");
+		JournalFolder folder111 = _addFolder(
+			group.getGroupId(), folder11.getFolderId(), "Test 1.1.1");
+
+		JournalFolder folder2 = _addFolder(group.getGroupId(), 0, "Test 2");
+		JournalFolder folder21 = null;
+
+		try {
+			folder21 = _addFolder(
+				group.getGroupId(), folder2.getFolderId(), "Test Same Name");
+		}
+		catch (DuplicateFolderNameException dfne) {
+			Assert.fail(
+				"Unable to add two folders of the same name in different " +
+					"folders");
+		}
+
+		Assert.assertTrue(folder1.isRoot());
+		Assert.assertFalse(folder111.isRoot());
+
+		JournalFolderLocalServiceUtil.moveFolder(
+			folder111.getFolderId(), folder1.getFolderId(),
+			new ServiceContext());
+
+		Assert.assertEquals(
+			folder1.getFolderId(), folder111.getParentFolderId());
+
+		JournalFolderLocalServiceUtil.moveFolder(
+			folder111.getFolderId(), DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			new ServiceContext());
+
+		Assert.assertTrue(folder111.isRoot());
+
+		try {
+			JournalFolderLocalServiceUtil.moveFolder(
+				folder21.getFolderId(), folder1.getFolderId(),
+				new ServiceContext());
+			
+			Assert.fail("Able to add two folders of the same name");
+		}
+		catch (DuplicateFolderNameException dfne) {
+		}
+	}
+
+	@Test
+	public void testSearch() throws Exception {
+		Group group = ServiceTestUtil.addGroup("Test Group");
+
+		JournalFolder folder = _addFolder(group.getGroupId(), 0, "Test Folder");
+
+		JournalArticle article = _addArticle(
+			group.getGroupId(), folder.getFolderId(),
+			"Test Article about sports",
+			"This is a test article about football", true);
+
+		_indexedSearch(article, true, "football", true);
+		_indexedSearch(article, true, "sports", true);
+		_indexedSearch(article, true, "tennis", false);
+
+		_databaseSearch(article, true, "football", true);
+		_databaseSearch(article, true, "sports", true);
+		_databaseSearch(article, true, "tennis", false);
+
+		Assert.assertEquals(article.getFolderId(), folder.getFolderId());
+
+		JournalFolderLocalServiceUtil.deleteFolder(folder);
+		GroupLocalServiceUtil.deleteGroup(group);
+	}
+
 	protected JournalArticle _addArticle(
-			long groupId, long folderId, String name, String content)
+			long groupId, long folderId, String name, String content,
+			boolean index)
 		throws Exception {
 
 		Map<Locale, String> titleMap = new HashMap<Locale, String>();
@@ -116,30 +201,114 @@ public class JournalFolderServiceTest {
 		sb.append("</p>]]>");
 		sb.append("</static-content></root>");
 
-		return JournalArticleLocalServiceUtil.addArticle(
-			TestPropsValues.getUserId(), groupId, folderId, 0, 0,
-			StringPool.BLANK, true, 1, titleMap, descriptionMap, sb.toString(),
-			"general", null, null, null, 1, 1, 1965, 0, 0, 0, 0, 0, 0, 0, true,
-			0, 0, 0, 0, 0, true, false, false, null, null, null, null,
-			serviceContext);
+		if (index) {
+			return JournalArticleLocalServiceUtil.addArticle(
+				TestPropsValues.getUserId(), groupId, folderId, titleMap,
+				descriptionMap, sb.toString(), serviceContext);
+		}
+		else {
+			return JournalArticleLocalServiceUtil.addArticle(
+				TestPropsValues.getUserId(), groupId, folderId, 0, 0,
+				StringPool.BLANK, true, 1, titleMap, descriptionMap,
+				sb.toString(), "general", null, null, null, 1, 1, 1965, 0, 0, 0,
+				0, 0, 0, 0, true, 0, 0, 0, 0, 0, true, false, false, null, null,
+				null, null,	serviceContext);
+		}
 	}
 
 	protected JournalFolder _addFolder(
 			long groupId, long parentFolderId, String name)
 		throws Exception {
 
-		JournalFolder folder = JournalFolderLocalServiceUtil.fetchFolder(
-			groupId, name);
-
-		if (folder != null) {
-			return folder;
-		}
-
 		ServiceContext serviceContext = ServiceTestUtil.getServiceContext();
 
 		return JournalFolderLocalServiceUtil.addFolder(
 			TestPropsValues.getUserId(), groupId, parentFolderId, name,
 			"This is a test folder.", serviceContext);
+	}
+
+	protected void _databaseSearch(
+			JournalArticle article, boolean rootFolder, String keywords,
+			boolean assertTrue)
+		throws Exception {
+
+		List<JournalArticle> results = JournalArticleServiceUtil.search(
+			article.getCompanyId(), article.getGroupId(), article.getFolderId(),
+			0, keywords, null, "", "", "", null, null,
+			WorkflowConstants.STATUS_APPROVED, null, 0,
+			SearchContainer.DEFAULT_DELTA, null);
+		
+		boolean found = false;
+
+		for (JournalArticle curArticle : results) {
+			String articleId = curArticle.getArticleId();
+
+			if (articleId.equals(article.getArticleId())) {
+				found = true;
+
+				break;
+			}
+		}
+
+		String message = "Database search engine could not find ";
+
+		if (rootFolder) {
+			message += "root article by " + keywords;
+		}
+		else {
+			message += "article by " + keywords;
+		}
+
+		if (assertTrue) {
+			Assert.assertTrue(message, found);
+		}
+		else {
+			Assert.assertFalse(message, found);
+		}
+	}
+
+	protected void _indexedSearch(
+			JournalArticle article, boolean rootFolder, String keywords,
+			boolean assertTrue)
+		throws Exception {
+
+		Hits hits = JournalArticleLocalServiceUtil.search(
+			article.getCompanyId(), article.getGroupId(), article.getFolderId(),
+			0, "", "", keywords, null, 0, SearchContainer.DEFAULT_DELTA,
+			null);
+
+		List<Document> documents = hits.toList();
+
+		boolean found = false;
+
+		for (Document document : documents) {
+			String articleId = GetterUtil.getString(
+				document.get("articleId"));
+
+			if (articleId.equals(article.getArticleId())) {
+				found = true;
+
+				break;
+			}
+		}
+
+		String message = "Search engine could not find ";
+
+		if (rootFolder) {
+			message += "root article by " + keywords;
+		}
+		else {
+			message += "article by " + keywords;
+		}
+
+		message += " using query " + hits.getQuery();
+
+		if (assertTrue) {
+			Assert.assertTrue(message, found);
+		}
+		else {
+			Assert.assertFalse(message, found);
+		}
 	}
 
 }
