50f229fd6e7abc2351b729a0a4a02f48dbbef7cb
riccardo.ferrari <riccardo.ferrari@liferay.com>
2012-06-25 17:28:25 +0200

    LPS-28105 Import fails when an article's default language is not available

--- portal-impl/src/com/liferay/portlet/journal/lar/JournalPortletDataHandlerImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/lar/JournalPortletDataHandlerImpl.java
@@ -56,6 +56,7 @@ import com.liferay.portlet.documentlibrary.lar.DLPortletDataHandlerImpl;
 import com.liferay.portlet.documentlibrary.service.DLAppLocalServiceUtil;
 import com.liferay.portlet.documentlibrary.util.DLUtil;
 import com.liferay.portlet.journal.ArticleContentException;
+import com.liferay.portlet.journal.ArticleTitleException;
 import com.liferay.portlet.journal.FeedTargetLayoutFriendlyUrlException;
 import com.liferay.portlet.journal.NoSuchArticleException;
 import com.liferay.portlet.journal.NoSuchStructureException;
@@ -2611,6 +2612,14 @@ public class JournalPortletDataHandlerImpl extends BasePortletDataHandler {
 							"Skipping article with path " + path +
 								" because of invalid content");
 					}
+				} catch (ArticleTitleException ate){
+					if (_log.isWarnEnabled()) {
+						String path = articleElement.attributeValue("path");
+
+						_log.warn(
+							"Skipping article with path " + path +
+								" because of invalid article title");
+					}					
 				}
 			}
 		}
