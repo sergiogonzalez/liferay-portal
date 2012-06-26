435d9d41767fe41eaac09c6a3647d05b1f0f6cae
Kalman Vincze <kalman.vincze@liferay.com>
2012-06-26 12:10:07 +0200

    LPS-26005 Password policy expiration time and grace limit fix

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -1446,19 +1446,12 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 			}
 			else {
 				user.setDigest(StringPool.BLANK);
+				user.setPasswordReset(true);
 
 				userPersistence.update(user, false);
 
 				throw new PasswordExpiredException();
 			}
-		}
-
-		// Check if warning message should be sent
-
-		if (isPasswordExpiringSoon(user)) {
-			user.setPasswordReset(true);
-
-			userPersistence.update(user, false);
 		}
 
 		// Check if user should be forced to change password on first login
@@ -2898,6 +2891,8 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 	public boolean isPasswordExpiringSoon(User user)
 		throws PortalException, SystemException {
 
+		boolean isPasswordExpiringSoon = false;
+
 		PasswordPolicy passwordPolicy = user.getPasswordPolicy();
 
 		if (passwordPolicy.isExpireable()) {
@@ -2917,14 +2912,19 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 				passwordExpiresOn - (passwordPolicy.getWarningTime() * 1000);
 
 			if (now.getTime() > timeStartWarning) {
-				return true;
-			}
-			else {
-				return false;
+
+				if (isPasswordExpired(user)) {
+					if (!user.isPasswordReset()) {
+						isPasswordExpiringSoon = true;
+					}
+				}
+				else {
+					isPasswordExpiringSoon = true;
+				}
 			}
 		}
 
-		return false;
+		return isPasswordExpiringSoon;
 	}
 
 	/**
