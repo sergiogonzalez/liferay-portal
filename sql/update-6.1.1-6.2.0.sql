302c555e7913c713636a978b5b09eff13db01ab2
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2012-09-28 13:34:17 +0200

    LPS-30203 Add new field hidden to DLFolder and add new finder for Repository

--- sql/update-6.1.1-6.2.0.sql
+++ sql/update-6.1.1-6.2.0.sql
@@ -246,6 +246,7 @@ COMMIT_TRANSACTION;
 
 update DLFileShortcut set active_ = TRUE;
 
+alter table DLFolder add hidden BOOLEAN;
 alter table DLFolder add status INTEGER;
 alter table DLFolder add statusByUserId LONG;
 alter table DLFolder add statusByUserName VARCHAR(75) null;
@@ -253,6 +254,7 @@ alter table DLFolder add statusDate DATE null;
 
 COMMIT_TRANSACTION;
 
+update DLFolder set hidden = FALSE;
 update DLFolder set status = 0;
 update DLFolder set statusByUserId = userId;
 update DLFolder set statusByUserName = userName;
