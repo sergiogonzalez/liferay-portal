ab6f762392fd8965a24ca7fbb806a0eccafd4b3e
Sergio González <sergio.gonzalez@liferay.com>
2013-01-24 18:39:16 +0100

    LPS-14702 Source formatting

--- portal-impl/src/com/liferay/portal/tools/PortalPropertiesParser.java
+++ portal-impl/src/com/liferay/portal/tools/PortalPropertiesParser.java
@@ -14,9 +14,11 @@
 
 package com.liferay.portal.tools;
 
+import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -42,14 +44,15 @@ import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.AttributesImpl;
 
+/**
+ * @author Igor Spasic
+ * @author Sergio González
+ */
 public class PortalPropertiesParser implements XMLReader {
 
-	public static final int PARAM_DEEP_RIGHT = 1;
-	public static final int PARAM_MIN_LEFT = 2;
-
 	public static void main(String[] args) throws Exception {
-
-		PortalPropertiesParser parser = new PortalPropertiesParser();
+		PortalPropertiesParser portalPropertiesParser =
+			new PortalPropertiesParser();
 
 		String destinationHtml = "properties-doc.html";
 
@@ -60,31 +63,36 @@ public class PortalPropertiesParser implements XMLReader {
 		if (args.length >= 2) {
 			boolean fullToc = GetterUtil.getBoolean(args[1]);
 
-			parser.setGenerateFullToc(fullToc);
+			portalPropertiesParser.setGenerateFullToc(fullToc);
 		}
 
-		InputStream props =
-				ClassLoader.getSystemResourceAsStream("portal.properties");
+		InputStream portalPropertiesInputStream =
+			ClassLoader.getSystemResourceAsStream("portal.properties");
 
-		InputSource inputSource = new InputSource(props);
+		InputStream xslInputStream = ClassLoader.getSystemResourceAsStream(
+			"com/liferay/portal/tools/dependencies/properties-html.xsl");
 
-		SAXSource saxSource = new SAXSource(parser, inputSource);
+		try {
+			InputSource inputSource = new InputSource(
+				portalPropertiesInputStream);
 
-		TransformerFactory factory = TransformerFactory.newInstance();
+			SAXSource saxSource = new SAXSource(
+				portalPropertiesParser, inputSource);
 
-		InputStream xsl = ClassLoader.getSystemResourceAsStream(
-				"com/liferay/portal/tools/dependencies/properties-html.xsl");
+			StreamSource xslSource = new StreamSource(xslInputStream);
 
-		StreamSource xslSource = new StreamSource(xsl);
+			TransformerFactory transformerFactory =
+				TransformerFactory.newInstance();
 
-		Transformer transformer = factory.newTransformer(xslSource);
+			Transformer transformer = transformerFactory.newTransformer(
+				xslSource);
 
-		StreamResult result = new StreamResult(destinationHtml);
-
-		transformer.transform(saxSource, result);
-
-		xsl.close();
-		props.close();
+			transformer.transform(saxSource, new StreamResult(destinationHtml));
+		}
+		finally {
+			xslInputStream.close();
+			portalPropertiesInputStream.close();
+		}
 	}
 
 	public ContentHandler getContentHandler() {
@@ -152,13 +160,14 @@ public class PortalPropertiesParser implements XMLReader {
 	}
 
 	private void _attributeAdd(
-			AttributesImpl attributes, String key, String value) {
+		AttributesImpl attributes, String key, String value) {
+
 		attributes.addAttribute(_namespaceURI, key, key, "", value);
 	}
 
-	private void _createProperty
-			(String propertyTagName, Attributes attrs,
-			 PropertyData propertyData) throws SAXException {
+	private void _createProperty(
+			String propertyTagName, Attributes attrs, PropertyData propertyData)
+		throws SAXException {
 
 		_tagStart(propertyTagName, attrs);
 
@@ -175,10 +184,10 @@ public class PortalPropertiesParser implements XMLReader {
 	}
 
 	private void _createSection(Section section) throws SAXException {
-
 		_tagStart("section");
 
 		_tag("title", section.title);
+
 		_tag("anchor", section.anchor);
 
 		_tagStart("content");
@@ -186,30 +195,29 @@ public class PortalPropertiesParser implements XMLReader {
 		for (PropertyData propertyData : section.propertyDataList) {
 			AttributesImpl attrs = new AttributesImpl();
 
-			_attributeAdd(attrs, "hidden",
-					Boolean.toString(propertyData.hidden));
+			_attributeAdd(attrs, "hidden", String.valueOf(propertyData.hidden));
 
-			_attributeAdd(attrs, "group",
-					Integer.toString(propertyData.group));
+			_attributeAdd(attrs, "group", String.valueOf(propertyData.group));
 
 			if (propertyData.groupStart) {
 				_attributeAdd(attrs, "prefix", propertyData.prefix);
-			} else {
-				_attributeAdd(attrs, "prefix", "");
+			}
+			else {
+				_attributeAdd(attrs, "prefix", StringPool.BLANK);
 			}
 
 			String propertyTagName = "property";
 
-			if (!propertyData.groupStart && propertyData.group > 0) {
-				propertyTagName += "-group";
+			if (!propertyData.groupStart && (propertyData.group > 0)) {
+				propertyTagName = propertyTagName.concat("-group");
 			}
 
 			_createProperty(propertyTagName, attrs, propertyData);
 
-			// duplicate property to property-group if group is started
-			if (propertyData.groupStart && propertyData.group > 0) {
+			// Duplicate property to property-group if group is started
 
-				propertyTagName += "-group";
+			if (propertyData.groupStart && (propertyData.group > 0)) {
+				propertyTagName = propertyTagName.concat("-group");
 
 				_createProperty(propertyTagName, attrs, propertyData);
 			}
@@ -221,85 +229,96 @@ public class PortalPropertiesParser implements XMLReader {
 	}
 
 	private String _makePrefix(String name, int minLeft, int deepRight) {
+		int pos = 0;
 
-		int ndx = 0;
 		while (minLeft > 0) {
-			ndx = name.indexOf('.', ndx);
-			if (ndx == -1) {
+			pos = name.indexOf(CharPool.PERIOD, pos);
+
+			if (pos == -1) {
 				break;
 			}
 
-			ndx++;
+			pos++;
+
 			minLeft--;
 		}
 
-		if (ndx == -1) {
-			return "";
+		if (pos == -1) {
+			return StringPool.BLANK;
 		}
 
 		String prefix;
-		if (ndx > 0) {
-			ndx--;
 
-			prefix = name.substring(0, ndx);
-			name = name.substring(ndx);
-		} else {
-			prefix = "";
+		if (pos > 0) {
+			pos--;
+
+			prefix = name.substring(0, pos);
+
+			name = name.substring(pos);
+		}
+		else {
+			prefix = StringPool.BLANK;
 		}
 
 		while (deepRight > 0) {
-			ndx = name.lastIndexOf('.');
-			if (ndx == -1) {
+			pos = name.lastIndexOf(CharPool.PERIOD);
+
+			if (pos == -1) {
 				return prefix;
 			}
-			name = name.substring(0, ndx);
+
+			name = name.substring(0, pos);
+
 			deepRight--;
 		}
+
 		return prefix + name;
 	}
 
-	private void _parse(String p) throws SAXException {
-
+	private void _parse(String content) throws SAXException {
 		_contentHandler.startDocument();
 
 		_tagStart("params");
+
 		_tag("fullToc", String.valueOf(_generateFullToc));
+
 		_tagEnd("params");
 
 		_tagStart("properties");
 
-		int ndx = 0;
-		while (true) {
-			ndx = p.indexOf("## ", ndx);
+		int pos = 0;
 
-			if (ndx == -1) {
+		while (true) {
+			pos = content.indexOf(_DOUBLE_HASH_SPACE, pos);
+
+			if (pos == -1) {
 				break;
 			}
 
 			Section section = new Section();
 
-			int titleEndNdx = p.indexOf('\n', ndx);
+			int titleX = pos + _DOUBLE_HASH_SPACE.length();
+			int titleY = content.indexOf(CharPool.NEW_LINE, pos);
 
-			section.title = p.substring(ndx + 3, titleEndNdx).trim();
+			section.title = content.substring(titleX, titleY).trim();
 			section.anchor = nameToAnchor(section.title);
 
-			titleEndNdx += 5;
+			int sectionX = titleY + 5;
+			int sectionY = content.indexOf(_DOUBLE_POUND, sectionX);
 
-			int sectionEndNdx = p.indexOf("##", titleEndNdx);
-
-			if (sectionEndNdx == -1) {
-				sectionEndNdx = p.length();
+			if (sectionY == -1) {
+				sectionY = content.length();
 			}
 
-			String content = p.substring(titleEndNdx, sectionEndNdx);
+			String sectionContent = content.substring(titleY, sectionY);
 
-			section.propertyDataList = _parseContent(content);
+			section.propertyDataList = _parseSectionContent(sectionContent);
 
 			_resolveGroups(section.propertyDataList);
 
 			_createSection(section);
 
-			ndx = sectionEndNdx;
+			pos = sectionY;
 		}
 
 		_tagEnd("properties");
@@ -307,11 +326,10 @@ public class PortalPropertiesParser implements XMLReader {
 		_contentHandler.endDocument();
 	}
 
-	private List<PropertyData> _parseContent(String content) {
-
+	private List<PropertyData> _parseSectionContent(String content) {
 		List<PropertyData> propertyDataList = new ArrayList<PropertyData>();
 
-		String[] lines = StringUtil.split(content, "\n");
+		String[] lines = StringUtil.split(content, CharPool.NEW_LINE);
 
 		PropertyData propertyData = new PropertyData();
 
@@ -323,75 +341,79 @@ public class PortalPropertiesParser implements XMLReader {
 
 			hidden = false;
 
-			if ((line.length() >= 1) && (line.charAt(0) == '#')) {
-				// comments
+			if ((line.length() >= 1) && (line.charAt(0) == CharPool.POUND)) {
+
+				// Comments
 
 				if ((line.length() >= 2) && (line.charAt(1) != ' ')) {
 					line = line.substring(1);
+
 					hidden = true;
 				}
 				else {
-					propertyData.description += line.substring(1) + '\n';
+					propertyData.description = propertyData.description.concat(
+						line.substring(1)).concat(StringPool.NEW_LINE);
+
 					continue;
 				}
 			}
 
-			// values
-			int equalNdx = line.indexOf('=');
+			// Values
 
-			if (equalNdx == -1) {
+			int pos = line.indexOf(CharPool.EQUAL);
+
+			if (pos == -1) {
 				continue;
 			}
 
-			propertyData.name = line.substring(0, equalNdx);
+			propertyData.name = line.substring(0, pos);
 			propertyData.anchor = nameToAnchor(propertyData.name + '+' + i);
 			propertyData.hidden = hidden;
-			propertyData.prefix =_makePrefix(
-					propertyData.name, PARAM_MIN_LEFT, PARAM_DEEP_RIGHT);
+			propertyData.prefix = _makePrefix(
+				propertyData.name, _PARAM_MIN_LEFT, _PARAM_DEEP_RIGHT);
 
-			String value = line.substring(equalNdx + 1);
+			String value = line.substring(pos + 1);
+
 			while (value.endsWith("\\")) {
 				value = value.substring(0, value.length() - 1);
+
 				i++;
+
 				value += lines[i].trim();
 			}
 
 			propertyData.value = value;
 
-			// check previous property for alternative values
-			if (propertyData.description.length() == 0) {
+			// Check previous property for alternative values
 
-				if (propertyDataList.isEmpty() == false) {
+			if (Validator.isNull(propertyData.description) &&
+				!propertyDataList.isEmpty()) {
 
-					PropertyData previousData =
-							propertyDataList.get(propertyDataList.size() - 1);
+				PropertyData previousData = propertyDataList.get(
+					propertyDataList.size() - 1);
 
-					if (previousData.name.equals(propertyData.name)) {
-
-						if (previousData.value.equals(propertyData.value)) {
-							propertyData = new PropertyData();
-							continue;
-						}
-
-						previousData.alternativeValues.add(propertyData.value);
+				if (previousData.name.equals(propertyData.name)) {
+					if (previousData.value.equals(propertyData.value)) {
 						propertyData = new PropertyData();
 						continue;
 					}
+
+					previousData.alternativeValues.add(propertyData.value);
+
+					propertyData = new PropertyData();
+
+					continue;
 				}
 			}
 
 			propertyDataList.add(propertyData);
 			propertyData = new PropertyData();
-
 		}
 
 		return propertyDataList;
-
 	}
 
-	private void _resolveGroups(
-			List<PropertyData> propertyDataList) {
-
+	private void _resolveGroups(List<PropertyData> propertyDataList) {
 		int groupCount = 0;
 		boolean newGroup = true;
 
@@ -399,15 +421,13 @@ public class PortalPropertiesParser implements XMLReader {
 			PropertyData prev = propertyDataList.get(i - 1);
 			PropertyData curr = propertyDataList.get(i);
 
-			if (
-					prev.hidden == curr.hidden &&
-					prev.alternativeValues.isEmpty() &&
-					curr.alternativeValues.isEmpty() &&
-					prev.prefix.equals(curr.prefix) &&
-					(curr.description.length() == 0)
-					) {
+			if ((prev.hidden == curr.hidden) &&
+				prev.alternativeValues.isEmpty() &&
+				curr.alternativeValues.isEmpty() &&
+				prev.prefix.equals(curr.prefix) &&
+				Validator.isNull(curr.description)) {
 
-				// group founded
+				// Group founded
 
 				if (newGroup == true) {
 					groupCount++;
@@ -415,13 +435,13 @@ public class PortalPropertiesParser implements XMLReader {
 					prev.groupStart = true;
 					newGroup = false;
 				}
+
 				curr.group = prev.group;
-			} else {
+			}
+			else {
 				newGroup = true;
 			}
-
 		}
-
 	}
 
 	private void _tag(String tagName, String text) throws SAXException {
@@ -431,7 +451,7 @@ public class PortalPropertiesParser implements XMLReader {
 	}
 
 	private void _tag(String tagName, String text, String key, String value)
-			throws SAXException {
+		throws SAXException {
 
 		AttributesImpl attributes = new AttributesImpl();
 
@@ -443,49 +463,56 @@ public class PortalPropertiesParser implements XMLReader {
 	}
 
 	private void _tagEnd(String tagName) throws SAXException {
-
 		_contentHandler.endElement(_namespaceURI, tagName, tagName);
-
 	}
 
 	private void _tagStart(String tagName) throws SAXException {
-
 		_contentHandler.startElement(
-				_namespaceURI, tagName, tagName, _attributes);
-
+			_namespaceURI, tagName, tagName, _attributes);
 	}
 
 	private void _tagStart(String tagName, Attributes attributes)
-			throws SAXException {
+		throws SAXException {
 
 		_contentHandler.startElement(
-				_namespaceURI, tagName, tagName, attributes);
-
+			_namespaceURI, tagName, tagName, attributes);
 	}
 
 	private void _tagText(String text) throws SAXException {
-
 		_contentHandler.characters(text.toCharArray(), 0, text.length());
 	}
 
 	private String nameToAnchor(String name) {
-		return name.replace(' ', '+');
+		return name.replace(CharPool.SPACE, CharPool.PLUS);
 	}
+
+	private static final String _DOUBLE_HASH_SPACE = "## ";
+
+	private static final String _DOUBLE_POUND = "##";
+
+	private static final int _PARAM_DEEP_RIGHT = 1;
+
+	private static final int _PARAM_MIN_LEFT = 2;
+
+	private AttributesImpl _attributes = new AttributesImpl();
+	private ContentHandler _contentHandler;
+	private boolean _generateFullToc = false;
+	private String _namespaceURI = StringPool.BLANK;
 
 	private static class PropertyData {
 		public List<String> alternativeValues = new ArrayList<String>();
-		public String anchor = "";
-		public String description = "";
+		public String anchor = StringPool.BLANK;
+		public String description = StringPool.BLANK;
 		public int group;
 		public boolean groupStart;
 		public boolean hidden;
-		public String name = "";
-		public String prefix = "";
-		public String value = "";
+		public String name = StringPool.BLANK;
+		public String prefix = StringPool.BLANK;
+		public String value = StringPool.BLANK;
+
 	}
 
 	private static class Section {
-
 		public String anchor;
 
 		public List<PropertyData> propertyDataList;
@@ -493,10 +520,5 @@ public class PortalPropertiesParser implements XMLReader {
 		public String title;
 
 	}
-
-	private AttributesImpl _attributes = new AttributesImpl();
-	private ContentHandler _contentHandler;
-	private boolean _generateFullToc = false;
-	private String _namespaceURI = "";
 
 }
