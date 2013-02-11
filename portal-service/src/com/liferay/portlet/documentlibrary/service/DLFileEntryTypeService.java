de5386d94cd8ca1b5adc4a827fe9aeec9fc0b542
Tibor Lipusz <tibor.lipusz@liferay.com>
2013-02-11 15:16:17 +0100

    LPS-32718 Autogenerated

--- portal-service/src/com/liferay/portlet/documentlibrary/service/DLFileEntryTypeService.java
+++ portal-service/src/com/liferay/portlet/documentlibrary/service/DLFileEntryTypeService.java
@@ -88,6 +88,12 @@ public interface DLFileEntryTypeService extends BaseService {
 		throws com.liferay.portal.kernel.exception.SystemException;
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntryType> getFolderFileEntryTypes(
+		long[] groupIds, long folderId, boolean inherited)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntryType> search(
 		long companyId, long[] groupIds, java.lang.String keywords,
 		boolean includeBasicFileEntryType, int start, int end,
