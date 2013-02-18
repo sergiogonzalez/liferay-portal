cf4255539fd8fe478dae4e15167b19e100cc1005
Sampsa Sohlman <sampsa.sohlman@liferay.com>
2013-02-18 09:39:24 +0200

    LPS-27229 - Fix for LAR file import

--- portal-impl/src/com/liferay/portlet/documentlibrary/util/VideoProcessorImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/util/VideoProcessorImpl.java
@@ -286,11 +286,12 @@ public class VideoProcessorImpl
 			return;
 		}
 
-		for (String previewType : _PREVIEW_TYPES) {
+		for (int i=0; i<_PREVIEW_TYPES.length; i++) {
+			String previewType = _PREVIEW_TYPES[i];
 			if (previewType.equals("mp4") || previewType.equals("ogv")) {
 				importPreview(
 					portletDataContext, fileEntry, importedFileEntry,
-					fileEntryElement, "video", previewType);
+					fileEntryElement, "video", previewType, i);
 			}
 		}
 	}
