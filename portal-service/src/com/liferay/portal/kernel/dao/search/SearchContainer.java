6af991d3752181b7de1dcf6bb79d92ad868fac0a
Roberto Díaz <roberto.diaz@liferay.com>
2013-03-20 10:47:44 +0100

    LPS-32658 Modify SearchContainer:setTotal(). Now it calculates the cur returning the last page that has results.

--- portal-service/src/com/liferay/portal/kernel/dao/search/SearchContainer.java
+++ portal-service/src/com/liferay/portal/kernel/dao/search/SearchContainer.java
@@ -429,10 +429,7 @@ public class SearchContainer<R> {
 	public void setTotal(int total) {
 		_total = total;
 
-		if (((_cur - 1) * _delta) >= _total) {
-			_cur = DEFAULT_CUR;
-		}
-
+		_calculateCur();
 		_calculateStartAndEnd();
 	}
 
@@ -446,6 +443,12 @@ public class SearchContainer<R> {
 		for (String headerName : headerNames) {
 			_normalizedHeaderNames.add(
 				FriendlyURLNormalizerUtil.normalize(headerName));
+		}
+	}
+
+	private void _calculateCur() {
+		if (((_cur - 1) * _delta) >= _total) {
+			_cur = (_total /_delta) + 1;
 		}
 	}
 
