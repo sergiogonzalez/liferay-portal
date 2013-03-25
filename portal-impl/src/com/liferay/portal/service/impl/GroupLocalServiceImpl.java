85cc37d23b8ba55690aa35c365637058f36f7a1f
Manuel de la Peña <manuel.delapenya@liferay.com>
2013-03-25 13:20:58 +0100

    LPS-33928 Validate that a group cannot be its own parent

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -17,6 +17,7 @@ package com.liferay.portal.service.impl;
 import com.liferay.portal.DuplicateGroupException;
 import com.liferay.portal.GroupFriendlyURLException;
 import com.liferay.portal.GroupNameException;
+import com.liferay.portal.GroupParentGroupIdException;
 import com.liferay.portal.NoSuchGroupException;
 import com.liferay.portal.NoSuchLayoutSetException;
 import com.liferay.portal.NoSuchUserException;
@@ -294,6 +295,8 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 
 		validateFriendlyURL(
 			user.getCompanyId(), groupId, classNameId, classPK, friendlyURL);
+
+		validateParentGroup(groupId, parentGroupId);
 
 		Group group = groupPersistence.create(groupId);
 
@@ -2999,6 +3002,8 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 			group.getCompanyId(), group.getGroupId(), group.getClassNameId(),
 			group.getClassPK(), friendlyURL);
 
+		validateParentGroup(group.getGroupId(), parentGroupId);
+
 		group.setParentGroupId(parentGroupId);
 		group.setTreePath(group.buildTreePath());
 		group.setName(name);
@@ -3648,6 +3653,15 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 			if (name.equals(company.getName())) {
 				throw new DuplicateGroupException();
 			}
+		}
+	}
+
+	protected void validateParentGroup(long groupId, long parentGroupId)
+		throws PortalException, SystemException {
+
+		if (groupId == parentGroupId) {
+			throw new GroupParentGroupIdException(
+				GroupParentGroupIdException.SELF_DESCENDANT);
 		}
 	}
 
