04b862562ea6c759256264a1f0db5282b5a5c760
Roberto Díaz <roberto.diaz@liferay.com>
2013-05-29 14:03:19 +0200

    LPS-35679 Upgrade process to Mb & WIKI email body. Now it includes the signature

--- portal-impl/src/com/liferay/portal/upgrade/v6_2_0/UpgradeWiki.java
+++ portal-impl/src/com/liferay/portal/upgrade/v6_2_0/UpgradeWiki.java
@@ -16,6 +16,7 @@ package com.liferay.portal.upgrade.v6_2_0;
 
 import com.liferay.portal.kernel.upgrade.BaseUpgradePortletPreferences;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portlet.PortletPreferencesFactoryUtil;
@@ -55,7 +56,95 @@ public class UpgradeWiki extends BaseUpgradePortletPreferences {
 			"mailPageUpdatedSubject", "mailPageUpdatedSubjectPrefix",
 			portletPreferences);
 
+		// Email Subscription Body
+
+		portletPreferences = upgradeSubscriptionBody(
+			"mailPageAddedBody", "mailPageAddedSignature", portletPreferences);
+
+		portletPreferences = upgradeSubscriptionBody(
+			"mailPageUpdatedBody", "mailPageUpdatedSignature",
+			portletPreferences);
+
 		return PortletPreferencesFactoryUtil.toXML(portletPreferences);
+	}
+
+	protected PortletPreferences upgradeSubscriptionBody(
+			String valueKey, String oldValueKey,
+			PortletPreferences portletPreferences)
+		throws Exception {
+
+		String bodyValue = GetterUtil.getString(
+			portletPreferences.getValue(valueKey, StringPool.BLANK));
+
+		String signatureValue = GetterUtil.getString(
+			portletPreferences.getValue(oldValueKey, StringPool.BLANK));
+
+		if (Validator.isNull(signatureValue) && Validator.isNull(bodyValue)) {
+			return portletPreferences;
+		}
+
+		if (Validator.isNull(signatureValue)) {
+			StringBundler sb = new StringBundler(8);
+
+			sb.append(
+				"<div style=\"background: #eee; border-top: 1px solid #ccc; " +
+					"color: #999; margin: 5px; padding: 5px; " +
+					"text-align: center;\">\n");
+
+			if (valueKey.startsWith("mailPageAdded")) {
+				sb.append(
+					"\t<a href=\"[$PAGE_URL$]\">View Page ([$PAGE_TITLE$])\n");
+			}
+			else {
+				sb.append(
+					"\t<a href=\"[$PAGE_URL$]\">View Page " +
+						"([$PAGE_TITLE$])</a> | <a href=\"[$DIFFS_URL$]\">" +
+						"View Changes</a>\n");
+			}
+
+			sb.append("\n");
+			sb.append("\t<p>\n");
+			sb.append("\t\t[$COMPANY_NAME$] Wiki<br />\n");
+			sb.append("\t\t[$PORTAL_URL$]\n");
+			sb.append("\t</p>\n");
+			sb.append("</div>");
+
+			signatureValue = sb.toString();
+		}
+		else if (Validator.isNull(bodyValue)) {
+			StringBundler sb = new StringBundler(6);
+
+			sb.append(
+				"<div style=\"background: #dff4ff; " +
+					"border: 1px solid #a7cedf; color: #34404f; " +
+					"margin: 5px; padding: 5px;\">\n");
+			sb.append(
+				"\tBy <strong>[$PAGE_USER_NAME$]</strong> "+
+					"([$PAGE_DATE_UPDATE$])<br />\n");
+
+			if (valueKey.startsWith("mailPageAdded")) {
+				sb.append("\tPage Title: [$PAGE_TITLE$]\n");
+				sb.append("</div>\n");
+				sb.append("\n");
+				sb.append("[$PAGE_CONTENT$]");
+			}
+			else {
+				sb.append("\tSummary: [$PAGE_SUMMARY$]\n");
+				sb.append("</div>\n");
+				sb.append("\n");
+				sb.append("[$PAGE_DIFFS$]");
+			}
+
+			bodyValue = sb.toString();
+		}
+
+		bodyValue = bodyValue.concat("\n--\n" + signatureValue);
+
+		portletPreferences.setValue(valueKey, bodyValue);
+
+		portletPreferences.reset(oldValueKey);
+
+		return portletPreferences;
 	}
 
 	protected PortletPreferences upgradeSubscriptionSubject(
