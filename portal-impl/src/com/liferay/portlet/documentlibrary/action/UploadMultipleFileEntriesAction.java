246d8b987f9e46b76fa73efee438158921036cd8
Roberto Díaz <roberto.diaz@liferay.com>
2013-11-22 14:56:26 +0100

    LPS-41008 Copy-paste processAction and render methods from EditFileEntryAction to UploadMultipleFileEntriesAction-- no logic changes

--- portal-impl/src/com/liferay/portlet/documentlibrary/action/UploadMultipleFileEntriesAction.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/action/UploadMultipleFileEntriesAction.java
@@ -14,7 +14,26 @@
 
 package com.liferay.portlet.documentlibrary.action;
 
+import com.liferay.portal.NoSuchRepositoryEntryException;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.upload.UploadException;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.HttpUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.security.auth.PrincipalException;
+import com.liferay.portal.struts.ActionConstants;
 import com.liferay.portal.struts.PortletAction;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portal.util.WebKeys;
+import com.liferay.portlet.documentlibrary.FileSizeException;
+import com.liferay.portlet.documentlibrary.NoSuchFileEntryException;
+import com.liferay.portlet.documentlibrary.NoSuchFileVersionException;
+import com.liferay.portlet.documentlibrary.model.DLFileEntry;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -24,6 +43,7 @@ import javax.portlet.ActionResponse;
 import javax.portlet.PortletConfig;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
+import javax.portlet.WindowState;
 
 /**
  * @author Roberto Díaz
@@ -37,6 +57,133 @@ public class UploadMultipleFileEntriesAction extends PortletAction {
 			ActionResponse actionResponse)
 		throws Exception {
 
+		String cmd = ParamUtil.getString(actionRequest, Constants.CMD);
+
+		FileEntry fileEntry = null;
+
+		try {
+			if (Validator.isNull(cmd)) {
+				UploadException uploadException =
+					(UploadException)actionRequest.getAttribute(
+						WebKeys.UPLOAD_EXCEPTION);
+
+				if (uploadException != null) {
+					if (uploadException.isExceededSizeLimit()) {
+						throw new FileSizeException(uploadException.getCause());
+					}
+
+					throw new PortalException(uploadException.getCause());
+				}
+			}
+			else if (cmd.equals(Constants.ADD) ||
+				cmd.equals(Constants.ADD_DYNAMIC) ||
+				cmd.equals(Constants.UPDATE) ||
+				cmd.equals(Constants.UPDATE_AND_CHECKIN)) {
+
+				fileEntry = updateFileEntry(
+					portletConfig, actionRequest, actionResponse);
+			}
+			else if (cmd.equals(Constants.ADD_MULTIPLE)) {
+				addMultipleFileEntries(
+					portletConfig, actionRequest, actionResponse);
+			}
+			else if (cmd.equals(Constants.ADD_TEMP)) {
+				addTempFileEntry(actionRequest, actionResponse);
+			}
+			else if (cmd.equals(Constants.DELETE)) {
+				deleteFileEntry(actionRequest, false);
+			}
+			else if (cmd.equals(Constants.DELETE_TEMP)) {
+				deleteTempFileEntry(actionRequest, actionResponse);
+			}
+			else if (cmd.equals(Constants.CANCEL_CHECKOUT)) {
+				cancelFileEntriesCheckOut(actionRequest);
+			}
+			else if (cmd.equals(Constants.CHECKIN)) {
+				checkInFileEntries(actionRequest);
+			}
+			else if (cmd.equals(Constants.CHECKOUT)) {
+				checkOutFileEntries(actionRequest);
+			}
+			else if (cmd.equals(Constants.MOVE)) {
+				moveFileEntries(actionRequest, false);
+			}
+			else if (cmd.equals(Constants.MOVE_FROM_TRASH)) {
+				moveFileEntries(actionRequest, true);
+			}
+			else if (cmd.equals(Constants.MOVE_TO_TRASH)) {
+				deleteFileEntry(actionRequest, true);
+			}
+			else if (cmd.equals(Constants.REVERT)) {
+				revertFileEntry(actionRequest);
+			}
+
+			WindowState windowState = actionRequest.getWindowState();
+
+			if (cmd.equals(Constants.ADD_TEMP) ||
+				cmd.equals(Constants.DELETE_TEMP)) {
+
+				setForward(actionRequest, ActionConstants.COMMON_NULL);
+			}
+			else if (cmd.equals(Constants.PREVIEW)) {
+			}
+			else if (!cmd.equals(Constants.MOVE_FROM_TRASH) &&
+				!windowState.equals(LiferayWindowState.POP_UP)) {
+
+				sendRedirect(actionRequest, actionResponse);
+			}
+			else {
+				String redirect = ParamUtil.getString(
+					actionRequest, "redirect");
+				int workflowAction = ParamUtil.getInteger(
+					actionRequest, "workflowAction",
+					WorkflowConstants.ACTION_SAVE_DRAFT);
+
+				if ((fileEntry != null) &&
+					(workflowAction == WorkflowConstants.ACTION_SAVE_DRAFT)) {
+
+					redirect = getSaveAndContinueRedirect(
+						portletConfig, actionRequest, fileEntry, redirect);
+
+					sendRedirect(actionRequest, actionResponse, redirect);
+				}
+				else {
+					if (!windowState.equals(LiferayWindowState.POP_UP)) {
+						sendRedirect(actionRequest, actionResponse);
+					}
+					else {
+						redirect = PortalUtil.escapeRedirect(
+							ParamUtil.getString(actionRequest, "redirect"));
+
+						if (Validator.isNotNull(redirect)) {
+							if (cmd.equals(Constants.ADD) &&
+								(fileEntry != null)) {
+
+								String portletId = HttpUtil.getParameter(
+									redirect, "p_p_id", false);
+
+								String namespace =
+									PortalUtil.getPortletNamespace(portletId);
+
+								redirect = HttpUtil.addParameter(
+									redirect, namespace + "className",
+									DLFileEntry.class.getName());
+								redirect = HttpUtil.addParameter(
+									redirect, namespace + "classPK",
+									fileEntry.getFileEntryId());
+							}
+
+							actionResponse.sendRedirect(redirect);
+						}
+					}
+				}
+			}
+		}
+		catch (Exception e) {
+			handleUploadException(
+				portletConfig, actionRequest, actionResponse, cmd, e);
+		}
+
 	}
 
 	@Override
@@ -46,7 +193,28 @@ public class UploadMultipleFileEntriesAction extends PortletAction {
 			RenderResponse renderResponse)
 		throws Exception {
 
-		return actionMapping.findForward("portlet.document_library.error");
+		try {
+			ActionUtil.getFileEntry(renderRequest);
+		}
+		catch (Exception e) {
+			if (e instanceof NoSuchFileEntryException ||
+				e instanceof NoSuchFileVersionException ||
+				e instanceof NoSuchRepositoryEntryException ||
+				e instanceof PrincipalException) {
+
+				SessionErrors.add(renderRequest, e.getClass());
+
+				return actionMapping.findForward(
+					"portlet.document_library.error");
+			}
+			else {
+				throw e;
+			}
+		}
+
+		String forward = "portlet.document_library.edit_file_entry";
+
+		return actionMapping.findForward(getForward(renderRequest, forward));
 	}
 
 }
