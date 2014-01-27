sergiogonzalez/liferay-portal

commit e69e227002d707a64c270197af07c1e9f8d89899

LPS-36693 Move null check into get*URL methods

portal-impl/src/com/liferay/portlet/wiki/service/impl/WikiPageLocalServiceImpl.java
@@ -1974,7 +1974,11 @@ public class WikiPageLocalServiceImpl extends WikiPageLocalServiceBaseImpl {
 		if (previousVersionPage == null) {
 			return StringPool.BLANK;
 		}
-		
+
+		if (Validator.isNotNull(serviceContext.getLayoutFullURL())) {
+			return StringPool.BLANK;
+		}
+
 		HttpServletRequest request = serviceContext.getRequest();
 		
 		if (request == null) {
@@ -2015,6 +2019,10 @@ public class WikiPageLocalServiceImpl extends WikiPageLocalServiceBaseImpl {
 	protected String getPageURL(
 			WikiNode node, WikiPage page, ServiceContext serviceContext)
 		throws PortalException, SystemException {
+
+		if (Validator.isNotNull(serviceContext.getLayoutFullURL())) {
+			return StringPool.BLANK;
+		}
 
 		HttpServletRequest request = serviceContext.getRequest();
 		
@@ -2161,14 +2169,9 @@ public class WikiPageLocalServiceImpl extends WikiPageLocalServiceBaseImpl {
 			pageContent = WikiUtil.processContent(pageContent);
 		}
 
-		String pageURL = StringPool.BLANK;
-		String diffsURL = StringPool.BLANK;
-
-		if (Validator.isNotNull(serviceContext.getLayoutFullURL())) {
-			pageURL = getPageURL(node, page, serviceContext);
-			diffsURL = getDiffsURL(
-				node, page, previousVersionPage, serviceContext);
-		}
+		String pageURL = getPageURL(node, page, serviceContext);
+		String diffsURL = getDiffsURL(
+			node, page, previousVersionPage, serviceContext);
 
 		String fromName = WikiUtil.getEmailFromName(
 			preferences, page.getCompanyId());

commit 026fb7b88bdb3b7c6402d0009afdc31201a1ec8a

LPS-43724 Fix wrong SF in wiki introduced in e69e227002d707a64c270197af07c1e9f8d89899

portal-impl/src/com/liferay/portlet/wiki/service/impl/WikiPageLocalServiceImpl.java
@@ -2244,10 +2244,6 @@ public class WikiPageLocalServiceImpl extends WikiPageLocalServiceBaseImpl {
 			return StringPool.BLANK;
 		}
 
-		if (Validator.isNotNull(serviceContext.getLayoutFullURL())) {
-			return StringPool.BLANK;
-		}
-
 		HttpServletRequest request = serviceContext.getRequest();
 
 		if (request == null) {
@@ -2288,10 +2284,6 @@ public class WikiPageLocalServiceImpl extends WikiPageLocalServiceBaseImpl {
 	protected String getPageURL(
 			WikiNode node, WikiPage page, ServiceContext serviceContext)
 		throws PortalException, SystemException {
-
-		if (Validator.isNotNull(serviceContext.getLayoutFullURL())) {
-			return StringPool.BLANK;
-		}
 
 		HttpServletRequest request = serviceContext.getRequest();
 
@@ -2403,6 +2395,12 @@ public class WikiPageLocalServiceImpl extends WikiPageLocalServiceBaseImpl {
 			WikiNode node, WikiPage page, ServiceContext serviceContext,
 			boolean update)
 		throws PortalException, SystemException {
+
+		String layoutFullURL = serviceContext.getLayoutFullURL();
+
+		if (!page.isApproved() || Validator.isNull(layoutFullURL)) {
+			return;
+		}
 
 		PortletPreferences preferences = null;
 