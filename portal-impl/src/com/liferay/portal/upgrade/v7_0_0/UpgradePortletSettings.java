1db5ec92630319ecf3c6dc5f7ce43ef992e17d9c
Iván Zaera Avellón <ivan.zaera@liferay.com>
2014-03-11 15:28:55 +0100

    LPS-44944 Upgrade process to migrate wiki preferences to settings

--- portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradePortletSettings.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradePortletSettings.java
@@ -19,6 +19,7 @@ import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.util.PortletKeys;
 import com.liferay.portlet.messageboards.util.MBConstants;
 import com.liferay.portlet.shopping.util.ShoppingConstants;
+import com.liferay.portlet.wiki.util.WikiConstants;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,6 +32,7 @@ public class UpgradePortletSettings extends UpgradeProcess {
 	public UpgradePortletSettings() {
 		_serviceNames.put(PortletKeys.MESSAGE_BOARDS, MBConstants.SERVICE_NAME);
 		_serviceNames.put(PortletKeys.SHOPPING, ShoppingConstants.SERVICE_NAME);
+		_serviceNames.put(PortletKeys.WIKI, WikiConstants.SERVICE_NAME);
 	}
 
 	@Override
