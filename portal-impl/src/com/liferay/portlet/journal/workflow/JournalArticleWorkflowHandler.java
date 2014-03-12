5d152b77e936390cd419dde99c4c4c1839723f7b
Roberto Díaz <roberto.diaz@liferay.com>
2014-03-12 14:04:38 +0100

    LPS-45000 Modify Workfow handlers

--- portal-impl/src/com/liferay/portlet/journal/workflow/JournalArticleWorkflowHandler.java
+++ portal-impl/src/com/liferay/portlet/journal/workflow/JournalArticleWorkflowHandler.java
@@ -22,8 +22,6 @@ import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.security.permission.ResourceActionsUtil;
 import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.theme.ThemeDisplay;
-import com.liferay.portal.util.PortalUtil;
-import com.liferay.portal.util.PortletKeys;
 import com.liferay.portlet.journal.model.JournalArticle;
 import com.liferay.portlet.journal.service.JournalArticleLocalServiceUtil;
 
@@ -67,12 +65,8 @@ public class JournalArticleWorkflowHandler extends BaseWorkflowHandler {
 		ServiceContext serviceContext = (ServiceContext)workflowContext.get(
 			"serviceContext");
 
-		String articleURL = PortalUtil.getControlPanelFullURL(
-			serviceContext.getScopeGroupId(), PortletKeys.JOURNAL, null);
-
 		return JournalArticleLocalServiceUtil.updateStatus(
-			userId, article, status, articleURL, workflowContext,
-			serviceContext);
+			userId, article, status, workflowContext, serviceContext);
 	}
 
 	@Override
