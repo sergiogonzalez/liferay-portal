c5a9fcad24b383edc3764d6446c8b4ef5a19484c
Roberto Díaz <roberto.diaz@liferay.com>
2014-03-13 17:35:23 +0100

    LPS-45000 Autogenerated

--- portal-service/src/com/liferay/portlet/blogs/service/BlogsEntryLocalService.java
+++ portal-service/src/com/liferay/portlet/blogs/service/BlogsEntryLocalService.java
@@ -691,6 +691,18 @@ public interface BlogsEntryLocalService extends BaseLocalService,
 
 	public com.liferay.portlet.blogs.model.BlogsEntry updateStatus(
 		long userId, long entryId, int status,
+		java.util.Map<java.lang.String, java.io.Serializable> workflowContext,
+		com.liferay.portal.service.ServiceContext serviceContext)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException;
+
+	/**
+	* @deprecated As of 7.0.0, replaced by {@link #updateStatus(long, long,
+	int, Map, ServiceContext)} )}
+	*/
+	@Deprecated
+	public com.liferay.portlet.blogs.model.BlogsEntry updateStatus(
+		long userId, long entryId, int status,
 		com.liferay.portal.service.ServiceContext serviceContext)
 		throws com.liferay.portal.kernel.exception.PortalException,
 			com.liferay.portal.kernel.exception.SystemException;
--- portal-service/src/com/liferay/portlet/blogs/service/BlogsEntryLocalServiceWrapper.java
+++ portal-service/src/com/liferay/portlet/blogs/service/BlogsEntryLocalServiceWrapper.java
@@ -896,6 +896,22 @@ public class BlogsEntryLocalServiceWrapper implements BlogsEntryLocalService,
 	@Override
 	public com.liferay.portlet.blogs.model.BlogsEntry updateStatus(
 		long userId, long entryId, int status,
+		java.util.Map<java.lang.String, java.io.Serializable> workflowContext,
+		com.liferay.portal.service.ServiceContext serviceContext)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return _blogsEntryLocalService.updateStatus(userId, entryId, status,
+			workflowContext, serviceContext);
+	}
+
+	/**
+	* @deprecated As of 7.0.0, replaced by {@link #updateStatus(long, long,
+	int, Map, ServiceContext)} )}
+	*/
+	@Deprecated
+	@Override
+	public com.liferay.portlet.blogs.model.BlogsEntry updateStatus(
+		long userId, long entryId, int status,
 		com.liferay.portal.service.ServiceContext serviceContext)
 		throws com.liferay.portal.kernel.exception.PortalException,
 			com.liferay.portal.kernel.exception.SystemException {
