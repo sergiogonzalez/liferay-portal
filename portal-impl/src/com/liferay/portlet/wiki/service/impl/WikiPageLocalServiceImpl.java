fa1b6fd00be9077115d8af05523a5c5790f987b3
Roberto Díaz <roberto.diaz@liferay.com>
2014-03-14 17:45:18 +0100

    LPS-45000 Auto SF

--- portal-impl/src/com/liferay/portlet/wiki/service/impl/WikiPageLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/wiki/service/impl/WikiPageLocalServiceImpl.java
@@ -95,7 +95,6 @@ import java.io.Serializable;
 
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
