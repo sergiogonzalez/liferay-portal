1c7f3b14bb64a6bf8ba47235fda575e3101e88c9
Roberto Díaz <roberto.diaz@liferay.com>
2014-04-09 10:18:28 +0200

    LPS-45000 SF - Change notation to keep consistency

--- portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -479,7 +479,7 @@ public class JournalArticleLocalServiceImpl
 		else {
 			updateStatus(
 				userId, article, WorkflowConstants.STATUS_APPROVED, null,
-				new HashMap<String, Serializable>(), serviceContext);
+				serviceContext, new HashMap<String, Serializable>());
 		}
 
 		return journalArticlePersistence.findByPrimaryKey(article.getId());
@@ -3307,8 +3307,8 @@ public class JournalArticleLocalServiceImpl
 			}
 
 			updateStatus(
-				userId, article, status, null,
-				new HashMap<String, Serializable>(), serviceContext);
+				userId, article, status, null, serviceContext,
+				new HashMap<String, Serializable>());
 
 			// Trash
 
@@ -3590,8 +3590,8 @@ public class JournalArticleLocalServiceImpl
 		serviceContext.setScopeGroupId(article.getGroupId());
 
 		updateStatus(
-			userId, article, trashEntry.getStatus(), null,
-			new HashMap<String, Serializable>(), serviceContext);
+			userId, article, trashEntry.getStatus(), null, serviceContext,
+			new HashMap<String, Serializable>());
 
 		// Trash
 
@@ -4851,7 +4851,7 @@ public class JournalArticleLocalServiceImpl
 		if (expired && imported) {
 			updateStatus(
 				userId, article, article.getStatus(), articleURL,
-				new HashMap<String, Serializable>(), serviceContext);
+				serviceContext, new HashMap<String, Serializable>());
 		}
 
 		if (serviceContext.getWorkflowAction() ==
@@ -5193,8 +5193,8 @@ public class JournalArticleLocalServiceImpl
 	@Override
 	public JournalArticle updateStatus(
 			long userId, JournalArticle article, int status, String articleURL,
-			Map<String, Serializable> workflowContext,
-			ServiceContext serviceContext)
+			ServiceContext serviceContext,
+			Map<String, Serializable> workflowContext)
 		throws PortalException, SystemException {
 
 		// Article
@@ -5443,7 +5443,7 @@ public class JournalArticleLocalServiceImpl
 		JournalArticle article = getArticle(classPK);
 
 		return journalArticleLocalService.updateStatus(
-			userId, article, status, null, workflowContext, serviceContext);
+			userId, article, status, null, serviceContext, workflowContext);
 	}
 
 	/**
@@ -5480,8 +5480,8 @@ public class JournalArticleLocalServiceImpl
 			groupId, articleId, version);
 
 		return journalArticleLocalService.updateStatus(
-			userId, article, status, articleURL, workflowContext,
-			serviceContext);
+			userId, article, status, articleURL, serviceContext,
+			workflowContext);
 	}
 
 	/**
@@ -5649,7 +5649,7 @@ public class JournalArticleLocalServiceImpl
 
 			journalArticleLocalService.updateStatus(
 				article.getUserId(), article, WorkflowConstants.STATUS_APPROVED,
-				null, new HashMap<String, Serializable>(), serviceContext);
+				null, serviceContext, new HashMap<String, Serializable>());
 		}
 	}
 
