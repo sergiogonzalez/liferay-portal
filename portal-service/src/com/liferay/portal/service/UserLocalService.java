37973c7cf57ef447fd4bc650d8547489a94a1a69
adolfo.perez <adolfo.perez@liferay.com>
2014-04-23 16:32:03 +0200

    LPS-46184 autogenerated files

--- portal-service/src/com/liferay/portal/service/UserLocalService.java
+++ portal-service/src/com/liferay/portal/service/UserLocalService.java
@@ -1762,6 +1762,20 @@ public interface UserLocalService extends BaseLocalService,
 		throws com.liferay.portal.kernel.exception.PortalException,
 			com.liferay.portal.kernel.exception.SystemException;
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portal.model.User> getSocialUsers(
+		long userId1, long userId2, long[] groupIds, int[] types, int start,
+		int end, com.liferay.portal.kernel.util.OrderByComparator obc)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portal.model.User> getSocialUsers(
+		long userId, long[] groupIds, int[] types, java.lang.String keywords,
+		int start, int end, com.liferay.portal.kernel.util.OrderByComparator obc)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException;
+
 	/**
 	* Returns the number of users with a social relation with the user.
 	*
@@ -1824,6 +1838,17 @@ public interface UserLocalService extends BaseLocalService,
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public int getSocialUsersCount(long userId1, long userId2, int type)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public int getSocialUsersCount(long userId1, long userId2, long[] groupIds,
+		int[] types)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public int getSocialUsersCount(long userId, long[] groupIds, int[] types)
 		throws com.liferay.portal.kernel.exception.PortalException,
 			com.liferay.portal.kernel.exception.SystemException;
 
