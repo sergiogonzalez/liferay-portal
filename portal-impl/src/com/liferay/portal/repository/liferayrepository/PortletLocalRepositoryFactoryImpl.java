35c12eec87c8530371093251d77f02174bccc4eb
adolfo.perez <adolfo.perez@liferay.com>
2014-06-03 12:28:40 +0200

    LPS-47330 implement `PortletLocalRepositoryFactoryImpl` in terms of `BaseRepositoryFactory`

--- portal-impl/src/com/liferay/portal/repository/liferayrepository/PortletLocalRepositoryFactoryImpl.java
+++ portal-impl/src/com/liferay/portal/repository/liferayrepository/PortletLocalRepositoryFactoryImpl.java
@@ -18,27 +18,99 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.repository.LocalRepository;
 import com.liferay.portal.kernel.repository.LocalRepositoryFactory;
+import com.liferay.portal.model.Repository;
 import com.liferay.portal.repository.BaseRepositoryFactory;
+import com.liferay.portal.service.RepositoryLocalService;
+import com.liferay.portlet.documentlibrary.model.DLFileEntry;
+import com.liferay.portlet.documentlibrary.model.DLFileVersion;
+import com.liferay.portlet.documentlibrary.model.DLFolder;
+import com.liferay.portlet.documentlibrary.service.DLFileEntryLocalService;
+import com.liferay.portlet.documentlibrary.service.DLFileVersionLocalService;
+import com.liferay.portlet.documentlibrary.service.DLFolderLocalService;
 
 /**
  * @author Adolfo Pérez
  */
-public class PortletLocalRepositoryFactoryImpl extends BaseRepositoryFactory
+public class PortletLocalRepositoryFactoryImpl
+	extends BaseRepositoryFactory<LocalRepository>
 	implements LocalRepositoryFactory {
 
 	@Override
-	public LocalRepository create(long repositoryId)
+	protected LocalRepository createExternalRepository(
+			long repositoryId, long classNameId)
 		throws PortalException, SystemException {
 
-		throw new UnsupportedOperationException("Not implemented");
+		throw new UnsupportedOperationException(
+			"This factory cannot be used to create external repositories");
 	}
 
 	@Override
-	public LocalRepository create(
+	protected LocalRepository createExternalRepository(
 			long folderId, long fileEntryId, long fileVersionId)
 		throws PortalException, SystemException {
 
-		throw new UnsupportedOperationException("Not implemented");
+		throw new UnsupportedOperationException(
+			"This factory cannot be used to create external repositories");
+	}
+
+	@Override
+	protected LocalRepository createLiferayRepositoryInstance(
+		long groupId, long repositoryId, long dlFolderId) {
+
+		return new PortletLocalRepository(
+			getRepositoryLocalService(), getRepositoryService(),
+			getDlAppHelperLocalService(), getDlFileEntryLocalService(),
+			getDlFileEntryService(), getDlFileEntryTypeLocalService(),
+			getDlFileVersionLocalService(), getDlFileVersionService(),
+			getDlFolderLocalService(), getDlFolderService(),
+			getResourceLocalService(), groupId, repositoryId, dlFolderId);
+	}
+
+	@Override
+	protected long getFileEntryRepositoryId(long fileEntryId)
+		throws PortalException, SystemException {
+
+		DLFileEntryLocalService dlFileEntryLocalService =
+			getDlFileEntryLocalService();
+
+		DLFileEntry dlFileEntry = dlFileEntryLocalService.getFileEntry(
+			fileEntryId);
+
+		return dlFileEntry.getRepositoryId();
+	}
+
+	@Override
+	protected long getFileVersionRepositoryId(long fileVersionId)
+		throws PortalException, SystemException {
+
+		DLFileVersionLocalService dlFileVersionLocalService =
+			getDlFileVersionLocalService();
+
+		DLFileVersion dlFileVersion = dlFileVersionLocalService.getFileVersion(
+			fileVersionId);
+
+		return dlFileVersion.getRepositoryId();
+	}
+
+	@Override
+	protected long getFolderRepositoryId(long folderId)
+		throws PortalException, SystemException {
+
+		DLFolderLocalService dlFolderLocalService = getDlFolderLocalService();
+
+		DLFolder dlFolder = dlFolderLocalService.getFolder(folderId);
+
+		return dlFolder.getRepositoryId();
+	}
+
+	@Override
+	protected Repository getRepository(long repositoryId)
+		throws SystemException {
+
+		RepositoryLocalService repositoryLocalService =
+			getRepositoryLocalService();
+
+		return repositoryLocalService.fetchRepository(repositoryId);
 	}
 
 }
