ca2b7668e960d9ebeacffe135b89b40e3a970ab0
Bryan Engler <bryan.engler@liferay.com>
2014-08-01 09:31:38 -0700

    LPS-48902 Make sure thumbnail is enabled before import/export

--- portal-service/src/com/liferay/portlet/documentlibrary/util/DLPreviewableProcessor.java
+++ portal-service/src/com/liferay/portlet/documentlibrary/util/DLPreviewableProcessor.java
@@ -598,6 +598,10 @@ public abstract class DLPreviewableProcessor implements DLProcessor {
 			Element fileEntryElement, String binPathName, int index)
 		throws PortalException {
 
+		if (!isThumbnailEnabled(index)) {
+			return;
+		}
+
 		FileVersion fileVersion = fileEntry.getFileVersion();
 
 		if (!hasThumbnail(fileVersion, index)) {
@@ -1130,6 +1134,10 @@ public abstract class DLPreviewableProcessor implements DLProcessor {
 			FileEntry importedFileEntry, Element fileEntryElement,
 			String binPathName, int index)
 		throws Exception {
+
+		if (!isThumbnailEnabled(index)) {
+			return;
+		}
 
 		if (!portletDataContext.isPerformDirectBinaryImport()) {
 			importThumbnailFromLAR(
