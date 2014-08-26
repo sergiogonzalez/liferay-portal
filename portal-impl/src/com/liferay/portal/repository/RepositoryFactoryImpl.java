80176e92d22059ad8fc85a0517d7450bf78f401a
adolfo.perez <adolfo.perez@liferay.com>
2014-08-26 15:52:42 +0200

    LPS-48728 copy capability collections before modifying
    Collections returned by definitions may be shared.

--- portal-impl/src/com/liferay/portal/repository/RepositoryFactoryImpl.java
+++ portal-impl/src/com/liferay/portal/repository/RepositoryFactoryImpl.java
@@ -40,6 +40,8 @@ import com.liferay.portlet.documentlibrary.service.DLFileVersionService;
 import com.liferay.portlet.documentlibrary.service.DLFolderLocalService;
 import com.liferay.portlet.documentlibrary.service.DLFolderService;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -93,8 +95,14 @@ public class RepositoryFactoryImpl implements RepositoryFactory {
 			repository);
 
 		if (cmisRepositoryHandler != null) {
+			externalSupportedCapabilities = new HashMap<>(
+				externalSupportedCapabilities);
+
 			externalSupportedCapabilities.put(
 				CMISRepositoryHandler.class, cmisRepositoryHandler);
+
+			externalExportedCapabilityClasses = new HashSet<>(
+				externalExportedCapabilityClasses);
 
 			externalExportedCapabilityClasses.add(CMISRepositoryHandler.class);
 		}
