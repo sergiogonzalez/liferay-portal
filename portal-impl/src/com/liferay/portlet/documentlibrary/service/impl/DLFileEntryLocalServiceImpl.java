274d05888e796e5cbebe0710b9c4b675c4386c7f
Michael Young <michael.young@liferay.com>
2014-08-28 10:48:40 -0700

    LPS-49619 upgrade

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
@@ -174,7 +174,7 @@ public class DLFileEntryLocalServiceImpl
 		String name = String.valueOf(
 			counterLocalService.increment(DLFileEntry.class.getName()));
 		String extension = DLAppUtil.getExtension(title, sourceFileName);
-		String filename = getFilename(title, extension);
+		String filename = DLUtil.getFilename(title, extension);
 
 		if (fileEntryTypeId == -1) {
 			fileEntryTypeId =
@@ -2021,19 +2021,6 @@ public class DLFileEntryLocalServiceImpl
 		}
 	}
 
-	protected String getFilename(String title, String extension) {
-		String filename = StringUtil.replace(
-			title, StringPool.SLASH, StringPool.UNDERLINE);
-
-		if (Validator.isNotNull(extension) &&
-			!filename.endsWith(StringPool.PERIOD + extension)) {
-
-			filename += StringPool.PERIOD + extension;
-		}
-
-		return filename;
-	}
-
 	protected String getNextVersion(
 			DLFileEntry dlFileEntry, boolean majorVersion, int workflowAction)
 		throws PortalException {
@@ -2371,7 +2358,7 @@ public class DLFileEntryLocalServiceImpl
 				}
 			}
 
-			String filename = getFilename(title, extension);
+			String filename = DLUtil.getFilename(title, extension);
 
 			Date now = new Date();
 
