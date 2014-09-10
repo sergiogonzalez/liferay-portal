17f0de7d328f015bd117fa7fa86a4902aa47ab88
adolfo.perez <adolfo.perez@liferay.com>
2014-09-10 17:38:05 +0200

    LPS-48895 remove duplication

--- portal-impl/src/com/liferay/portal/repository/capabilities/LiferayBulkOperationCapability.java
+++ portal-impl/src/com/liferay/portal/repository/capabilities/LiferayBulkOperationCapability.java
@@ -16,13 +16,15 @@ package com.liferay.portal.repository.capabilities;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.repository.capabilities.BulkOperationCapability;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.repository.model.RepositoryModel;
 import com.liferay.portal.kernel.repository.model.RepositoryModelOperation;
-import com.liferay.portal.repository.liferayrepository.model.LiferayFileEntry;
-import com.liferay.portal.repository.liferayrepository.model.LiferayFolder;
 import com.liferay.portlet.documentlibrary.model.DLFileEntry;
 import com.liferay.portlet.documentlibrary.model.DLFolder;
 import com.liferay.portlet.documentlibrary.service.DLFileEntryLocalServiceUtil;
 import com.liferay.portlet.documentlibrary.service.DLFolderLocalServiceUtil;
+import com.liferay.portlet.documentlibrary.util.RepositoryModelUtil;
 
 import java.util.List;
 
@@ -40,16 +42,16 @@ public class LiferayBulkOperationCapability implements BulkOperationCapability {
 	public void execute(RepositoryModelOperation repositoryModelOperation)
 		throws PortalException {
 
-		executeFileEntryBulkOperation(repositoryModelOperation);
-		executeFolderBulkOperation(repositoryModelOperation);
+		execute(new FileEntryRepositoryModelSource(), repositoryModelOperation);
+		execute(new FolderRepositoryModelSource(), repositoryModelOperation);
 	}
 
-	protected void executeFileEntryBulkOperation(
+	protected <T extends RepositoryModel<T>> void execute(
+			RepositoryModelSource<T> repositoryModelSource,
 			RepositoryModelOperation repositoryModelOperation)
 		throws PortalException {
 
-		int count = DLFileEntryLocalServiceUtil.getRepositoryFileEntriesCount(
-			_groupId, _repositoryId);
+		int count = repositoryModelSource.getCount();
 
 		int pages = count / _DEFAULT_PAGE_SIZE;
 
@@ -61,44 +63,11 @@ public class LiferayBulkOperationCapability implements BulkOperationCapability {
 		int end = _DEFAULT_PAGE_SIZE;
 
 		while (pages > 0) {
-			List<DLFileEntry> dlFileEntries =
-				DLFileEntryLocalServiceUtil.getRepositoryFileEntries(
-					_groupId, _repositoryId, start, end);
+			List<T> repositoryModels = repositoryModelSource.getPage(
+				start, end);
 
-			for (DLFileEntry fileEntry : dlFileEntries) {
-				repositoryModelOperation.execute(
-					new LiferayFileEntry(fileEntry));
-			}
-
-			start = end;
-			end += _DEFAULT_PAGE_SIZE;
-			pages--;
-		}
-	}
-
-	protected void executeFolderBulkOperation(
-			RepositoryModelOperation repositoryModelOperation)
-		throws PortalException {
-
-		int count = DLFolderLocalServiceUtil.getRepositoryFoldersCount(
-			_groupId, _repositoryId);
-
-		int pages = count / _DEFAULT_PAGE_SIZE;
-
-		if ((count % _DEFAULT_PAGE_SIZE) > 0) {
-			pages++;
-		}
-
-		int start = 0;
-		int end = _DEFAULT_PAGE_SIZE;
-
-		while (pages > 0) {
-			List<DLFolder> dlFolders =
-				DLFolderLocalServiceUtil.getRepositoryFolders(
-					_groupId, _repositoryId, start, end);
-
-			for (DLFolder folder : dlFolders) {
-				repositoryModelOperation.execute(new LiferayFolder(folder));
+			for (T repositoryModel : repositoryModels) {
+				repositoryModel.execute(repositoryModelOperation);
 			}
 
 			start = end;
@@ -111,5 +80,53 @@ public class LiferayBulkOperationCapability implements BulkOperationCapability {
 
 	private long _groupId;
 	private long _repositoryId;
+
+	private class FileEntryRepositoryModelSource
+		implements RepositoryModelSource<FileEntry> {
+
+		@Override
+		public int getCount() {
+			return DLFileEntryLocalServiceUtil.getRepositoryFileEntriesCount(
+				_groupId, _repositoryId);
+		}
+
+		@Override
+		public List<FileEntry> getPage(int start, int end) {
+			List<DLFileEntry> dlFileEntries =
+				DLFileEntryLocalServiceUtil.getRepositoryFileEntries(
+					_groupId, _repositoryId, start, end);
+
+			return RepositoryModelUtil.toFileEntries(dlFileEntries);
+		}
+
+	}
+
+	private class FolderRepositoryModelSource
+		implements RepositoryModelSource<Folder> {
+
+		@Override
+		public int getCount() {
+			return DLFolderLocalServiceUtil.getRepositoryFoldersCount(
+				_groupId, _repositoryId);
+		}
+
+		@Override
+		public List<Folder> getPage(int start, int end) {
+			List<DLFolder> dlFolders =
+				DLFolderLocalServiceUtil.getRepositoryFolders(
+					_groupId, _repositoryId, start, end);
+
+			return RepositoryModelUtil.toFolders(dlFolders);
+		}
+
+	}
+
+	private interface RepositoryModelSource<T extends RepositoryModel> {
+
+		public int getCount();
+
+		public List<T> getPage(int start, int end);
+
+	}
 
 }
