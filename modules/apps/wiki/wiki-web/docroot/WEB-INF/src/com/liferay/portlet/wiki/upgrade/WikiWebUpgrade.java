36df37ce73702889323eb5bf698665378bd7b757
Iván Zaera Avellón <ivan.zaera@liferay.com>
2014-10-10 15:22:31 +0200

    LPS-50604 Move Wiki upgrade process to OSGi module

---
+++ modules/apps/wiki/wiki-web/docroot/WEB-INF/src/com/liferay/portlet/wiki/upgrade/WikiWebUpgrade.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.wiki.upgrade;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.service.ReleaseLocalService;
+import com.liferay.portal.upgrade.util.UpgradePortletId;
+
+import java.util.Collections;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Iván Zaera
+ */
+@Component(
+	immediate = true, service = WikiWebUpgrade.class
+)
+public class WikiWebUpgrade {
+
+	@Reference(unbind = "-")
+	protected void setReleaseLocalService(
+		ReleaseLocalService releaseLocalService) {
+
+		_releaseLocalService = releaseLocalService;
+	}
+
+	@Activate
+	protected void upgrade() throws PortalException {
+		UpgradePortletId upgradePortletId = new UpgradePortletId() {
+
+			@Override
+			protected String[][] getRenamePortletIdsArray() {
+				return new String[][] {
+					new String[] {"36", "36_WAR_pollsweb"},
+					new String[] {"154", "154_WAR_pollsweb"},
+					new String[] {"54", "54_WAR_pollsweb"}
+				};
+			}
+
+		};
+
+		_releaseLocalService.updateRelease(
+			"com.liferay.wiki.web",
+			Collections.<UpgradeProcess>singletonList(upgradePortletId), 1, 0,
+			false);
+	}
+
+	private ReleaseLocalService _releaseLocalService;
+
+}
