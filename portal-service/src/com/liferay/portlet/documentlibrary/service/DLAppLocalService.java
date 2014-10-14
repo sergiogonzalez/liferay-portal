6dc4cc063a36aa0927a52b6ddd1c3e61c4f7a0b5
Roberto Díaz <roberto.diaz@liferay.com>
2014-10-14 18:17:41 +0200

    LPS-48961 Autogenerated

--- portal-service/src/com/liferay/portlet/documentlibrary/service/DLAppLocalService.java
+++ portal-service/src/com/liferay/portlet/documentlibrary/service/DLAppLocalService.java
@@ -445,8 +445,8 @@ public interface DLAppLocalService extends BaseLocalService {
 
 	/**
 	* @deprecated As of 7.0.0, replaced by {@link
-	com.liferay.portal.kernel.repository.util.RepositoryTrashUtil.moveFileEntryFromTrash(
-	long, long, long, long, ServiceContext)}
+	RepositoryTrashUtil#moveFileEntryFromTrash(long, long, long,
+	long, ServiceContext)}
 	*/
 	@java.lang.Deprecated
 	public com.liferay.portal.kernel.repository.model.FileEntry moveFileEntryFromTrash(
@@ -456,8 +456,7 @@ public interface DLAppLocalService extends BaseLocalService {
 
 	/**
 	* @deprecated As of 7.0.0, replaced by {@link
-	com.liferay.portal.kernel.repository.util.RepositoryTrashUtil.moveFileEntryToTrash(
-	long, long, long)}
+	RepositoryTrashUtil#moveFileEntryToTrash(long, long, long)}
 	*/
 	@java.lang.Deprecated
 	public com.liferay.portal.kernel.repository.model.FileEntry moveFileEntryToTrash(
@@ -471,8 +470,8 @@ public interface DLAppLocalService extends BaseLocalService {
 
 	/**
 	* @deprecated As of 7.0.0, replaced by {@link
-	com.liferay.portal.kernel.repository.util.RepositoryTrashUtil.restoreFileEntryFromTrash(
-	long, long, long)}
+	RepositoryTrashUtil#restoreFileEntryFromTrash(long, long,
+	long)}
 	*/
 	@java.lang.Deprecated
 	public void restoreFileEntryFromTrash(long userId, long fileEntryId)
@@ -743,13 +742,13 @@ public interface DLAppLocalService extends BaseLocalService {
 	the file entry type to default all Liferay file entries to </li>
 	<li> dlFileEntryTypesSearchContainerPrimaryKeys - a
 	comma-delimited list of file entry type primary keys allowed in
-	the given folder and all descendants </li> <li>
-	overrideFileEntryTypes - boolean specifying whether to override
-	ancestral folder's restriction of file entry types allowed </li>
+	the given folder and all descendants </li> <li> restrictionType
+	- specifying restriction type of file entry types allowed </li>
 	<li> workflowDefinitionXYZ - the workflow definition name
 	specified per file entry type. The parameter name must be the
-	string <code>workflowDefinition</code> appended by the <code>
-	fileEntryTypeId</code> (optionally <code>0</code>). </li> </ul>
+	string <code>workflowDefinition</code> appended by the
+	<code>fileEntryTypeId</code> (optionally <code>0</code>).</li>
+	</ul>
 	* @return the folder
 	* @throws PortalException if the current or new parent folder could not be
 	found, or if the new parent folder's information was invalid
