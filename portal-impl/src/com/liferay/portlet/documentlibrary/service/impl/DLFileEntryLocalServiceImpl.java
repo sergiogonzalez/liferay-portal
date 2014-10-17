f3649ed89754ad5838c270ecc26b6554171b60d1
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2014-10-17 09:57:42 +0200

    LPS-48992 SF

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
@@ -663,7 +663,7 @@ public class DLFileEntryLocalServiceImpl
 
 	@Override
 	public void deleteFileEntries(
-			final long groupId, final long folderId,
+			long groupId, final long folderId,
 			final boolean includeTrashedEntries)
 		throws PortalException {
 
@@ -675,17 +675,14 @@ public class DLFileEntryLocalServiceImpl
 
 				@Override
 				public void addCriteria(DynamicQuery dynamicQuery) {
-					Property groupIdProperty = PropertyFactoryUtil.forName(
-						"groupId");
+					Property folderIdproperty = PropertyFactoryUtil.forName(
+						"folderId");
 
-					dynamicQuery.add(groupIdProperty.eq(groupId));
-
-					Property property = PropertyFactoryUtil.forName("folderId");
-
-					dynamicQuery.add(property.eq(folderId));
+					dynamicQuery.add(folderIdproperty.eq(folderId));
 				}
 
 			});
+		actionableDynamicQuery.setGroupId(groupId);
 		actionableDynamicQuery.setPerformActionMethod(
 			new ActionableDynamicQuery.PerformActionMethod() {
 
