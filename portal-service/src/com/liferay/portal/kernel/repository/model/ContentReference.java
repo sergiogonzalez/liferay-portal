5d97811cd4050e87b1f8c897f848f918b4e6b9c9
adolfo.perez <adolfo.perez@liferay.com>
2014-11-26 17:00:01 +0100

    LPS-51846 add extension to content ref

--- portal-service/src/com/liferay/portal/kernel/repository/model/ContentReference.java
+++ portal-service/src/com/liferay/portal/kernel/repository/model/ContentReference.java
@@ -24,23 +24,27 @@ import java.io.InputStream;
 public class ContentReference {
 
 	public static final ContentReference fromBytes(
-		String sourceFileName, String mimeType, byte[] bytes) {
+		String sourceFileName, String extension, String mimeType,
+		byte[] bytes) {
 
 		return fromInputStream(
-			sourceFileName, mimeType, new ByteArrayInputStream(bytes),
-			bytes.length);
+			sourceFileName, extension, mimeType,
+			new ByteArrayInputStream(bytes), bytes.length);
 	}
 
 	public static final ContentReference fromFile(
-		String sourceFileName, String mimeType, File file) {
+		String sourceFileName, String extension, String mimeType, File file) {
 
-		return new ContentReference(sourceFileName, mimeType, file, null, 0);
+		return new ContentReference(
+			sourceFileName, extension, mimeType, file, null, 0);
 	}
 
 	public static final ContentReference fromInputStream(
-		String sourceFileName, String mimeType, InputStream is, long size) {
+		String sourceFileName, String extension, String mimeType,
+		InputStream is, long size) {
 
-		return new ContentReference(sourceFileName, mimeType, null, is, size);
+		return new ContentReference(
+			sourceFileName, extension, mimeType, null, is, size);
 	}
 
 	public String getMimeType() {
@@ -64,16 +68,18 @@ public class ContentReference {
 	}
 
 	protected ContentReference(
-		String sourceFileName, String mimeType, File file, InputStream is,
-		long size) {
+		String sourceFileName, String extension, String mimeType, File file,
+		InputStream is, long size) {
 
 		_sourceFileName = sourceFileName;
+		_extension = extension;
 		_mimeType = mimeType;
 		_file = file;
 		_inputStream = is;
 		_size = size;
 	}
 
+	private final String _extension;
 	private final File _file;
 	private final InputStream _inputStream;
 	private final String _mimeType;
