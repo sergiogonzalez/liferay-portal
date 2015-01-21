bca471c18c8c53205f419601cab1c8399911354e
Jonathan McCann <jonathan.mccann@liferay.com>
2015-01-21 11:02:27 -0800

    LPS-48992 Source Formatting

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
@@ -921,8 +921,7 @@ public class DLFileEntryLocalServiceImpl
 			final boolean includeTrashedEntries)
 		throws PortalException {
 
-		int total = dlFileEntryPersistence.countByR_F(
-			repositoryId, folderId);
+		int total = dlFileEntryPersistence.countByR_F(repositoryId, folderId);
 
 		final IntervalAction intervalAction = new IntervalAction(total);
 
