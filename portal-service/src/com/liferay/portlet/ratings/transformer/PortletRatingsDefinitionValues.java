8790c810b9023fe15146320570821eac3350347e
Roberto Díaz <roberto.diaz@liferay.com>
2015-01-29 13:15:54 +0100

    LPS-52630 Simplify framework

--- portal-service/src/com/liferay/portlet/ratings/transformer/PortletRatingsDefinitionValues.java
+++ portal-service/src/com/liferay/portlet/ratings/transformer/PortletRatingsDefinitionValues.java
@@ -16,7 +16,31 @@ package com.liferay.portlet.ratings.transformer;
 
 /**
  * @author Roberto Díaz
- * @author Sergio González
  */
-public interface PortletRatingsDefinition {
+public class PortletRatingsDefinitionValues {
+
+	public PortletRatingsDefinitionValues(
+		String[] classNames, RatingsType defaultRatingsType, String portletId) {
+
+		_classNames = classNames;
+		_defaultRatingsType = defaultRatingsType;
+		_portletId = portletId;
+	}
+
+	public String[] getClassNames() {
+		return _classNames;
+	}
+
+	public RatingsType getDefaultRatingsType() {
+		return _defaultRatingsType;
+	}
+
+	public String getPortletId() {
+		return _portletId;
+	}
+
+	private final String[] _classNames;
+	private final RatingsType _defaultRatingsType;
+	private final String _portletId;
+
 }
