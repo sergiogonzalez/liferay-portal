ea271c3691fca1798c94c83d94a658494da2d783
Iván Zaera Avellón <ivan.zaera@liferay.com>
2015-03-24 16:25:49 +0100

    LPS-54497 Convert wiki settings to the new format

--- modules/apps/wiki/wiki-api/src/com/liferay/wiki/settings/WikiPortletInstanceSettings.java
+++ modules/apps/wiki/wiki-api/src/com/liferay/wiki/settings/WikiPortletInstanceSettings.java
@@ -14,107 +14,105 @@
 
 package com.liferay.wiki.settings;
 
-import com.liferay.portal.kernel.settings.ModifiableSettings;
 import com.liferay.portal.kernel.settings.PortletInstanceSettings;
-import com.liferay.portal.kernel.settings.Settings;
-import com.liferay.portal.kernel.settings.TypedSettings;
-import com.liferay.portal.util.PortalUtil;
-import com.liferay.wiki.constants.WikiPortletKeys;
-
-import java.io.IOException;
-
-import javax.portlet.ValidatorException;
+import com.liferay.wiki.configuration.WikiPortletInstanceConfiguration;
 
 /**
  * @author Iván Zaera
  */
-@Settings.Config(
-	settingsIds = {
-		WikiPortletKeys.WIKI, WikiPortletKeys.WIKI_ADMIN,
-		WikiPortletKeys.WIKI_DISPLAY
-	}
-)
-public class WikiPortletInstanceSettings implements PortletInstanceSettings {
+//@Settings.Config(
+//	settingsIds = {
+//		WikiPortletKeys.WIKI, WikiPortletKeys.WIKI_ADMIN,
+//		WikiPortletKeys.WIKI_DISPLAY
+//	}
+//)
+public interface WikiPortletInstanceSettings
+	extends PortletInstanceSettings, WikiPortletInstanceConfiguration,
+		WikiPortletInstanceSettingsExtra {
 
-	public WikiPortletInstanceSettings(Settings settings) {
-		_typedSettings = new TypedSettings(settings);
-	}
+//	public WikiPortletInstanceSettings(Settings settings) {
+//		_typedSettings = new TypedSettings(settings);
+//	}
 
-	public String getDisplayStyle() {
-		return _typedSettings.getValue("displayStyle");
-	}
+//	public String getDisplayStyle() {
+//		return _typedSettings.getValue("displayStyle");
+//	}
 
-	public long getDisplayStyleGroupId(long defaultDisplayStyleGroupId) {
-		return _typedSettings.getLongValue(
-			"displayStyleGroupId", defaultDisplayStyleGroupId);
-	}
+//	public long getDisplayStyleGroupId(long defaultDisplayStyleGroupId) {
+//		return _typedSettings.getLongValue(
+//			"displayStyleGroupId", defaultDisplayStyleGroupId);
+//	}
 
-	public String[] getHiddenNodes() {
-		return _typedSettings.getValues("hiddenNodes");
-	}
+//	public String[] getHiddenNodes() {
+//		return _typedSettings.getValues("hiddenNodes");
+//	}
 
-	@Settings.Property(name = "rssDelta")
-	public int getRssDelta() {
-		return _typedSettings.getIntegerValue("rssDelta");
-	}
+//	@Settings.Property(name = "rssDelta")
+//	public int getRssDelta() {
+//		return _typedSettings.getIntegerValue("rssDelta");
+//	}
 
-	@Settings.Property(name = "rssDisplayStyle")
-	public String getRssDisplayStyle() {
-		return _typedSettings.getValue("rssDisplayStyle");
-	}
+//	@Settings.Property(name = "rssDisplayStyle")
+//	public String getRssDisplayStyle() {
+//		return _typedSettings.getValue("rssDisplayStyle");
+//	}
 
-	@Settings.Property(name = "rssFeedType")
-	public String getRssFeedType() {
-		return _typedSettings.getValue("rssFeedType");
-	}
+//	@Settings.Property(name = "rssFeedType")
+//	public String getRssFeedType() {
+//		return _typedSettings.getValue("rssFeedType");
+//	}
 
-	public String[] getVisibleNodes() {
-		return _typedSettings.getValues("visibleNodes");
-	}
+//	public String[] getVisibleNodes() {
+//		return _typedSettings.getValues("visibleNodes");
+//	}
 
-	public boolean isEnableCommentRatings() {
-		return _typedSettings.getBooleanValue("enableCommentRatings");
-	}
+//	public boolean isEnableCommentRatings() {
+//		return _typedSettings.getBooleanValue("enableCommentRatings");
+//	}
 
-	@Settings.Property(name = "enableComments")
-	public boolean isEnablePageComments() {
-		return _typedSettings.getBooleanValue("enableComments");
-	}
+//	@Settings.Property(name = "enableComments")
+//	public boolean isEnablePageComments() {
+//		return _typedSettings.getBooleanValue("enableComments");
+//	}
 
-	public boolean isEnablePageRatings() {
-		return _typedSettings.getBooleanValue("enablePageRatings");
-	}
+//	public boolean isEnablePageRatings() {
+//		return _typedSettings.getBooleanValue("enablePageRatings");
+//	}
 
-	public boolean isEnableRelatedAssets() {
-		return _typedSettings.getBooleanValue("enableRelatedAssets");
-	}
+//	public boolean isEnableRelatedAssets() {
+//		return _typedSettings.getBooleanValue("enableRelatedAssets");
+//	}
 
-	@Settings.Property(name = "enableRss")
-	public boolean isEnableRSS() {
-		if (!PortalUtil.isRSSFeedsEnabled()) {
-			return false;
-		}
-
-		return _typedSettings.getBooleanValue("enableRss");
-	}
-
-	public void setHiddenNodes(String[] hiddenNodes) {
-		_typedSettings.setValues("hiddenNodes", hiddenNodes);
-	}
-
-	public void setVisibleNodes(String[] visibleNodes) {
-		_typedSettings.setValues("visibleNodes", visibleNodes);
-	}
-
-	public void store() throws IOException, ValidatorException {
-		Settings settings = _typedSettings.getWrappedSettings();
-
-		ModifiableSettings modifiableSettings =
-			settings.getModifiableSettings();
-
-		modifiableSettings.store();
-	}
-
-	private final TypedSettings _typedSettings;
+//	/* extra */
+//	@Settings.Property(name = "enableRss")
+//	public boolean isEnableRSS() {
+//		if (!PortalUtil.isRSSFeedsEnabled()) {
+//			return false;
+//		}
+//
+//		return _typedSettings.getBooleanValue("enableRss");
+//	}
+//
+//	/* extra */
+//	public void setHiddenNodes(String[] hiddenNodes) {
+//		_typedSettings.setValues("hiddenNodes", hiddenNodes);
+//	}
+//
+//	/* extra */
+//	public void setVisibleNodes(String[] visibleNodes) {
+//		_typedSettings.setValues("visibleNodes", visibleNodes);
+//	}
+//
+//	/* extra */
+//	public void store() throws IOException, ValidatorException {
+//		Settings settings = _typedSettings.getWrappedSettings();
+//
+//		ModifiableSettings modifiableSettings =
+//			settings.getModifiableSettings();
+//
+//		modifiableSettings.store();
+//	}
+//
+//	private final TypedSettings _typedSettings;
 
 }
