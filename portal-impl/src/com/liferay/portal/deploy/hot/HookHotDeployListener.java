672dbec45161663bb800877b7fbb3c47d9925aa4
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-05-22 10:26:38 +0200

    LPS-55555 Rename getInstance methods, as they are not accurate

--- portal-impl/src/com/liferay/portal/deploy/hot/HookHotDeployListener.java
+++ portal-impl/src/com/liferay/portal/deploy/hot/HookHotDeployListener.java
@@ -477,7 +477,7 @@ public class HookHotDeployListener
 		}
 
 		if (portalProperties.containsKey(PropsKeys.DL_STORE_IMPL)) {
-			StoreFactory.setInstance(null);
+			StoreFactory.setStoreInstance(null);
 		}
 
 		Set<String> liferayFilterClassNames =
@@ -1712,7 +1712,7 @@ public class HookHotDeployListener
 			Store store = (Store)newInstance(
 				portletClassLoader, Store.class, storeClassName);
 
-			StoreFactory.setInstance(store);
+			StoreFactory.setStoreInstance(store);
 		}
 
 		if (portalProperties.containsKey(
