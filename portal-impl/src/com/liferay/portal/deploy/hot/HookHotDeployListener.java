db61a09ce4af75c47756e8ed259c44b6a62eeae1
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-05-22 10:40:10 +0200

    LPS-55555 Convert StoreFactory into a singleton, changing static methods to instance methods

--- portal-impl/src/com/liferay/portal/deploy/hot/HookHotDeployListener.java
+++ portal-impl/src/com/liferay/portal/deploy/hot/HookHotDeployListener.java
@@ -477,7 +477,9 @@ public class HookHotDeployListener
 		}
 
 		if (portalProperties.containsKey(PropsKeys.DL_STORE_IMPL)) {
-			StoreFactory.setStoreInstance(null);
+			StoreFactory storeFactory = StoreFactory.getInstance();
+
+			storeFactory.setStoreInstance(null);
 		}
 
 		Set<String> liferayFilterClassNames =
@@ -1712,7 +1714,9 @@ public class HookHotDeployListener
 			Store store = (Store)newInstance(
 				portletClassLoader, Store.class, storeClassName);
 
-			StoreFactory.setStoreInstance(store);
+			StoreFactory storeFactory = StoreFactory.getInstance();
+
+			storeFactory.setStoreInstance(store);
 		}
 
 		if (portalProperties.containsKey(
