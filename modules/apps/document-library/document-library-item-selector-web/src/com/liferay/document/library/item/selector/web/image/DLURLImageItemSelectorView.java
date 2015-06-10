51b60dab8974b9525a8978c5771b0ef4d6be6ed4
Roberto Díaz <roberto.diaz@liferay.com>
2015-06-10 17:51:47 +0200

    LPS-56403 Add new views to add images and files by their url

---
+++ modules/apps/document-library/document-library-item-selector-web/src/com/liferay/document/library/item/selector/web/image/DLURLImageItemSelectorView.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.item.selector.web.image;
+
+import com.liferay.document.library.item.selector.web.BaseDLItemSelectorView;
+import com.liferay.item.selector.ItemSelectorView;
+import com.liferay.item.selector.criteria.DefaultItemSelectorReturnType;
+import com.liferay.item.selector.criteria.image.criterion.ImageItemSelectorCriterion;
+import com.liferay.portal.util.PropsValues;
+
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Roberto Díaz
+ */
+@Component(service = ItemSelectorView.class)
+public class DLURLImageItemSelectorView
+	extends BaseDLItemSelectorView
+		<ImageItemSelectorCriterion, DefaultItemSelectorReturnType> {
+
+	@Override
+	public Class<ImageItemSelectorCriterion> getItemSelectorCriterionClass() {
+		return ImageItemSelectorCriterion.class;
+	}
+
+	@Override
+	public String getJspPath() {
+		return "/o/document-library-item-selector-web/url.jsp";
+	}
+
+	@Override
+	public String[] getMimeTypes() {
+		return PropsValues.DL_FILE_ENTRY_PREVIEW_IMAGE_MIME_TYPES;
+	}
+
+	@Override
+	public Set<DefaultItemSelectorReturnType>
+		getSupportedItemSelectorReturnTypes() {
+
+		return SUPPORTED_ITEM_SELECTOR_RETURN_TYPES;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundle.getBundle(
+			"content/Language", locale);
+
+		return resourceBundle.getString("image-url");
+	}
+
+	private static final Set<DefaultItemSelectorReturnType>
+		SUPPORTED_ITEM_SELECTOR_RETURN_TYPES = new HashSet<>();
+
+	static {
+		SUPPORTED_ITEM_SELECTOR_RETURN_TYPES.add(
+			DefaultItemSelectorReturnType.URL);
+	}
+
+}
