2e9cb882344804743ee8d48b482d8b9c56e5863e
Adolfo Pérez <adolfo.perez@liferay.com>
2015-06-11 18:10:33 +0200

    LPS-56285 Add handler addUniqueActivity method (II)

--- portal-impl/src/com/liferay/portlet/blogs/service/impl/BlogsEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/blogs/service/impl/BlogsEntryLocalServiceImpl.java
@@ -1538,7 +1538,7 @@ public class BlogsEntryLocalServiceImpl extends BlogsEntryLocalServiceBaseImpl {
 						extraDataJSONObject.toString(), 0);
 				}
 				else {
-					socialActivityLocalService.addUniqueActivity(
+					SocialActivityHandlerUtil.addUniqueActivity(
 						user.getUserId(), entry.getGroupId(),
 						BlogsEntry.class.getName(), entryId,
 						BlogsActivityKeys.ADD_ENTRY,
