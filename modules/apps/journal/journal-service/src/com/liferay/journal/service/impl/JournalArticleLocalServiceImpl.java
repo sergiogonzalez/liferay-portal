fc13e1c8c373a28ec23cce304d0cfcaf15d4013c
Adolfo Pérez <adolfo.perez@liferay.com>
2015-06-11 18:10:33 +0200

    LPS-56285 Add handler addUniqueActivity method (II)

--- modules/apps/journal/journal-service/src/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -5812,7 +5812,7 @@ public class JournalArticleLocalServiceImpl
 						extraDataJSONObject.toString(), 0);
 				}
 				else {
-					socialActivityLocalService.addUniqueActivity(
+					SocialActivityHandlerUtil.addUniqueActivity(
 						user.getUserId(), article.getGroupId(),
 						JournalArticle.class.getName(),
 						article.getResourcePrimKey(),
