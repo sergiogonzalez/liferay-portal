5d129c5ee54f742bd4bceeceb185798c3ef29b73
Roberto Díaz <roberto.diaz@liferay.com>
2015-06-11 10:08:52 +0200

    LPS-55295 Create blogs criterion bundle skeleton

---
+++ modules/apps/blogs/blogs-item-selector-api/src/com/liferay/blogs/item/selector/handler/BlogsItemSelectorCriterionHandler.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.blogs.item.selector.handler;
+
+import com.liferay.blogs.item.selector.criterion.BlogsItemSelectorCriterion;
+import com.liferay.item.selector.BaseItemSelectorCriterionHandler;
+import com.liferay.item.selector.criteria.DefaultItemSelectorReturnType;
+
+/**
+ * @author Roberto Díaz
+ */
+public class BlogsItemSelectorCriterionHandler
+	extends BaseItemSelectorCriterionHandler
+		<BlogsItemSelectorCriterion, DefaultItemSelectorReturnType> {
+
+	@Override
+	public Class<BlogsItemSelectorCriterion> getItemSelectorCriterionClass() {
+		return BlogsItemSelectorCriterion.class;
+	}
+
+}
