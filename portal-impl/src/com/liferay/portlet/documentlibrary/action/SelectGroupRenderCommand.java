fce4158b8bfda8a48e1222b541402ac461369410
Iván Zaera Avellón <ivan.zaera@liferay.com>
2015-06-12 14:29:08 +0200

    Convert pure JSP forwards from document library to Commands

---
+++ portal-impl/src/com/liferay/portlet/documentlibrary/action/SelectGroupRenderCommand.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.documentlibrary.action;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.ActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.RenderCommand;
+import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
+import com.liferay.portal.util.PortletKeys;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+/**
+ * @author Iván Zaera
+ */
+@OSGiBeanProperties(
+	property = {
+		"javax.portlet.name=" + PortletKeys.DOCUMENT_LIBRARY,
+		"render.command.name=/document_library/select_group"
+	},
+	service = ActionCommand.class
+)
+public class SelectGroupRenderCommand implements RenderCommand {
+
+	@Override
+	public String processCommand(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		return "/html/portlet/document_library/select_group.jsp";
+	}
+
+}
