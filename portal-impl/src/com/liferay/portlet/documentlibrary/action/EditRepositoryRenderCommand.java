6bdf3b0c5f52efc333af552c57ea580a241dd46b
Iván Zaera Avellón <ivan.zaera@liferay.com>
2015-06-12 12:20:40 +0200

    Convert EditRepositoryAction to Command

---
+++ portal-impl/src/com/liferay/portlet/documentlibrary/action/EditRepositoryRenderCommand.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.documentlibrary.action;
+
+import com.liferay.portal.NoSuchRepositoryException;
+import com.liferay.portal.kernel.portlet.bridges.mvc.RenderCommand;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
+import com.liferay.portal.security.auth.PrincipalException;
+import com.liferay.portal.util.PortletKeys;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+/**
+ * @author Sergio González
+ */
+@OSGiBeanProperties(
+	property = {
+		"javax.portlet.name=" + PortletKeys.DOCUMENT_LIBRARY,
+		"render.command.name=/document_library/edit_repository"
+	},
+	service = RenderCommand.class
+)
+public class EditRepositoryRenderCommand implements RenderCommand {
+
+	@Override
+	public String processCommand(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		try {
+			ActionUtil.getRepository(renderRequest);
+		}
+		catch (Exception e) {
+			if (e instanceof NoSuchRepositoryException ||
+				e instanceof PrincipalException) {
+
+				SessionErrors.add(renderRequest, e.getClass());
+
+				return "/hmlt/portlet/document_library/error.jsp";
+			}
+			else {
+				throw new PortletException(e);
+			}
+		}
+
+		return "/html/portlet/document_library/edit_repository.jsp";
+	}
+
+}
