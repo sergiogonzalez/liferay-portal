b17170f1e9391d1e718dfaa336ed2af3fe0c2ffd
Iván Zaera Avellón <ivan.zaera@liferay.com>
2015-06-22 17:11:09 +0200

    LPS-56244 Convert ViewAction to Command

--- portal-impl/src/com/liferay/portlet/documentlibrary/action/FindFolderAction.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/action/FindFolderAction.java
@@ -46,7 +46,7 @@ public class FindFolderAction extends FindAction {
 		String rootPortletId = PortletConstants.getRootPortletId(portletId);
 
 		if (rootPortletId.equals(PortletKeys.DOCUMENT_LIBRARY_DISPLAY)) {
-			return "/document_library_display/view";
+			return "/document_library/view";
 		}
 		else if (rootPortletId.equals(PortletKeys.MEDIA_GALLERY_DISPLAY)) {
 			return "/image_gallery_display/view";
--- portal-impl/src/com/liferay/portlet/documentlibrary/action/ViewMVCRenderCommand.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/action/ViewMVCRenderCommand.java
@@ -14,31 +14,35 @@
 
 package com.liferay.portlet.documentlibrary.action;
 
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
 import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
 import com.liferay.portal.security.auth.PrincipalException;
-import com.liferay.portal.struts.PortletAction;
+import com.liferay.portal.util.PortletKeys;
 import com.liferay.portlet.documentlibrary.NoSuchFolderException;
 
-import javax.portlet.PortletConfig;
+import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
-import org.apache.struts.action.ActionForm;
-import org.apache.struts.action.ActionForward;
-import org.apache.struts.action.ActionMapping;
-
 /**
- * @author Brian Wing Shun Chan
- * @author Sergio González
+ * @author Iván Zaera
  */
-public class ViewAction extends PortletAction {
+@OSGiBeanProperties(
+	property = {
+		"javax.portlet.name=" + PortletKeys.DOCUMENT_LIBRARY,
+		"mvc.command.name=",
+		"mvc.command.name=/document_library/select_file_entry",
+		"mvc.command.name=/document_library/view"
+	},
+	service = MVCRenderCommand.class
+)
+public class ViewMVCRenderCommand implements MVCRenderCommand {
 
 	@Override
-	public ActionForward render(
-			ActionMapping actionMapping, ActionForm actionForm,
-			PortletConfig portletConfig, RenderRequest renderRequest,
-			RenderResponse renderResponse)
-		throws Exception {
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
 
 		try {
 			ActionUtil.getFolder(renderRequest);
@@ -49,15 +53,14 @@ public class ViewAction extends PortletAction {
 
 				SessionErrors.add(renderRequest, e.getClass());
 
-				return actionMapping.findForward(
-					"portlet.document_library.error");
+				return "/html/portlet/document_library/error.jsp";
 			}
 			else {
-				throw e;
+				throw new PortletException(e);
 			}
 		}
 
-		return actionMapping.findForward("portlet.document_library.view");
+		return "/html/portlet/document_library/view.jsp";
 	}
 
 }
