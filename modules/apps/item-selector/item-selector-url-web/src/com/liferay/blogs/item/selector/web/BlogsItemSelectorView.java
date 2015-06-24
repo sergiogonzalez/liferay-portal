2d2f25f3db5a48611840f6fa86d0cdf40cd059e1
Roberto Díaz <roberto.diaz@liferay.com>
2015-06-24 15:29:05 +0200

    LPS-56403 Copy view bundle

---
+++ modules/apps/item-selector/item-selector-url-web/src/com/liferay/blogs/item/selector/web/BlogsItemSelectorView.java
@@ -0,0 +1,118 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.blogs.item.selector.web;
+
+import com.liferay.blogs.item.selector.criterion.BlogsItemSelectorCriterion;
+import com.liferay.blogs.item.selector.web.display.context.BlogsItemSelectorViewDisplayContext;
+import com.liferay.item.selector.ItemSelectorReturnType;
+import com.liferay.item.selector.ItemSelectorView;
+import com.liferay.item.selector.criteria.FileEntryItemSelectorReturnType;
+import com.liferay.item.selector.criteria.URLItemSelectorReturnType;
+import com.liferay.portal.kernel.util.SetUtil;
+
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Roberto Díaz
+ */
+@Component
+public class BlogsItemSelectorView
+	implements ItemSelectorView<BlogsItemSelectorCriterion> {
+
+	public static final String BLOGS_ITEM_SELECTOR_VIEW_DISPLAY_CONTEXT =
+		"BLOGS_ITEM_SELECTOR_VIEW_DISPLAY_CONTEXT";
+
+	@Override
+	public Class<BlogsItemSelectorCriterion> getItemSelectorCriterionClass() {
+		return BlogsItemSelectorCriterion.class;
+	}
+
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Override
+	public Set<ItemSelectorReturnType> getSupportedItemSelectorReturnTypes() {
+		return _supportedItemSelectorReturnTypes;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundle.getBundle(
+			"content/Language", locale);
+
+		return resourceBundle.getString("blogs-images");
+	}
+
+	@Override
+	public void renderHTML(
+			ServletRequest request, ServletResponse response,
+			BlogsItemSelectorCriterion blogsItemSelectorCriterion,
+			PortletURL portletURL, String itemSelectedEventName)
+		throws IOException, ServletException {
+
+		BlogsItemSelectorViewDisplayContext
+			blogsItemSelectorViewDisplayContext =
+				new BlogsItemSelectorViewDisplayContext(
+					blogsItemSelectorCriterion, this, itemSelectedEventName,
+					portletURL);
+
+		request.setAttribute(
+			BLOGS_ITEM_SELECTOR_VIEW_DISPLAY_CONTEXT,
+			blogsItemSelectorViewDisplayContext);
+
+		ServletContext servletContext = getServletContext();
+
+		RequestDispatcher requestDispatcher =
+			servletContext.getRequestDispatcher("/blogs_attachments.jsp");
+
+		requestDispatcher.include(request, response);
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.blogs.item.selector.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		_servletContext = servletContext;
+	}
+
+	private static final Set<ItemSelectorReturnType>
+		_supportedItemSelectorReturnTypes = Collections.unmodifiableSet(
+			SetUtil.fromArray(
+				new ItemSelectorReturnType[] {
+					new FileEntryItemSelectorReturnType(),
+					new URLItemSelectorReturnType()
+				}));
+
+	private ServletContext _servletContext;
+
+}
---
+++ modules/apps/item-selector/item-selector-url-web/src/com/liferay/blogs/item/selector/web/display/context/BlogsItemSelectorViewDisplayContext.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.blogs.item.selector.web.display.context;
+
+import com.liferay.blogs.item.selector.criterion.BlogsItemSelectorCriterion;
+import com.liferay.blogs.item.selector.web.BlogsItemSelectorView;
+import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portlet.blogs.service.BlogsEntryLocalServiceUtil;
+
+import java.util.Locale;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Roberto Díaz
+ */
+public class BlogsItemSelectorViewDisplayContext {
+
+	public BlogsItemSelectorViewDisplayContext(
+		BlogsItemSelectorCriterion blogsItemSelectorCriterion,
+		BlogsItemSelectorView blogsItemSelectorView,
+		String itemSelectedEventName, PortletURL portletURL) {
+
+		_blogsItemSelectorCriterion = blogsItemSelectorCriterion;
+		_blogsItemSelectorView = blogsItemSelectorView;
+		_itemSelectedEventName = itemSelectedEventName;
+		_portletURL = portletURL;
+	}
+
+	public Folder fetchAttachmentsFolder(long userId, long groupId) {
+		return BlogsEntryLocalServiceUtil.fetchAttachmentsFolder(
+			userId, groupId);
+	}
+
+	public BlogsItemSelectorCriterion getBlogsItemSelectorCriterion() {
+		return _blogsItemSelectorCriterion;
+	}
+
+	public String getDisplayStyle(HttpServletRequest request) {
+		return ParamUtil.getString(request, "displayStyle");
+	}
+
+	public String getItemSelectedEventName() {
+		return _itemSelectedEventName;
+	}
+
+	public PortletURL getPortletURL() {
+		return _portletURL;
+	}
+
+	public String getTitle(Locale locale) {
+		return _blogsItemSelectorView.getTitle(locale);
+	}
+
+	private final BlogsItemSelectorCriterion _blogsItemSelectorCriterion;
+	private final BlogsItemSelectorView _blogsItemSelectorView;
+	private final String _itemSelectedEventName;
+	private final PortletURL _portletURL;
+
+}
