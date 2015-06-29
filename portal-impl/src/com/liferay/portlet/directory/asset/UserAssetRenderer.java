9587aa129fda2a9e958f9d1ddb7acb9635d27e0a
Pei-Jung Lan <pei-jung.lan@liferay.com>
2015-06-29 15:24:20 -0700

    LPS-55590 Update struts for EditUser render URL

--- portal-impl/src/com/liferay/portlet/directory/asset/UserAssetRenderer.java
+++ portal-impl/src/com/liferay/portlet/directory/asset/UserAssetRenderer.java
@@ -83,7 +83,8 @@ public class UserAssetRenderer extends BaseAssetRenderer {
 			getControlPanelPlid(liferayPortletRequest), PortletKeys.USERS_ADMIN,
 			PortletRequest.RENDER_PHASE);
 
-		portletURL.setParameter("struts_action", "/users_admin/edit_user");
+		portletURL.setParameter(
+			"mvcRenderCommandName", "/users_admin/edit_user");
 		portletURL.setParameter("p_u_i_d", String.valueOf(_user.getUserId()));
 
 		return portletURL;
