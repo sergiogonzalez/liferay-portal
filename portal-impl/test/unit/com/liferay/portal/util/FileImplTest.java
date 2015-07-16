be2d73df7274f39559173b8cea04100fae532232
Roberto Díaz <roberto.diaz@liferay.com>
2015-07-16 11:06:41 +0200

    LPS-56817 Add tests

--- portal-impl/test/unit/com/liferay/portal/util/FileImplTest.java
+++ portal-impl/test/unit/com/liferay/portal/util/FileImplTest.java
@@ -25,6 +25,23 @@ import org.junit.Test;
 public class FileImplTest {
 
 	@Test
+	public void testAppendSuffix() throws Exception {
+		String fileName1 = _fileImpl.appendSuffix("test.jsp", "1");
+		String fileName2 = _fileImpl.appendSuffix("test.jsp", "1111111");
+		String fileName3 = _fileImpl.appendSuffix("test.jsp", "A");
+		String fileName4 = _fileImpl.appendSuffix("test.jsp", "AAAAAAA");
+		String fileName5 = _fileImpl.appendSuffix("test.jsp", "1!$eae1");
+		String fileName6 = _fileImpl.appendSuffix("test(1).jsp", "1");
+
+		Assert.assertEquals("test(1).jsp", fileName1);
+		Assert.assertEquals("test(1111111).jsp", fileName2);
+		Assert.assertEquals("test(A).jsp", fileName3);
+		Assert.assertEquals("test(AAAAAAA).jsp", fileName4);
+		Assert.assertEquals("test(1!$eae1).jsp", fileName5);
+		Assert.assertEquals("test(1)(1).jsp", fileName6);
+	}
+
+	@Test
 	public void testGetPathBackSlashForwardSlash() {
 		Assert.assertEquals(
 			"aaa\\bbb/ccc\\ddd",
@@ -57,6 +74,28 @@ public class FileImplTest {
 	@Test
 	public void testGetShortFileNameNoPath() {
 		Assert.assertEquals("aaa.bbb", _fileImpl.getShortFileName("aaa.bbb"));
+	}
+
+	@Test
+	public void testStripSuffix() throws Exception {
+		String fileName1 = _fileImpl.appendSuffix("test.jsp", "1");
+		String fileName2 = _fileImpl.appendSuffix("test.jsp", "1111111");
+		String fileName3 = _fileImpl.appendSuffix("test.jsp", "A");
+		String fileName4 = _fileImpl.appendSuffix("test.jsp", "AAAAAAA");
+		String fileName5 = _fileImpl.appendSuffix("test.jsp", "1!$eae1");
+		String fileName6 = _fileImpl.appendSuffix("test(1).jsp", "1");
+
+		Assert.assertEquals("test.jsp", _fileImpl.stripSuffix(fileName1));
+		Assert.assertEquals("test.jsp", _fileImpl.stripSuffix(fileName2));
+		Assert.assertEquals("test.jsp", _fileImpl.stripSuffix(fileName3));
+		Assert.assertEquals("test.jsp", _fileImpl.stripSuffix(fileName4));
+		Assert.assertEquals("test.jsp", _fileImpl.stripSuffix(fileName5));
+		Assert.assertEquals("test(1).jsp", _fileImpl.stripSuffix(fileName6));
+		Assert.assertEquals("test.jsp", _fileImpl.stripSuffix("test.jsp"));
+		Assert.assertEquals("()test.jsp", _fileImpl.stripSuffix("()test.jsp"));
+		Assert.assertEquals("test(1.jsp", _fileImpl.stripSuffix("test(1.jsp"));
+		Assert.assertEquals(
+			"test)1(.jsp", _fileImpl.stripSuffix("test)1(.jsp"));
 	}
 
 	private final FileImpl _fileImpl = new FileImpl();
