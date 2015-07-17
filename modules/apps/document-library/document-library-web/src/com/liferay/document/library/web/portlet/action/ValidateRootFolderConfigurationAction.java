97d9e9fd62c6581edf848ecef49ebea3550c4a79
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-07-17 15:10:34 +0200

    LPS-56703 Move ConfigurationAction and create a ValidateRootFolderConfigurationAction so they can reuse common logic regarding validation

--- modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/portlet/action/ValidateRootFolderConfigurationAction.java
+++ modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/portlet/action/ValidateRootFolderConfigurationAction.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portlet.documentlibrary.action;
+package com.liferay.document.library.web.portlet.action;
 
 import com.liferay.portal.NoSuchRepositoryEntryException;
 import com.liferay.portal.kernel.portlet.BaseJSPSettingsConfigurationAction;
@@ -30,10 +30,9 @@ import javax.portlet.ActionResponse;
 import javax.portlet.PortletConfig;
 
 /**
- * @author Jorge Ferrer
  * @author Sergio González
  */
-public class ConfigurationActionImpl
+public abstract class ValidateRootFolderConfigurationAction
 	extends BaseJSPSettingsConfigurationAction {
 
 	@Override
@@ -52,17 +51,7 @@ public class ConfigurationActionImpl
 	}
 
 	protected void validate(ActionRequest actionRequest) throws Exception {
-		validateDisplayStyleViews(actionRequest);
 		validateRootFolder(actionRequest);
-	}
-
-	protected void validateDisplayStyleViews(ActionRequest actionRequest) {
-		String displayViews = GetterUtil.getString(
-			getParameter(actionRequest, "displayViews"));
-
-		if (Validator.isNull(displayViews)) {
-			SessionErrors.add(actionRequest, "displayViewsInvalid");
-		}
 	}
 
 	protected void validateRootFolder(ActionRequest actionRequest)
