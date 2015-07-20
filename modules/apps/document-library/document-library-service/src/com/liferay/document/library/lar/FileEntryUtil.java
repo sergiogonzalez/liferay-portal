e5a22ff726313d2ea6d4e360ff2adcba89310d43
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-07-20 09:24:39 +0200

    LPS-56703 Move StagedModelDataHandlers to OSGi module

---
+++ modules/apps/document-library/document-library-service/src/com/liferay/document/library/lar/FileEntryUtil.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.lar;
+
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.repository.liferayrepository.model.LiferayFileEntry;
+import com.liferay.portlet.documentlibrary.NoSuchFileEntryException;
+import com.liferay.portlet.documentlibrary.model.DLFileEntry;
+import com.liferay.portlet.documentlibrary.service.persistence.DLFileEntryUtil;
+import com.liferay.portlet.documentlibrary.util.RepositoryModelUtil;
+
+import java.util.List;
+
+/**
+ * @author Alexander Chow
+ */
+public class FileEntryUtil {
+
+	public static FileEntry fetchByR_F_T(
+		long repositoryId, long folderId, String title) {
+
+		DLFileEntry dlFileEntry = DLFileEntryUtil.fetchByG_F_T(
+			repositoryId, folderId, title);
+
+		if (dlFileEntry == null) {
+			return null;
+		}
+
+		return new LiferayFileEntry(dlFileEntry);
+	}
+
+	public static FileEntry fetchByUUID_R(String uuid, long repositoryId) {
+		DLFileEntry dlFileEntry = DLFileEntryUtil.fetchByUUID_G(
+			uuid, repositoryId);
+
+		if (dlFileEntry == null) {
+			return null;
+		}
+
+		return new LiferayFileEntry(dlFileEntry);
+	}
+
+	/**
+	 * @deprecated As of 7.0.0, with no direct replacement
+	 */
+	@Deprecated
+	public static List<FileEntry> findByR_F(long repositoryId, long folderId) {
+		List<DLFileEntry> dlFileEntries = DLFileEntryUtil.findByG_F(
+			repositoryId, folderId);
+
+		return RepositoryModelUtil.toFileEntries(dlFileEntries);
+	}
+
+	/**
+	 * @deprecated As of 7.0.0, with no direct replacement
+	 */
+	@Deprecated
+	public static FileEntry findByR_F_T(
+			long repositoryId, long folderId, String title)
+		throws NoSuchFileEntryException {
+
+		DLFileEntry dlFileEntry = DLFileEntryUtil.findByG_F_T(
+			repositoryId, folderId, title);
+
+		return new LiferayFileEntry(dlFileEntry);
+	}
+
+}
