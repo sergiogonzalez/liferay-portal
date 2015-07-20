ad65b3a7909eaf0a5e8759419c8e01f6127fc2e2
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-07-20 11:22:03 +0200

    LPS-56703 TrashHandler

--- modules/apps/document-library/document-library-service/src/com/liferay/document/library/trash/DLFileShortcutTrashRenderer.java
+++ modules/apps/document-library/document-library-service/src/com/liferay/document/library/trash/DLFileShortcutTrashRenderer.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portlet.documentlibrary.trash;
+package com.liferay.document.library.trash;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
