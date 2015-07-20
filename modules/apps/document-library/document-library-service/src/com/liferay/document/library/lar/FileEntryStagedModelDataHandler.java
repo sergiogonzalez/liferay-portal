4fb93115c6d0787528443a21394a5e5bc649d33a
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-07-20 09:17:54 +0200

    LPS-56703 Move PortletDataHandlers to OSGi module

--- modules/apps/document-library/document-library-service/src/com/liferay/document/library/lar/FileEntryStagedModelDataHandler.java
+++ modules/apps/document-library/document-library-service/src/com/liferay/document/library/lar/FileEntryStagedModelDataHandler.java
@@ -14,6 +14,7 @@
 
 package com.liferay.document.library.lar;
 
+import com.liferay.document.library.web.lar.DLPortletDataHandler;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
