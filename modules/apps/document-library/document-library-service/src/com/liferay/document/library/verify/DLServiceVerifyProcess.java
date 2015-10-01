c58cf2ba7655dc7b10141f1dfa30642d2f1b211e
Jorge Díaz <jorge.diaz@liferay.com>
2015-10-01 10:30:56 +0200

    LPS-58632 Adjust code to avoid adding suffix when title is ok

--- modules/apps/document-library/document-library-service/src/com/liferay/document/library/verify/DLServiceVerifyProcess.java
+++ modules/apps/document-library/document-library-service/src/com/liferay/document/library/verify/DLServiceVerifyProcess.java
@@ -541,15 +541,6 @@ public class DLServiceVerifyProcess extends VerifyProcess {
 		String uniqueTitle = title;
 
 		for (int i = 1;;) {
-			uniqueTitle =
-				titleWithoutExtension + StringPool.UNDERLINE +
-					String.valueOf(i);
-
-			if (Validator.isNotNull(titleExtension)) {
-				uniqueTitle = uniqueTitle.concat(
-					StringPool.PERIOD.concat(titleExtension));
-			}
-
 			String uniqueFileName = DLUtil.getSanitizedFileName(
 				uniqueTitle, extension);
 
@@ -566,8 +557,15 @@ public class DLServiceVerifyProcess extends VerifyProcess {
 
 					throw pe;
 				}
+			}
 
-				i++;
+			uniqueTitle =
+				titleWithoutExtension + StringPool.UNDERLINE +
+					String.valueOf(i++);
+
+			if (Validator.isNotNull(titleExtension)) {
+				uniqueTitle = uniqueTitle.concat(
+					StringPool.PERIOD.concat(titleExtension));
 			}
 		}
 	}
