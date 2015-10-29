0d7cdef3231078d45a5cf52b2587fe6dfac24a9c
Roberto Díaz <roberto.diaz@liferay.com>
2015-10-29 17:18:32 +0100

    LPS-58641 Change the way the inner urls to item selector are been created in order to change the tab scope. We need to create different urls to different scopes depending on the tab.

--- modules/apps/item-selector/item-selector-api/src/main/java/com/liferay/item/selector/ItemSelector.java
+++ modules/apps/item-selector/item-selector-api/src/main/java/com/liferay/item/selector/ItemSelector.java
@@ -14,6 +14,7 @@
 
 package com.liferay.item.selector;
 
+import com.liferay.portal.model.Group;
 import com.liferay.portlet.RequestBackedPortletURLFactory;
 
 import java.util.List;
@@ -24,6 +25,7 @@ import javax.portlet.PortletURL;
 
 /**
  * @author Iván Zaera
+ * @author Roberto Díaz
  */
 public interface ItemSelector {
 
@@ -32,6 +34,11 @@ public interface ItemSelector {
 
 	public ItemSelectorRendering getItemSelectorRendering(
 		PortletRequest portletRequest, PortletResponse portletResponse);
+
+	public PortletURL getItemSelectorURL(
+		RequestBackedPortletURLFactory requestBackedPortletURLFactory,
+		Group group, long refererPlid, String itemSelectedEventName,
+		ItemSelectorCriterion... itemSelectorCriteria);
 
 	public PortletURL getItemSelectorURL(
 		RequestBackedPortletURLFactory requestBackedPortletURLFactory,
