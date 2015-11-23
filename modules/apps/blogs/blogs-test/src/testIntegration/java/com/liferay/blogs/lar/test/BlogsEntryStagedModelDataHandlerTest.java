159e6a566e2f71e82e24068b46ce64ca1f00985b
Roberto Díaz <roberto.diaz@liferay.com>
2015-11-23 18:57:15 +0100

    LPS-60682 Test

--- modules/apps/blogs/blogs-test/src/testIntegration/java/com/liferay/blogs/lar/test/BlogsEntryStagedModelDataHandlerTest.java
+++ modules/apps/blogs/blogs-test/src/testIntegration/java/com/liferay/blogs/lar/test/BlogsEntryStagedModelDataHandlerTest.java
@@ -15,11 +15,16 @@
 package com.liferay.blogs.lar.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.servlet.taglib.ui.ImageSelector;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.TransactionalTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.MimeTypesUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.TempFileEntryUtil;
 import com.liferay.portal.lar.test.BaseWorkflowedStagedModelDataHandlerTestCase;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.StagedModel;
@@ -29,12 +34,18 @@ import com.liferay.portlet.blogs.model.BlogsEntry;
 import com.liferay.portlet.blogs.service.BlogsEntryLocalServiceUtil;
 import com.liferay.portlet.blogs.util.test.BlogsTestUtil;
 
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import com.liferay.portlet.documentlibrary.service.DLAppLocalServiceUtil;
+import com.liferay.portlet.exportimport.lar.StagedModelDataHandlerUtil;
+import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -110,5 +121,64 @@ public class BlogsEntryStagedModelDataHandlerTest
 	protected boolean isCommentableStagedModel() {
 		return true;
 	}
+
+	@Test
+	public void A() throws Exception {
+		initExport();
+
+		StagedModel stagedModel = addBlogsEntryWithCoverImage();
+
+		initImport();
+
+		StagedModel exportedStagedModel = readExportedStagedModel(stagedModel);
+
+		Assert.assertNotNull(exportedStagedModel);
+
+		StagedModelDataHandlerUtil.importStagedModel(
+			portletDataContext, exportedStagedModel);
+
+		BlogsEntry importedEntry = (BlogsEntry)getStagedModel(
+			stagedModel.getUuid(), liveGroup);
+
+		long coverImageFileEntryId = importedEntry.getCoverImageFileEntryId();
+
+		FileEntry fileEntry = DLAppLocalServiceUtil.getFileEntry(
+			coverImageFileEntryId);
+
+		Assert.assertEquals("image1.jpg", fileEntry.getTitle());
+	}
+
+	private StagedModel addBlogsEntryWithCoverImage()
+		throws Exception {
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				stagingGroup.getGroupId(), TestPropsValues.getUserId());
+
+		Class<?> clazz = getClass();
+
+		ClassLoader classLoader = clazz.getClassLoader();
+
+		InputStream inputStream = classLoader.getResourceAsStream(
+			"com/liferay/portal/util/dependencies/test.jpg");
+
+		FileEntry fileEntry = TempFileEntryUtil.addTempFileEntry(
+				serviceContext.getScopeGroupId(), TestPropsValues.getUserId(),
+				BlogsEntry.class.getName(), "image1.jpg", inputStream,
+				MimeTypesUtil.getContentType("image1.jpg"));
+
+		ImageSelector imageSelector = new ImageSelector(
+			fileEntry.getFileEntryId(), StringPool.BLANK, IMAGE_CROP_REGION);
+
+		return BlogsEntryLocalServiceUtil.addEntry(
+				TestPropsValues.getUserId(), RandomTestUtil.randomString(),
+				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+				RandomTestUtil.randomString(), new Date(), true, true,
+				new String[0], StringPool.BLANK, imageSelector, null,
+				serviceContext);
+	}
+
+	protected static final String IMAGE_CROP_REGION =
+		"{\"height\": 10, \"width\": 10, \"x\": 0, \"y\": 0}";
 
 }
