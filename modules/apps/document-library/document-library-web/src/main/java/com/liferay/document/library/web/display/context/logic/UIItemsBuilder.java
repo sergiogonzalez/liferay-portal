sergiogonzalez/liferay-portal

commit 14cc967ebf5a22a765f7563f177ba2b6b30c0081

LPS-62330 Create an action to view folder

modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/display/context/logic/UIItemsBuilder.java
@@ -54,6 +54,7 @@ import com.liferay.portal.util.PropsValues;
 import com.liferay.portlet.PortletURLUtil;
 import com.liferay.portlet.documentlibrary.display.context.DLUIItemKeys;
 import com.liferay.portlet.documentlibrary.model.DLFileEntryConstants;
+import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
 import com.liferay.portlet.documentlibrary.util.DLUtil;
 import com.liferay.portlet.trash.util.TrashUtil;
 import com.liferay.taglib.security.PermissionsURLTag;
@@ -263,10 +264,18 @@ public class UIItemsBuilder {
 
 		PortletURL portletURL = liferayPortletResponse.createRenderURL();
 
-		portletURL.setParameter(
-			"mvcRenderCommandName", "/document_library/view");
-		portletURL.setParameter(
-			"folderId", String.valueOf(_fileEntry.getFolderId()));
+		long folderId = _fileEntry.getFolderId();
+
+		if (folderId == DLFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
+			portletURL.setParameter(
+				"mvcRenderCommandName", "/document_library/view");
+		}
+		else {
+			portletURL.setParameter(
+				"mvcRenderCommandName", "/document_library/view_folder");
+		}
+
+		portletURL.setParameter("folderId", String.valueOf(folderId));
 
 		StringBundler sb = new StringBundler(5);
 
@@ -376,10 +385,18 @@ public class UIItemsBuilder {
 
 		PortletURL redirectURL = liferayPortletResponse.createRenderURL();
 
-		redirectURL.setParameter(
-			"mvcRenderCommandName", "/document_library/view");
-		redirectURL.setParameter("folderId", String.valueOf(_folderId));
+		long folderId = _fileEntry.getFolderId();
 
+		if (folderId == DLFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
+			portletURL.setParameter(
+				"mvcRenderCommandName", "/document_library/view");
+		}
+		else {
+			portletURL.setParameter(
+				"mvcRenderCommandName", "/document_library/view_folder");
+		}
+
+		portletURL.setParameter("folderId", String.valueOf(folderId));
 		portletURL.setParameter("redirect", redirectURL.toString());
 
 		portletURL.setParameter(
@@ -417,8 +434,18 @@ public class UIItemsBuilder {
 
 		PortletURL portletURL = liferayPortletResponse.createRenderURL();
 
-		portletURL.setParameter(
-			"mvcRenderCommandName", "/document_library/view");
+		long folderId = _fileEntry.getFolderId();
+
+		if (folderId == DLFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
+			portletURL.setParameter(
+				"mvcRenderCommandName", "/document_library/view");
+		}
+		else {
+			portletURL.setParameter(
+				"mvcRenderCommandName", "/document_library/view_folder");
+		}
+
+		portletURL.setParameter("folderId", String.valueOf(folderId));
 		portletURL.setParameter(
 			"folderId", String.valueOf(_fileEntry.getFolderId()));
 

modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/asset/DLFolderAssetRenderer.java
@@ -148,7 +148,7 @@ public class DLFolderAssetRenderer
 			liferayPortletResponse, windowState);
 
 		portletURL.setParameter(
-			"mvcRenderCommandName", "/document_library/view");
+			"mvcRenderCommandName", "/document_library/view_folder");
 		portletURL.setParameter(
 			"folderId", String.valueOf(_folder.getFolderId()));
 		portletURL.setWindowState(windowState);

modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/portlet/action/ViewFolderMVCRenderCommand.java
@@ -1,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.portlet.action;
+
+import com.liferay.document.library.web.constants.DLPortletKeys;
+import com.liferay.document.library.web.constants.DLWebKeys;
+import com.liferay.document.library.web.portlet.toolbar.contributor.DLPortletToolbarContributor;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Roberto Díaz
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY,
+		"javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY_ADMIN,
+		"mvc.command.name=/document_library/view_folder"
+	},
+	service = MVCRenderCommand.class
+)
+public class ViewFolderMVCRenderCommand extends GetFolderMVCRenderCommand {
+
+	@Override
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		renderRequest.setAttribute(
+			DLWebKeys.DOCUMENT_LIBRARY_PORTLET_TOOLBAR_CONTRIBUTOR,
+			_dlPortletToolbarContributor);
+
+		return super.render(renderRequest, renderResponse);
+	}
+
+	@Override
+	protected String getPath() {
+		return "/document_library/view.jsp";
+	}
+
+	@Reference(unbind = "-")
+	protected void setDLPortletToolbarContributor(
+		DLPortletToolbarContributor dlPortletToolbarContributor) {
+
+		_dlPortletToolbarContributor = dlPortletToolbarContributor;
+	}
+
+	private DLPortletToolbarContributor _dlPortletToolbarContributor;
+
+}

modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/portlet/configuration/icon/DeleteFileEntryPortletConfigurationIcon.java
@@ -21,6 +21,7 @@ import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigura
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
 import com.liferay.portlet.trash.util.TrashUtil;
 
 import javax.portlet.ActionRequest;
@@ -70,10 +71,18 @@ public class DeleteFileEntryPortletConfigurationIcon
 			portletRequest, DLPortletKeys.DOCUMENT_LIBRARY_ADMIN,
 			PortletRequest.RENDER_PHASE);
 
-		redirectURL.setParameter(
-			"mvcRenderCommandName", "/document_library/view");
-		redirectURL.setParameter(
-			"folderId", String.valueOf(_fileEntry.getFolderId()));
+		long folderId = _fileEntry.getFolderId();
+
+		if (folderId == DLFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
+			redirectURL.setParameter(
+				"mvcRenderCommandName", "/document_library/view");
+		}
+		else {
+			redirectURL.setParameter(
+				"mvcRenderCommandName", "/document_library/view_folder");
+		}
+
+		redirectURL.setParameter("folderId", String.valueOf(folderId));
 
 		portletURL.setParameter("redirect", redirectURL.toString());
 		portletURL.setParameter(

modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/portlet/configuration/icon/MoveFileEntryPortletConfigurationIcon.java
@@ -20,6 +20,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
 
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletURL;
@@ -56,10 +57,18 @@ public class MoveFileEntryPortletConfigurationIcon
 			portletRequest, DLPortletKeys.DOCUMENT_LIBRARY_ADMIN,
 			PortletRequest.RENDER_PHASE);
 
-		redirectURL.setParameter(
-			"mvcRenderCommandName", "/document_library/view");
-		redirectURL.setParameter(
-			"folderId", String.valueOf(_fileEntry.getFolderId()));
+		long folderId = _fileEntry.getFolderId();
+
+		if (folderId == DLFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
+			redirectURL.setParameter(
+				"mvcRenderCommandName", "/document_library/view");
+		}
+		else {
+			redirectURL.setParameter(
+				"mvcRenderCommandName", "/document_library/view_folder");
+		}
+
+		redirectURL.setParameter("folderId", String.valueOf(folderId));
 
 		portletURL.setParameter("redirect", redirectURL.toString());
 

modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/util/DLBreadcrumbUtil.java
@@ -108,7 +108,7 @@ public class DLBreadcrumbUtil {
 		PortletURL portletURL = liferayPortletResponse.createRenderURL();
 
 		portletURL.setParameter(
-			"mvcRenderCommandName", "/document_library/view");
+			"mvcRenderCommandName", "/document_library/view_folder");
 
 		Map<String, Object> data = new HashMap<>();
 
@@ -233,8 +233,20 @@ public class DLBreadcrumbUtil {
 				ignoreRootFolder, portletURL);
 		}
 		else {
-			portletURL.setParameter(
-				"mvcRenderCommandName", "/document_library/view");
+			long folderId = DLFolderConstants.DEFAULT_PARENT_FOLDER_ID;
+
+			if (folder != null) {
+				folderId = folder.getFolderId();
+			}
+
+			if (folderId != DLFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
+				portletURL.setParameter(
+					"mvcRenderCommandName", "/document_library/view_folder");
+			}
+			else {
+				portletURL.setParameter(
+					"mvcRenderCommandName", "/document_library/view");
+			}
 		}
 
 		addPortletBreadcrumbEntries(folder, request, portletURL);

modules/apps/document-library/document-library-web/src/main/resources/META-INF/resources/document_library/display_style_buttons.jsp
@@ -36,7 +36,7 @@ String keywords = ParamUtil.getString(request, "keywords");
 
 PortletURL displayStyleURL = renderResponse.createRenderURL();
 
-displayStyleURL.setParameter("mvcRenderCommandName", Validator.isNull(keywords) ? "/document_library/view" : "/document_library/search");
+displayStyleURL.setParameter("mvcRenderCommandName", Validator.isNull(keywords) ? (folderId == DLFolderConstants.DEFAULT_PARENT_FOLDER_ID ? "/document_library/view" : "/document_library/view_folder") : "/document_library/search");
 displayStyleURL.setParameter("navigation", HtmlUtil.escapeJS(navigation));
 
 if (curEntry > 0) {

modules/apps/document-library/document-library-web/src/main/resources/META-INF/resources/document_library/folder_action.jsp
@@ -389,7 +389,7 @@ String iconMenuId = null;
 
 			<c:if test="<%= hasDeletePermission && !folder.isMountPoint() %>">
 				<portlet:renderURL var="redirectURL">
-					<portlet:param name="mvcRenderCommandName" value="/document_library/view" />
+					<portlet:param name="mvcRenderCommandName" value='<%= folder.getParentFolderId() == DLFolderConstants.DEFAULT_PARENT_FOLDER_ID ? "/document_library/view" : "/document_library/view_folder" %>' />
 					<portlet:param name="folderId" value="<%= String.valueOf(folder.getParentFolderId()) %>" />
 				</portlet:renderURL>
 
@@ -404,7 +404,7 @@ String iconMenuId = null;
 
 			<c:if test="<%= hasDeletePermission && folder.isMountPoint() %>">
 				<portlet:renderURL var="redirectURL">
-					<portlet:param name="mvcRenderCommandName" value="/document_library/view" />
+					<portlet:param name="mvcRenderCommandName" value='<%= folder.getParentFolderId() == DLFolderConstants.DEFAULT_PARENT_FOLDER_ID ? "/document_library/view" : "/document_library/view_folder" %>' />
 					<portlet:param name="folderId" value="<%= String.valueOf(folder.getParentFolderId()) %>" />
 				</portlet:renderURL>
 

commit e60ce391e93e20d3c2c1824043be046fb46a3d59

LPS-62822 Fix regression introduced in 14cc967 LPS-62330

modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/display/context/logic/UIItemsBuilder.java
@@ -383,20 +383,20 @@ public class UIItemsBuilder {
 		portletURL.setParameter(
 			"mvcRenderCommandName", "/document_library/move_entry");
 
+		PortletURL redirectURL = liferayPortletResponse.createRenderURL();
+
 		long folderId = _fileEntry.getFolderId();
 
 		if (folderId == DLFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
-			portletURL.setParameter(
+			redirectURL.setParameter(
 				"mvcRenderCommandName", "/document_library/view");
 		}
 		else {
-			portletURL.setParameter(
+			redirectURL.setParameter(
 				"mvcRenderCommandName", "/document_library/view_folder");
 		}
 
-		portletURL.setParameter("folderId", String.valueOf(folderId));
-
-		PortletURL redirectURL = liferayPortletResponse.createRenderURL();
+		redirectURL.setParameter("folderId", String.valueOf(folderId));
 
 		portletURL.setParameter("redirect", redirectURL.toString());
 