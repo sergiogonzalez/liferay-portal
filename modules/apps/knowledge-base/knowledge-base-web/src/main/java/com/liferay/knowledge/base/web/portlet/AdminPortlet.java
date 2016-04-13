9e5058c8b249e385024e1dafcb770b32d319cc96
Adolfo Pérez <adolfo.perez@liferay.com>
2016-04-13 19:44:33 +0200

    LPS-64277 Revert and use static methods for this beans
    Though they're spring beans, it seems they're not published as DS
    components.

--- modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/portlet/AdminPortlet.java
+++ modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/portlet/AdminPortlet.java
@@ -46,7 +46,6 @@ import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.upload.UploadPortletRequest;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PortalUtil;
@@ -433,11 +432,6 @@ public class AdminPortlet extends BaseKBPortlet {
 	}
 
 	@Override
-	protected Http getHttp() {
-		return _http;
-	}
-
-	@Override
 	protected JSONFactory getJSONFactory() {
 		return _jsonFactory;
 	}
@@ -482,11 +476,6 @@ public class AdminPortlet extends BaseKBPortlet {
 	}
 
 	@Reference(unbind = "-")
-	protected void setHttp(Http http) {
-		_http = http;
-	}
-
-	@Reference(unbind = "-")
 	protected void setJSONFactory(JSONFactory jsonFactory) {
 		_jsonFactory = jsonFactory;
 	}
@@ -523,7 +512,6 @@ public class AdminPortlet extends BaseKBPortlet {
 		_portal = portal;
 	}
 
-	private Http _http;
 	private JSONFactory _jsonFactory;
 	private KBArticleService _kbArticleService;
 	private KBCommentLocalService _kbCommentLocalService;
--- modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/portlet/SearchPortlet.java
+++ modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/portlet/SearchPortlet.java
@@ -33,7 +33,6 @@ import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.Validator;
@@ -154,11 +153,6 @@ public class SearchPortlet extends BaseKBPortlet {
 	}
 
 	@Override
-	protected Http getHttp() {
-		return _http;
-	}
-
-	@Override
 	protected JSONFactory getJSONFactory() {
 		return _jsonFactory;
 	}
@@ -228,11 +222,6 @@ public class SearchPortlet extends BaseKBPortlet {
 	}
 
 	@Reference(unbind = "-")
-	protected void setHttp(Http http) {
-		_http = http;
-	}
-
-	@Reference(unbind = "-")
 	protected void setJSONFactory(JSONFactory jsonFactory) {
 		_jsonFactory = jsonFactory;
 	}
@@ -269,7 +258,6 @@ public class SearchPortlet extends BaseKBPortlet {
 		_portal = portal;
 	}
 
-	private Http _http;
 	private JSONFactory _jsonFactory;
 	private KBArticleService _kbArticleService;
 	private KBCommentLocalService _kbCommentLocalService;
