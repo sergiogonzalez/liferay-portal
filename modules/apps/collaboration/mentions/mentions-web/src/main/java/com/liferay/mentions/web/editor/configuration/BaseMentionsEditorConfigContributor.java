740d8a88fdfad62a9a9b0aa661b409635847f7c3
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2016-04-21 09:39:06 +0200

    LPS-65024 Alloy spaces in autocomplete for mentions and wiki pages

--- modules/apps/collaboration/mentions/mentions-web/src/main/java/com/liferay/mentions/web/editor/configuration/BaseMentionsEditorConfigContributor.java
+++ modules/apps/collaboration/mentions/mentions-web/src/main/java/com/liferay/mentions/web/editor/configuration/BaseMentionsEditorConfigContributor.java
@@ -78,6 +78,9 @@ public class BaseMentionsEditorConfigContributor
 
 		triggerJSONArray.put(triggerJSONObject);
 
+		autoCompleteConfigJSONObject.put(
+			"regExp", "(?:\\strigger|^trigger)(\\w+(\\s\\w+)+|[^\\s]+)");
+
 		autoCompleteConfigJSONObject.put("trigger", triggerJSONArray);
 
 		jsonObject.put("autocomplete", autoCompleteConfigJSONObject);
