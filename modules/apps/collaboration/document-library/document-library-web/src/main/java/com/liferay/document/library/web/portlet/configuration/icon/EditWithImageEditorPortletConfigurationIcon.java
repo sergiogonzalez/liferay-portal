eb341a504b817f54f2a46c4b88aeaeb573f651c9
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2016-05-05 09:27:58 +0200

    LPS-65407 Add Icon menu in file entry detail

---
+++ modules/apps/collaboration/document-library/document-library-web/src/main/java/com/liferay/document/library/web/portlet/configuration/icon/EditWithImageEditorPortletConfigurationIcon.java
@@ -0,0 +1,131 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.portlet.configuration.icon;
+
+import com.liferay.document.library.web.constants.DLPortletKeys;
+import com.liferay.document.library.web.display.context.logic.FileEntryDisplayContextHelper;
+import com.liferay.document.library.web.portlet.action.ActionUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.configuration.icon.BaseJSPPortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIcon;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.util.PropsValues;
+import com.liferay.trash.kernel.util.TrashUtil;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Ambrin Chaudhary
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY_ADMIN,
+		"path=/document_library/view_file_entry"
+	},
+	service = PortletConfigurationIcon.class
+)
+public class EditWithImageEditorPortletConfigurationIcon
+	extends BaseJSPPortletConfigurationIcon {
+
+	@Override
+	public String getJspPath() {
+		return "/document_library/configuration/icon/edit_image_editor.jsp";
+	}
+
+	@Override
+	public String getMessage(PortletRequest portletRequest) {
+		return LanguageUtil.get(
+			getResourceBundle(getLocale(portletRequest)),
+			"edit-with-image-editor");
+	}
+
+	public String getURL(
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
+		return "javascript:;";
+	}
+
+	@Override
+	public double getWeight() {
+		return 106;
+	}
+
+	@Override
+	public boolean isShow(PortletRequest portletRequest) {
+		try {
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)portletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+			FileEntry fileEntry = ActionUtil.getFileEntry(portletRequest);
+
+			FileEntryDisplayContextHelper fileEntryDisplayContextHelper =
+				new FileEntryDisplayContextHelper(
+					themeDisplay.getPermissionChecker(), fileEntry);
+
+			if (fileEntryDisplayContextHelper.isEditActionAvailable() &&
+				ArrayUtil.contains(
+					PropsValues.DL_FILE_ENTRY_PREVIEW_IMAGE_MIME_TYPES,
+					fileEntry.getMimeType())) {
+
+				return true;
+			}
+
+			return false;
+		}
+		catch (Exception pe) {
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean isToolTip() {
+		return false;
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.document.library.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+	protected boolean isTrashEnabled(long groupId) {
+		try {
+			if (TrashUtil.isTrashEnabled(groupId)) {
+				return true;
+			}
+		}
+		catch (PortalException pe) {
+		}
+
+		return false;
+	}
+
+}
