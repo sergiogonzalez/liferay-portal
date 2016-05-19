2695ec302ffd2259e399d6afce3b6ed2feb701ee
Adolfo Pérez <adolfo.perez@liferay.com>
2016-05-19 18:02:28 +0200

    LPS-65946 Refactor into base class

--- modules/apps/collaboration/social/social-private-messaging-web/src/main/java/com/liferay/social/privatemessaging/web/upgrade/PrivateMessagingWebUpgradeChecker.java
+++ modules/apps/collaboration/social/social-private-messaging-web/src/main/java/com/liferay/social/privatemessaging/web/upgrade/PrivateMessagingWebUpgradeChecker.java
@@ -14,77 +14,30 @@
 
 package com.liferay.social.privatemessaging.web.upgrade;
 
-import com.liferay.portal.kernel.dao.orm.Criterion;
-import com.liferay.portal.kernel.dao.orm.DynamicQuery;
-import com.liferay.portal.kernel.dao.orm.ProjectionFactoryUtil;
-import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.model.Release;
-import com.liferay.portal.kernel.service.PortletPreferencesLocalService;
-import com.liferay.portal.kernel.service.ReleaseLocalService;
+import com.liferay.portal.upgrade.util.BaseWebUpgradeChecker;
+import com.liferay.social.privatemessaging.constants.PrivateMessagingPortletKeys;
 
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Adolfo Pérez
  */
 @Component(immediate = true, service = Object.class)
-public class PrivateMessagingWebUpgradeChecker {
+public class PrivateMessagingWebUpgradeChecker extends BaseWebUpgradeChecker {
 
-	@Activate
-	public void activate() throws PortalException {
-		Release release = _releaseLocalService.fetchRelease(
-			"com.liferay.private.messaging.web");
-
-		if (release == null) {
-			return;
-		}
-
-		long oldPortletIdCount = _getPortletCount(
-			"1_WAR_privatemessagingportlet");
-
-		long newPortletIdCount = _getPortletCount(
-			"com_liferay_social_privatemessaging_web_portlet_" +
-				"PrivateMessagingPortlet");
-
-		if ((oldPortletIdCount > 0) || (newPortletIdCount == 0)) {
-			release.setSchemaVersion("0.0.1");
-
-			_releaseLocalService.updateRelease(release);
-		}
+	@Override
+	protected String getBundleSymbolicName() {
+		return "com.liferay.private.messaging.web";
 	}
 
-	@Reference(unbind = "-")
-	protected void setPortletPreferencesLocalService(
-		PortletPreferencesLocalService portletPreferencesLocalService) {
-
-		_portletPreferencesLocalService = portletPreferencesLocalService;
+	@Override
+	protected String[] getNewPortletIds() {
+		return new String[] {PrivateMessagingPortletKeys.PRIVATE_MESSAGING};
 	}
 
-	@Reference(unbind = "-")
-	protected void setReleaseLocalService(
-		ReleaseLocalService releaseLocalService) {
-
-		_releaseLocalService = releaseLocalService;
+	@Override
+	protected String[] getOldPortletIds() {
+		return new String[] {"1_WAR_privatemessagingportlet"};
 	}
-
-	private long _getPortletCount(String... portletIds) {
-		DynamicQuery dynamicQuery =
-			_portletPreferencesLocalService.dynamicQuery();
-
-		Criterion criterion = RestrictionsFactoryUtil.in(
-			"portletId", portletIds);
-
-		dynamicQuery.add(criterion);
-
-		dynamicQuery.setProjection(ProjectionFactoryUtil.count("portletId"));
-
-		return _portletPreferencesLocalService.dynamicQueryCount(dynamicQuery);
-	}
-
-	private PortletPreferencesLocalService _portletPreferencesLocalService;
-	private ReleaseLocalService _releaseLocalService;
 
 }
