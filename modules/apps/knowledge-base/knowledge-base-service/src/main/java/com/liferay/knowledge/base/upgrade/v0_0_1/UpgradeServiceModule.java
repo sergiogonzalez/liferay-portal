85a69e2e05a3c484b4ee2e24aca6bf1dcbd1d412
Roberto Díaz <roberto.diaz@liferay.com>
2016-05-23 15:11:52 +0200

    LPS-65859 Add missing upgrade processeses.

---
+++ modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/upgrade/v0_0_1/UpgradeServiceModule.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.knowledge.base.upgrade.v0_0_1;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.service.ReleaseLocalService;
+import com.liferay.portal.kernel.upgrade.UpgradeException;
+import com.liferay.portal.upgrade.v7_0_0.UpgradeModules;
+
+import java.io.IOException;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Roberto Díaz
+ */
+@Component(immediate = true, service = UpgradeServiceModule.class)
+public class UpgradeServiceModule {
+
+	@Activate
+	protected void activate() {
+		UpgradeModules upgradeModules = new UpgradeModules() {
+
+			@Override
+			public String[] getBundleSymbolicNames() {
+				return new String[0];
+			}
+
+			@Override
+			public String[][] getConvertedLegacyModules() {
+				return new String[][] {
+					{
+						"knowledge-base-portlet",
+						"com.liferay.knowledge.base.service", "KB"
+					}
+				};
+			}
+
+			@Override
+			protected void updateConvertedLegacyModules()
+				throws IOException, SQLException {
+
+				try (PreparedStatement ps = connection.prepareStatement(
+						"select servletContextName from Release_ where " +
+							"servletContextName = ?")) {
+
+					String newServletContextName =
+						getConvertedLegacyModules()[0][1];
+
+					ps.setString(1, newServletContextName);
+
+					try (ResultSet rs = ps.executeQuery()) {
+						if (!rs.next()) {
+							super.updateConvertedLegacyModules();
+						}
+					}
+				}
+			}
+
+		};
+
+		try {
+			upgradeModules.upgrade();
+		}
+		catch (UpgradeException ue) {
+			_log.error("Module could not be upgraded", ue);
+		}
+	}
+
+	@Reference(unbind = "-")
+	protected void setReleaseLocalService(
+		ReleaseLocalService releaseLocalService) {
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		UpgradeServiceModule.class);
+
+}
---
+++ modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/upgrade/v0_0_1/UpgradeServiceRelease.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.knowledge.base.upgrade.v0_0_1;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeException;
+import com.liferay.portal.kernel.util.LoggingTimer;
+import com.liferay.portal.upgrade.v7_0_0.UpgradeRelease;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Roberto Díaz
+ */
+@Component(immediate = true, service = UpgradeServiceRelease.class)
+public class UpgradeServiceRelease {
+
+	@Activate
+	protected void activate() {
+		UpgradeRelease upgradeRelease = new UpgradeRelease() {
+
+			@Override
+			protected void upgradeSchemaVersion() throws Exception {
+				try (LoggingTimer loggingTimer = new LoggingTimer();
+					PreparedStatement ps = connection.prepareStatement(
+						"select distinct buildNumber from Release_ " +
+							"where schemaVersion is null");
+
+					ResultSet rs = ps.executeQuery()) {
+
+					while (rs.next()) {
+						String buildNumber = rs.getString("buildNumber");
+
+						String schemaVersion = toSchemaVersion(buildNumber);
+
+						runSQL(
+							"update Release_ set schemaVersion = '" +
+								schemaVersion + "' where buildNumber = " +
+								buildNumber + " and schemaVersion is null and" +
+								" servletContextName = '" +
+								_SERVLET_CONTEXT_NAME + "'");
+					}
+				}
+			}
+
+		};
+
+		try {
+			upgradeRelease.upgrade();
+		}
+		catch (UpgradeException ue) {
+			_log.error("Module could not be upgraded", ue);
+		}
+	}
+
+	@Reference(unbind = "-")
+	protected void setUpgradeServiceModule(
+		UpgradeServiceModule upgradeServiceModule) {
+	}
+
+	private static final String _SERVLET_CONTEXT_NAME =
+		"com.liferay.knowledge.base.service";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		UpgradeServiceRelease.class);
+
+}
