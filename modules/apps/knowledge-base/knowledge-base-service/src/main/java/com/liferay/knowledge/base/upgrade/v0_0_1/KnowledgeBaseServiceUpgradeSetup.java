b8e572e475884d5a54eac9a091daa1326eaa506d
Roberto Díaz <roberto.diaz@liferay.com>
2016-05-27 12:41:50 +0200

    LPS-65859 Different approach to make it clearer (Service bundle)

--- modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/upgrade/v0_0_1/KnowledgeBaseServiceUpgradeSetup.java
+++ modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/upgrade/v0_0_1/KnowledgeBaseServiceUpgradeSetup.java
@@ -14,37 +14,75 @@
 
 package com.liferay.knowledge.base.upgrade.v0_0_1;
 
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeException;
 import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.LoggingTimer;
+import com.liferay.portal.upgrade.v7_0_0.UpgradeModules;
 import com.liferay.portal.upgrade.v7_0_0.UpgradeRelease;
+
+import java.io.IOException;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
+import java.sql.SQLException;
 
 /**
  * @author Roberto Díaz
  */
-@Component(immediate = true, service = UpgradeServiceRelease.class)
-public class UpgradeServiceRelease {
+public class KnowledgeBaseServiceUpgradeSetup {
 
-	@Activate
-	protected void activate() throws UpgradeException {
+	public void upgrade() throws UpgradeException {
+		UpgradeModules upgradeModules = new UpgradeModules() {
+
+			@Override
+			public String[] getBundleSymbolicNames() {
+				return new String[0];
+			}
+
+			@Override
+			public String[][] getConvertedLegacyModules() {
+				return new String[][] {
+					{
+						"knowledge-base-portlet",
+						"com.liferay.knowledge.base.service", "KB"
+					}
+				};
+			}
+
+			@Override
+			protected void updateConvertedLegacyModules()
+				throws IOException, SQLException {
+
+				try (PreparedStatement ps = connection.prepareStatement(
+						"select servletContextName from Release_ where " +
+							"servletContextName = ?")) {
+
+					String newServletContextName =
+						getConvertedLegacyModules()[0][1];
+
+					ps.setString(1, newServletContextName);
+
+					try (ResultSet rs = ps.executeQuery()) {
+						if (!rs.next()) {
+							super.updateConvertedLegacyModules();
+						}
+					}
+				}
+			}
+
+		};
+
+		upgradeModules.upgrade();
+
 		UpgradeRelease upgradeRelease = new UpgradeRelease() {
 
 			@Override
 			protected void upgradeSchemaVersion() throws Exception {
 				try (LoggingTimer loggingTimer = new LoggingTimer();
 					PreparedStatement ps = connection.prepareStatement(
-						"select distinct buildNumber from Release_ " +
-							"where schemaVersion is null and " +
-								"servletContextName = ?")) {
+						"select distinct buildNumber from Release_ where " +
+							"schemaVersion is null and servletContextName = " +
+								"?")) {
 
 					ps.setString(1, _SERVLET_CONTEXT_NAME);
 
@@ -76,15 +114,7 @@ public class UpgradeServiceRelease {
 		upgradeRelease.upgrade();
 	}
 
-	@Reference(unbind = "-")
-	protected void setUpgradeServiceModule(
-		UpgradeServiceModule upgradeServiceModule) {
-	}
-
 	private static final String _SERVLET_CONTEXT_NAME =
 		"com.liferay.knowledge.base.service";
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		UpgradeServiceRelease.class);
 
 }
