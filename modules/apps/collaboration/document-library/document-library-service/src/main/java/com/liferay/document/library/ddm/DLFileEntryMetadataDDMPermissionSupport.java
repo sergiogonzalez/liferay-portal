a5c13ce414607cd51f6af21afc4c6e5bba3fef8f
Adolfo Pérez <adolfo.perez@liferay.com>
2016-05-31 14:36:37 +0200

    LPS-66213 Publish component under the expected service

--- modules/apps/collaboration/document-library/document-library-service/src/main/java/com/liferay/document/library/ddm/DLFileEntryMetadataDDMPermissionSupport.java
+++ modules/apps/collaboration/document-library/document-library-service/src/main/java/com/liferay/document/library/ddm/DLFileEntryMetadataDDMPermissionSupport.java
@@ -26,7 +26,8 @@ import org.osgi.service.component.annotations.Component;
 	immediate = true,
 	property = {
 		"model.class.name=com.liferay.document.library.kernel.model.DLFileEntryMetadata"
-	}
+	},
+	service = DLFileEntryMetadataDDMPermissionSupport.class
 )
 public class DLFileEntryMetadataDDMPermissionSupport
 	implements DDMStructurePermissionSupport {
