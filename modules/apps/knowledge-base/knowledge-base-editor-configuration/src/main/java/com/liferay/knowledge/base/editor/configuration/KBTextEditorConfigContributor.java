28702fb4c872e66cfc99b9c20b611b3265eb6f0c
Roberto Díaz <roberto.diaz@liferay.com>
2016-06-14 13:41:43 +0200

    LPS-66605 Create a new bundle that will contain the editor config contributors and move the existing ones

--- modules/apps/knowledge-base/knowledge-base-editor-configuration/src/main/java/com/liferay/knowledge/base/editor/configuration/KBTextEditorConfigContributor.java
+++ modules/apps/knowledge-base/knowledge-base-editor-configuration/src/main/java/com/liferay/knowledge/base/editor/configuration/KBTextEditorConfigContributor.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.knowledge.base.web.editor.configuration;
+package com.liferay.knowledge.base.editor.configuration;
 
 import com.liferay.knowledge.base.constants.KBPortletKeys;
 import com.liferay.portal.kernel.editor.configuration.BaseEditorConfigContributor;
