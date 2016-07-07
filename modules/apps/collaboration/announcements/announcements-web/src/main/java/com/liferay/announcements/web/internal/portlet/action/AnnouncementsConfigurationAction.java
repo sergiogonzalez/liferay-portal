fbfeb6601e4332e311c39ca59ac9438b9ed56a89
Roberto Díaz <roberto.diaz@liferay.com>
2016-07-07 16:24:11 +0200

    LPS-67019 Consistent package naming

--- modules/apps/collaboration/announcements/announcements-web/src/main/java/com/liferay/announcements/web/internal/portlet/action/AnnouncementsConfigurationAction.java
+++ modules/apps/collaboration/announcements/announcements-web/src/main/java/com/liferay/announcements/web/internal/portlet/action/AnnouncementsConfigurationAction.java
@@ -15,13 +15,18 @@
 package com.liferay.announcements.web.internal.portlet.action;
 
 import com.liferay.announcements.web.constants.AnnouncementsPortletKeys;
+import com.liferay.portal.kernel.portlet.BaseJSPSettingsConfigurationAction;
 import com.liferay.portal.kernel.portlet.ConfigurationAction;
-import com.liferay.portal.kernel.portlet.DefaultConfigurationAction;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Adolfo Pérez
+ * @author Roberto Díaz
  */
 @Component(
 	immediate = true,
@@ -29,5 +34,20 @@ import org.osgi.service.component.annotations.Component;
 	service = ConfigurationAction.class
 )
 public class AnnouncementsConfigurationAction
-	extends DefaultConfigurationAction {
+	extends BaseJSPSettingsConfigurationAction {
+
+	@Override
+	public String getJspPath(HttpServletRequest httpServletRequest) {
+		return "/announcements/configuration.jsp";
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.announcements.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
 }
--- modules/apps/collaboration/announcements/announcements-web/src/main/java/com/liferay/announcements/web/internal/portlet/action/ViewMVCRenderCommand.java
+++ modules/apps/collaboration/announcements/announcements-web/src/main/java/com/liferay/announcements/web/internal/portlet/action/ViewMVCRenderCommand.java
@@ -40,7 +40,7 @@ public class ViewMVCRenderCommand implements MVCRenderCommand {
 	public String render(
 		RenderRequest renderRequest, RenderResponse renderResponse) {
 
-		return "/view.jsp";
+		return "/announcements/view.jsp";
 	}
 
 }
