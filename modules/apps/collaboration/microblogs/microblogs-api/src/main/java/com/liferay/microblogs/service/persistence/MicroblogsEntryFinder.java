36a5d3f282c19339219eabfc3b2872672341b5d0
Adolfo Pérez <adolfo.perez@liferay.com>
2016-07-13 16:41:43 +0200

    LPS-66986 Regen build-service

--- modules/apps/collaboration/microblogs/microblogs-api/src/main/java/com/liferay/microblogs/service/persistence/MicroblogsEntryFinder.java
+++ modules/apps/collaboration/microblogs/microblogs-api/src/main/java/com/liferay/microblogs/service/persistence/MicroblogsEntryFinder.java
@@ -24,11 +24,19 @@ import aQute.bnd.annotation.ProviderType;
 public interface MicroblogsEntryFinder {
 	public int countByUserId(long userId);
 
+	public int countByC_U(long companyId, long userId);
+
 	public int countByU_MU(long userId, long microblogsEntryUserId);
 
 	public int countByU_ATN(long userId, java.lang.String assetTagName);
 
+	public int countByC_U_ATN(long companyId, long userId,
+		java.lang.String assetTagName);
+
 	public int countByCCNI_ATN(long creatorClassNameId,
+		java.lang.String assetTagName);
+
+	public int countByC_CCNI_ATN(long companyId, long creatorClassNameId,
 		java.lang.String assetTagName);
 
 	public int countByU_T_MU(long userId, int type, long microblogsEntryUserId);
@@ -36,8 +44,14 @@ public interface MicroblogsEntryFinder {
 	public int countByCCNI_CCPK_ATN(long creatorClassNameId,
 		long creatorClassPK, java.lang.String assetTagName, boolean andOperator);
 
+	public int countByC_CCNI_CCPK_ATN(long companyId, long creatorClassNameId,
+		long creatorClassPK, java.lang.String assetTagName, boolean andOperator);
+
 	public java.util.List<com.liferay.microblogs.model.MicroblogsEntry> findByUserId(
 		long userId, int start, int end);
+
+	public java.util.List<com.liferay.microblogs.model.MicroblogsEntry> findByC_U(
+		long companyId, long userId, int start, int end);
 
 	public java.util.List<com.liferay.microblogs.model.MicroblogsEntry> findByU_MU(
 		long userId, long microblogsEntryUserId, int start, int end);
@@ -45,14 +59,26 @@ public interface MicroblogsEntryFinder {
 	public java.util.List<com.liferay.microblogs.model.MicroblogsEntry> findByU_ATN(
 		long userId, java.lang.String assetTagName, int start, int end);
 
+	public java.util.List<com.liferay.microblogs.model.MicroblogsEntry> findByC_U_ATN(
+		long companyId, long userId, java.lang.String assetTagName, int start,
+		int end);
+
 	public java.util.List<com.liferay.microblogs.model.MicroblogsEntry> findByCCNI_ATN(
 		long creatorClassNameId, java.lang.String assetTagName, int start,
 		int end);
+
+	public java.util.List<com.liferay.microblogs.model.MicroblogsEntry> findByC_CCNI_ATN(
+		long companyId, long creatorClassNameId, java.lang.String assetTagName,
+		int start, int end);
 
 	public java.util.List<com.liferay.microblogs.model.MicroblogsEntry> findByU_T_MU(
 		long userId, int type, long microblogsEntryUserId, int start, int end);
 
 	public java.util.List<com.liferay.microblogs.model.MicroblogsEntry> findByCCNI_CCPK_ATN(
 		long creatorClassNameId, long creatorClassPK,
+		java.lang.String assetTagName, boolean andOperator, int start, int end);
+
+	public java.util.List<com.liferay.microblogs.model.MicroblogsEntry> findByC_CCNI_CCPK_ATN(
+		long companyId, long creatorClassNameId, long creatorClassPK,
 		java.lang.String assetTagName, boolean andOperator, int start, int end);
 }
