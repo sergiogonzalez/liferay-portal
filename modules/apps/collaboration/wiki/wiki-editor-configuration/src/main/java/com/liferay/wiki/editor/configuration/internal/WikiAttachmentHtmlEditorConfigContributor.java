11f6b760c9666e53316e1f955e145b6a5d6b63bc
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2016-09-13 12:43:06 +0200

    LPS-67901 Configure Item Selector in each editor

--- modules/apps/collaboration/wiki/wiki-editor-configuration/src/main/java/com/liferay/wiki/editor/configuration/internal/WikiAttachmentHtmlEditorConfigContributor.java
+++ modules/apps/collaboration/wiki/wiki-editor-configuration/src/main/java/com/liferay/wiki/editor/configuration/internal/WikiAttachmentHtmlEditorConfigContributor.java
@@ -14,15 +14,25 @@
 
 package com.liferay.wiki.editor.configuration.internal;
 
+import com.liferay.item.selector.ItemSelector;
+import com.liferay.item.selector.ItemSelectorCriterion;
+import com.liferay.item.selector.ItemSelectorReturnType;
+import com.liferay.item.selector.criteria.FileEntryItemSelectorReturnType;
 import com.liferay.portal.kernel.editor.configuration.EditorConfigContributor;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.wiki.constants.WikiPortletKeys;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import javax.portlet.PortletURL;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Ambrin Chaudhary
@@ -49,6 +59,78 @@ public class WikiAttachmentHtmlEditorConfigContributor
 		super.populateConfigJSONObject(
 			jsonObject, inputEditorTaglibAttributes, themeDisplay,
 			requestBackedPortletURLFactory);
+
+		Map<String, String> fileBrowserParamsMap =
+			(Map<String, String>)inputEditorTaglibAttributes.get(
+				"liferay-ui:input-editor:fileBrowserParams");
+
+		long wikiPageResourcePrimKey = 0;
+
+		if (fileBrowserParamsMap != null) {
+			wikiPageResourcePrimKey = GetterUtil.getLong(
+				fileBrowserParamsMap.get("wikiPageResourcePrimKey"));
+		}
+
+		String name = GetterUtil.getString(
+			inputEditorTaglibAttributes.get("liferay-ui:input-editor:name"));
+
+		boolean inlineEdit = GetterUtil.getBoolean(
+			inputEditorTaglibAttributes.get(
+				"liferay-ui:input-editor:inlineEdit"));
+
+		if (!inlineEdit) {
+			String namespace = GetterUtil.getString(
+				inputEditorTaglibAttributes.get(
+					"liferay-ui:input-editor:namespace"));
+
+			name = namespace + name;
+		}
+
+		List<ItemSelectorReturnType> desiredItemSelectorReturnTypes =
+			new ArrayList<>();
+
+		desiredItemSelectorReturnTypes.add(
+			new FileEntryItemSelectorReturnType());
+
+		ItemSelectorCriterion imageItemSelectorCriterion =
+			getImageItemSelectorCriterion(desiredItemSelectorReturnTypes);
+
+		ItemSelectorCriterion urlItemSelectorCriterion =
+			getURLItemSelectorCriterion();
+
+		PortletURL itemSelectorURL = null;
+
+		if (wikiPageResourcePrimKey == 0) {
+			itemSelectorURL = _itemSelector.getItemSelectorURL(
+				requestBackedPortletURLFactory, name + "selectItem",
+				imageItemSelectorCriterion, urlItemSelectorCriterion);
+		}
+		else {
+			ItemSelectorCriterion attachmentItemSelectorCriterion =
+				getWikiAttachmentItemSelectorCriterion(
+					wikiPageResourcePrimKey, desiredItemSelectorReturnTypes);
+
+			ItemSelectorCriterion uploadItemSelectorCriterion =
+				getUploadItemSelectorCriterion(
+					wikiPageResourcePrimKey, themeDisplay,
+					requestBackedPortletURLFactory);
+
+			itemSelectorURL = _itemSelector.getItemSelectorURL(
+				requestBackedPortletURLFactory, name + "selectItem",
+				attachmentItemSelectorCriterion, imageItemSelectorCriterion,
+				urlItemSelectorCriterion, uploadItemSelectorCriterion);
+		}
+
+		jsonObject.put(
+			"filebrowserImageBrowseLinkUrl", itemSelectorURL.toString());
+		jsonObject.put("filebrowserImageBrowseUrl", itemSelectorURL.toString());
 	}
+
+	@Reference(unbind = "-")
+	public void setItemSelector(ItemSelector itemSelector) {
+		_itemSelector = itemSelector;
+	}
+
+	private ItemSelector _itemSelector;
 
 }
