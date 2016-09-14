99101c7e3fe48a059c007b7197bc2f6d0f7dfd04
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2016-09-14 09:42:20 +0200

    LPS-67901 Add CKEditor

--- modules/apps/collaboration/wiki/wiki-editor-configuration/src/main/java/com/liferay/wiki/editor/configuration/internal/WikiAttachmentCreoleEditorConfigContributor.java
+++ modules/apps/collaboration/wiki/wiki-editor-configuration/src/main/java/com/liferay/wiki/editor/configuration/internal/WikiAttachmentCreoleEditorConfigContributor.java
@@ -40,6 +40,7 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	property = {
 		"editor.config.key=contentEditor", "editor.name=alloyeditor_creole",
+		"editor.name=ckeditor_creole",
 		"javax.portlet.name=" + WikiPortletKeys.WIKI,
 		"javax.portlet.name=" + WikiPortletKeys.WIKI_ADMIN,
 		"javax.portlet.name=" + WikiPortletKeys.WIKI_DISPLAY,
