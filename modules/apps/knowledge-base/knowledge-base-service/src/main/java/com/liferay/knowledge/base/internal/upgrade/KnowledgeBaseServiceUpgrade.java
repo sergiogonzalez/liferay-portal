608829e492f0a34635f5ed69f3b748030f7ab9b2
Adolfo Pérez <adolfo.perez@liferay.com>
2016-10-26 19:09:00 +0200

    LPS-68555 Make upgrade executes from 1.3.1 onwards
    The `status` column was added to `KBComment` in version 1.3.1. Make sure
    it is executed in all of the intermediate steps.

--- modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/internal/upgrade/KnowledgeBaseServiceUpgrade.java
+++ modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/internal/upgrade/KnowledgeBaseServiceUpgrade.java
@@ -99,17 +99,23 @@ public class KnowledgeBaseServiceUpgrade implements UpgradeStepRegistrator {
 			new com.liferay.knowledge.base.internal.upgrade.v1_3_1.
 				UpgradeKBArticle(),
 			new com.liferay.knowledge.base.internal.upgrade.v1_3_1.
+				UpgradeKBComment(),
+			new com.liferay.knowledge.base.internal.upgrade.v2_0_0.
 				UpgradeKBComment());
 
 		registry.register(
 			"com.liferay.knowledge.base.service", "1.3.1", "1.3.2",
 			new com.liferay.knowledge.base.internal.upgrade.v1_3_2.
 				UpgradeKBArticle(),
+			new com.liferay.knowledge.base.internal.upgrade.v2_0_0.
+				UpgradeKBComment(),
 			new com.liferay.knowledge.base.internal.upgrade.v1_3_2.
 				UpgradeKBFolder());
 
 		registry.register(
 			"com.liferay.knowledge.base.service", "1.3.2", "1.3.3",
+			new com.liferay.knowledge.base.internal.upgrade.v2_0_0.
+				UpgradeKBComment(),
 			new com.liferay.knowledge.base.internal.upgrade.v1_3_3.
 				UpgradeKBFolder());
 
@@ -119,6 +125,8 @@ public class KnowledgeBaseServiceUpgrade implements UpgradeStepRegistrator {
 				UpgradeKBArticle(),
 			new com.liferay.knowledge.base.internal.upgrade.v1_3_4.
 				UpgradeKBComment(),
+			new com.liferay.knowledge.base.internal.upgrade.v2_0_0.
+				UpgradeKBComment(),
 			new com.liferay.knowledge.base.internal.upgrade.v1_3_4.
 				UpgradePortletPreferences(),
 			new com.liferay.knowledge.base.internal.upgrade.v1_3_4.
@@ -126,6 +134,8 @@ public class KnowledgeBaseServiceUpgrade implements UpgradeStepRegistrator {
 
 		registry.register(
 			"com.liferay.knowledge.base.service", "1.3.4", "1.3.5",
+			new com.liferay.knowledge.base.internal.upgrade.v2_0_0.
+				UpgradeKBComment(),
 			new com.liferay.knowledge.base.internal.upgrade.v1_3_5.
 				UpgradeLastPublishDate());
 
