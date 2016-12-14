ea6f5588295910708ea2e5817b44cadc3f9e2db3
Alejandro Tardín <alejandro.tardin@liferay.com>
2016-12-14 17:05:54 +0100

    LPS-67993 Creating action to resubscribe from the session

---
+++ modules/apps/my-subscriptions/my-subscriptions-web/src/main/java/com/liferay/my/subscriptions/web/internal/portlet/action/ResubscribeAction.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.my.subscriptions.web.internal.portlet.action;
+
+import com.liferay.my.subscriptions.web.internal.constants.MySubscriptionsPortletKeys;
+import com.liferay.portal.kernel.model.Subscription;
+import com.liferay.portal.kernel.model.Ticket;
+import com.liferay.portal.kernel.portlet.LiferayPortletURL;
+import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.service.SubscriptionLocalService;
+import com.liferay.portal.kernel.service.TicketLocalService;
+import com.liferay.portal.kernel.struts.BaseStrutsAction;
+import com.liferay.portal.kernel.struts.StrutsAction;
+import com.liferay.portal.kernel.util.ParamUtil;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import javax.portlet.PortletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(
+	immediate = true, property = {"path=/portal/resubscribe"},
+	service = StrutsAction.class
+)
+public class ResubscribeAction extends BaseStrutsAction {
+
+	@Override
+	public String execute(
+			HttpServletRequest request, HttpServletResponse response)
+		throws Exception {
+
+		Subscription subscription =
+			(Subscription) request.getSession().getAttribute(
+				MySubscriptionsPortletKeys.LAST_UNSUBSCRIBED_SUBSCRIPTION_KEY);
+
+		if (subscription == null) {
+			throw new Exception("There is no subscription in the session");
+		}
+
+		_subscriptionLocalService.addSubscription(subscription.getUserId(),
+			subscription.getGroupId(), subscription.getClassName(),
+			subscription.getClassPK(), subscription.getFrequency());
+
+		request.getSession().removeAttribute(
+			MySubscriptionsPortletKeys.LAST_UNSUBSCRIBED_SUBSCRIPTION_KEY);
+
+		LiferayPortletURL liferayPortletURL = PortletURLFactoryUtil.create(
+			request, MySubscriptionsPortletKeys.MY_SUBSCRIPTIONS,
+			PortletRequest.RENDER_PHASE);
+
+		response.sendRedirect(liferayPortletURL.toString());
+
+		return null;
+	}
+
+	@Reference
+	private SubscriptionLocalService _subscriptionLocalService;
+}
