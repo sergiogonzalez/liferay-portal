5d43a02d5346e7b3eefede0b640aeb0172392e89
Adolfo Pérez <adolfo.perez@liferay.com>
2017-02-06 14:03:16 +0100

    LPS-70471 Add Comment capability to CMIS repositories

--- modules/apps/collaboration/document-library/document-library-repository-cmis-impl/src/main/java/com/liferay/document/library/repository/cmis/internal/BaseCMISRepositoryDefiner.java
+++ modules/apps/collaboration/document-library/document-library-repository-cmis-impl/src/main/java/com/liferay/document/library/repository/cmis/internal/BaseCMISRepositoryDefiner.java
@@ -16,6 +16,7 @@ package com.liferay.document.library.repository.cmis.internal;
 
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.repository.DocumentRepository;
+import com.liferay.portal.kernel.repository.capabilities.CommentCapability;
 import com.liferay.portal.kernel.repository.capabilities.PortalCapabilityLocator;
 import com.liferay.portal.kernel.repository.capabilities.ProcessorCapability;
 import com.liferay.portal.kernel.repository.registry.BaseRepositoryDefiner;
@@ -54,6 +55,9 @@ public abstract class BaseCMISRepositoryDefiner extends BaseRepositoryDefiner {
 		PortalCapabilityLocator portalCapabilityLocator =
 			getPortalCapabilityLocator();
 
+		capabilityRegistry.addExportedCapability(
+			CommentCapability.class,
+			portalCapabilityLocator.getCommentCapability(documentRepository));
 		capabilityRegistry.addSupportedCapability(
 			ProcessorCapability.class,
 			portalCapabilityLocator.getProcessorCapability(documentRepository));
