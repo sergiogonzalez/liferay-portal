7fe59eb365c6ca41592b2f03e261dfe9e23e9569
Alejandro Hernández <info@alejandrohdezma.com>
2017-02-07 14:39:28 +0100

    LPS-70474 Add API methods

---
+++ modules/apps/collaboration/comment/comment-demo-data-creator-api/src/main/java/com/liferay/comment/demo/data/creator/CommentDemoDataCreator.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.comment.demo.data.creator;
+
+import com.liferay.portal.kernel.comment.Comment;
+import com.liferay.portal.kernel.exception.PortalException;
+
+/**
+ * @author Alejandro Hernández
+ */
+public interface CommentDemoDataCreator {
+
+	public Comment create(long userId, long parentCommentId)
+		throws PortalException;
+
+	public Comment create(long userId, String className, long classPK)
+		throws PortalException;
+
+	public void delete() throws PortalException;
+
+}
