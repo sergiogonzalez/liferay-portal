a12c08e20161878062e690592249365d2527f462
Mauro Mariuzzo <mauro.mariuzzo@smc.it>
2017-02-21 20:58:18 +0100

    Extended DLDisplayContext framework to manage the new element DLViewFileHistoryDisplayContext. This element is used to build the file entry history action menu and allows third party bundle to extend it

---
+++ modules/apps/collaboration/document-library/document-library-api/src/main/java/com/liferay/document/library/display/context/DLViewFileHistoryDisplayContext.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.display.context;
+
+import java.util.List;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.servlet.taglib.ui.Menu;
+import com.liferay.portal.kernel.servlet.taglib.ui.ToolbarItem;
+
+/**
+ * @author Mauro Mariuzzo
+ */
+public interface DLViewFileHistoryDisplayContext extends DLDisplayContext {
+
+	public Menu getMenu() throws PortalException;
+
+	public List<ToolbarItem> getToolbarItems() throws PortalException;
+
+}
