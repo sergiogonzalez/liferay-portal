4f6875b9623ca2567ea83849ac934c5dbea0c9bf
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2017-03-08 14:56:50 +0100

    Simplify the class

--- modules/apps/collaboration/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/display/context/DefaultDLViewFileHistoryDisplayContext.java
+++ modules/apps/collaboration/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/display/context/DefaultDLViewFileHistoryDisplayContext.java
@@ -14,18 +14,11 @@
 
 package com.liferay.document.library.web.internal.display.context;
 
-import com.liferay.document.library.display.context.DLMimeTypeDisplayContext;
 import com.liferay.document.library.display.context.DLViewFileHistoryDisplayContext;
 import com.liferay.document.library.web.internal.display.context.logic.DLPortletInstanceSettingsHelper;
-import com.liferay.document.library.web.internal.display.context.logic.FileEntryDisplayContextHelper;
-import com.liferay.document.library.web.internal.display.context.logic.FileVersionDisplayContextHelper;
 import com.liferay.document.library.web.internal.display.context.logic.UIItemsBuilder;
 import com.liferay.document.library.web.internal.display.context.util.DLRequestHelper;
-import com.liferay.dynamic.data.mapping.storage.StorageEngine;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.repository.model.FileEntry;
-import com.liferay.portal.kernel.repository.model.FileShortcut;
 import com.liferay.portal.kernel.repository.model.FileVersion;
 import com.liferay.portal.kernel.servlet.taglib.ui.Menu;
 import com.liferay.portal.kernel.servlet.taglib.ui.MenuItem;
@@ -37,7 +30,6 @@ import java.util.ResourceBundle;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author Mauro Mariuzzo
@@ -46,26 +38,18 @@ public class DefaultDLViewFileHistoryDisplayContext
 	implements DLViewFileHistoryDisplayContext {
 
 	public DefaultDLViewFileHistoryDisplayContext(
-			HttpServletRequest request, HttpServletResponse response,
-			FileShortcut fileShortcut,
-			DLMimeTypeDisplayContext dlMimeTypeDisplayContext,
-			ResourceBundle resourceBundle, StorageEngine storageEngine)
-		throws PortalException {
+		HttpServletRequest request, FileVersion fileVersion,
+		ResourceBundle resourceBundle) {
 
-		this(
-			request, fileShortcut.getFileVersion(), fileShortcut,
-			dlMimeTypeDisplayContext, resourceBundle, storageEngine);
-	}
+		_resourceBundle = resourceBundle;
 
-	public DefaultDLViewFileHistoryDisplayContext(
-		HttpServletRequest request, HttpServletResponse response,
-		FileVersion fileVersion,
-		DLMimeTypeDisplayContext dlMimeTypeDisplayContext,
-		ResourceBundle resourceBundle, StorageEngine storageEngine) {
+		DLRequestHelper dlRequestHelper = new DLRequestHelper(request);
 
-		this(
-			request, fileVersion, null, dlMimeTypeDisplayContext,
-			resourceBundle, storageEngine);
+		_dlPortletInstanceSettingsHelper = new DLPortletInstanceSettingsHelper(
+			dlRequestHelper);
+
+		_uiItemsBuilder = new UIItemsBuilder(
+			request, fileVersion, _resourceBundle);
 	}
 
 	@Override
@@ -93,57 +77,6 @@ public class DefaultDLViewFileHistoryDisplayContext
 		return _UUID;
 	}
 
-	private DefaultDLViewFileHistoryDisplayContext(
-		HttpServletRequest request, FileVersion fileVersion,
-		FileShortcut fileShortcut,
-		DLMimeTypeDisplayContext dlMimeTypeDisplayContext,
-		ResourceBundle resourceBundle, StorageEngine storageEngine) {
-
-		try {
-			_fileVersion = fileVersion;
-			_dlMimeTypeDisplayContext = dlMimeTypeDisplayContext;
-			_resourceBundle = resourceBundle;
-			_storageEngine = storageEngine;
-
-			DLRequestHelper dlRequestHelper = new DLRequestHelper(request);
-
-			_dlPortletInstanceSettingsHelper =
-				new DLPortletInstanceSettingsHelper(dlRequestHelper);
-
-			_fileEntryDisplayContextHelper = new FileEntryDisplayContextHelper(
-				dlRequestHelper.getPermissionChecker(),
-				_getFileEntry(fileVersion));
-
-			_fileVersionDisplayContextHelper =
-				new FileVersionDisplayContextHelper(fileVersion);
-
-			if (fileShortcut == null) {
-				_uiItemsBuilder = new UIItemsBuilder(
-					request, fileVersion, _resourceBundle);
-			}
-			else {
-				_uiItemsBuilder = new UIItemsBuilder(
-					request, fileShortcut, _resourceBundle);
-			}
-		}
-		catch (PortalException pe) {
-			throw new SystemException(
-				"Unable to build DefaultDLViewFileVersionDisplayContext for " +
-					fileVersion,
-				pe);
-		}
-	}
-
-	private FileEntry _getFileEntry(FileVersion fileVersion)
-		throws PortalException {
-
-		if (fileVersion != null) {
-			return fileVersion.getFileEntry();
-		}
-
-		return null;
-	}
-
 	private List<MenuItem> _getMenuItems() throws PortalException {
 		List<MenuItem> menuItems = new ArrayList<>();
 
@@ -165,27 +98,9 @@ public class DefaultDLViewFileHistoryDisplayContext
 	private static final UUID _UUID = UUID.fromString(
 		"8f4f3c55-3e93-41c5-a363-57d00161f274");
 
-	@SuppressWarnings("unused")
-	private final DLMimeTypeDisplayContext _dlMimeTypeDisplayContext;
-
 	private final DLPortletInstanceSettingsHelper
 		_dlPortletInstanceSettingsHelper;
-
-	@SuppressWarnings("unused")
-	private final FileEntryDisplayContextHelper _fileEntryDisplayContextHelper;
-
-	@SuppressWarnings("unused")
-	private final FileVersion _fileVersion;
-
-	@SuppressWarnings("unused")
-	private final FileVersionDisplayContextHelper
-		_fileVersionDisplayContextHelper;
-
 	private final ResourceBundle _resourceBundle;
-
-	@SuppressWarnings("unused")
-	private final StorageEngine _storageEngine;
-
 	private final UIItemsBuilder _uiItemsBuilder;
 
 }
