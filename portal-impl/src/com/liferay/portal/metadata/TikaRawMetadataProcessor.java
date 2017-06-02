1b23b73fbab63c299484cd5cea96b94ecf0e34e4
Adolfo Pérez <adolfo.perez@liferay.com>
2017-06-02 14:38:37 +0200

    LPS-72303 Don't add hint if the extension is empty

--- portal-impl/src/com/liferay/portal/metadata/TikaRawMetadataProcessor.java
+++ portal-impl/src/com/liferay/portal/metadata/TikaRawMetadataProcessor.java
@@ -27,6 +27,7 @@ import com.liferay.portal.kernel.process.ProcessExecutorUtil;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.util.PropsValues;
 
 import java.io.File;
@@ -118,9 +119,11 @@ public class TikaRawMetadataProcessor extends XugglerRawMetadataProcessor {
 			metadata = new Metadata();
 		}
 
-		metadata.set(
-			Metadata.RESOURCE_NAME_KEY,
-			file.getName() + StringPool.PERIOD + extension);
+		if (Validator.isNotNull(extension)) {
+			metadata.set(
+				Metadata.RESOURCE_NAME_KEY,
+				file.getName() + StringPool.PERIOD + extension);
+		}
 
 		boolean forkProcess = false;
 
