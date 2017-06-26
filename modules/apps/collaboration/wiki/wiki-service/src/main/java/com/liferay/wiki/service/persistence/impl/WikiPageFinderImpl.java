e777093abb76d90858df93d4e34247d5b3c0ba40
Roberto Díaz <robertodimen@gmail.com>
2017-06-26 14:54:38 +0200

    LPS-71640 Query does not contain this string so nothing will be replaced. Remove unused.

--- modules/apps/collaboration/wiki/wiki-service/src/main/java/com/liferay/wiki/service/persistence/impl/WikiPageFinderImpl.java
+++ modules/apps/collaboration/wiki/wiki-service/src/main/java/com/liferay/wiki/service/persistence/impl/WikiPageFinderImpl.java
@@ -206,19 +206,14 @@ public class WikiPageFinderImpl
 
 			String sql = CustomSQLUtil.get(getClass(), FIND_BY_NO_ASSETS);
 
-			long classNameId = ClassNameLocalServiceUtil.getClassNameId(
-				WikiPage.class);
-
-			sql = StringUtil.replace(
-				sql,
-				"[$CLASS_NAME_ID_COM.LIFERAY.PORTLET.WIKI.MODEL.WIKIPAGE$]",
-				String.valueOf(classNameId));
-
 			SQLQuery q = session.createSynchronizedSQLQuery(sql);
 
 			q.addEntity("WikiPage", WikiPageImpl.class);
 
 			QueryPos qPos = QueryPos.getInstance(q);
+
+			long classNameId = ClassNameLocalServiceUtil.getClassNameId(
+				WikiPage.class);
 
 			qPos.add(classNameId);
 
