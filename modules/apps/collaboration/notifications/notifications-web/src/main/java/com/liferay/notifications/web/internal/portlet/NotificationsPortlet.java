e04d60daf8ad9567e694a9d37b7551bb38c25b9f
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2017-07-20 13:32:36 +0200

    LPS-73691 SF

--- modules/apps/collaboration/notifications/notifications-web/src/main/java/com/liferay/notifications/web/internal/portlet/NotificationsPortlet.java
+++ modules/apps/collaboration/notifications/notifications-web/src/main/java/com/liferay/notifications/web/internal/portlet/NotificationsPortlet.java
@@ -108,11 +108,10 @@ public class NotificationsPortlet extends MVCPortlet {
 					UserNotificationDeliveryConstants.TYPE_WEBSITE, false,
 					false);
 
-		for (UserNotificationEvent notification : userNotificationEvents) {
-			notification.setArchived(true);
+		for (UserNotificationEvent userNotificationEvent :
+				userNotificationEvents) {
 
-			_userNotificationEventLocalService.updateUserNotificationEvent(
-				notification);
+			updateArchived(userNotificationEvent);
 		}
 
 		ResourceBundle resourceBundle =
@@ -277,6 +276,12 @@ public class NotificationsPortlet extends MVCPortlet {
 		if (userNotificationEvent == null) {
 			return;
 		}
+
+		updateArchived(userNotificationEvent);
+	}
+
+	protected void updateArchived(UserNotificationEvent userNotificationEvent)
+		throws Exception {
 
 		userNotificationEvent.setArchived(true);
 
