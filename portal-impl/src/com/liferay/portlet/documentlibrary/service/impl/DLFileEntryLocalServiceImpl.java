0bbdf9590224b69fe65b2db3adf60d90726e5bdd
Adolfo Pérez <adolfo.perez@liferay.com>
2017-08-21 16:52:02 +0200

    LPS-73684 Add methods to delete group/repo files and shortcuts

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
@@ -957,6 +957,60 @@ public class DLFileEntryLocalServiceImpl
 	}
 
 	@Override
+	public void deleteGroupFileEntries(long groupId) throws PortalException {
+		final RepositoryEventTrigger repositoryEventTrigger =
+			getFolderRepositoryEventTrigger(
+				groupId, DLFolderConstants.DEFAULT_PARENT_FOLDER_ID);
+
+		ActionableDynamicQuery actionableDynamicQuery =
+			dlFileEntryLocalService.getActionableDynamicQuery();
+
+		actionableDynamicQuery.setGroupId(groupId);
+		actionableDynamicQuery.setPerformActionMethod(
+			(DLFileEntry dlFileEntry) -> {
+				repositoryEventTrigger.trigger(
+					RepositoryEventType.Delete.class, FileEntry.class,
+					new LiferayFileEntry(dlFileEntry));
+
+				dlFileEntryLocalService.deleteFileEntry(dlFileEntry);
+			});
+
+		actionableDynamicQuery.performActions();
+	}
+
+	@Override
+	public void deleteRepositoryFileEntries(long repositoryId)
+		throws PortalException {
+
+		final RepositoryEventTrigger repositoryEventTrigger =
+			RepositoryUtil.getRepositoryEventTrigger(repositoryId);
+
+		int total = dlFileEntryPersistence.countByRepositoryId(repositoryId);
+
+		final IntervalActionProcessor<Void> intervalActionProcessor =
+			new IntervalActionProcessor<>(total);
+
+		intervalActionProcessor.setPerformIntervalActionMethod(
+			(start, end) -> {
+				List<DLFileEntry> dlFileEntries =
+					dlFileEntryPersistence.findByRepositoryId(
+						repositoryId, start, end);
+
+				for (DLFileEntry dlFileEntry : dlFileEntries) {
+					repositoryEventTrigger.trigger(
+						RepositoryEventType.Delete.class, FileEntry.class,
+						new LiferayFileEntry(dlFileEntry));
+
+					dlFileEntryLocalService.deleteFileEntry(dlFileEntry);
+				}
+
+				return null;
+			});
+
+		intervalActionProcessor.performIntervalActions();
+	}
+
+	@Override
 	public void deleteRepositoryFileEntries(long repositoryId, long folderId)
 		throws PortalException {
 
