8538fb3dbe29f30814fe4ed97ec07936c26d478d
Alejandro Tardín <alejandro.tardin@liferay.com>
2017-11-13 15:49:17 +0100

    LPS-74943 Autogenerated

--- modules/apps/collaboration/reading-time/reading-time-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/apps/collaboration/reading-time/reading-time-service/src/main/resources/META-INF/sql/tables.sql
@@ -1,10 +1,11 @@
 create table ReadingTimeEntry (
 	uuid_ VARCHAR(75) null,
 	entryId LONG not null primary key,
+	groupId LONG,
 	companyId LONG,
 	createDate DATE null,
 	modifiedDate DATE null,
 	classNameId LONG,
 	classPK LONG,
-	readingTimeInSeconds LONG
+	readingTime LONG
 );
