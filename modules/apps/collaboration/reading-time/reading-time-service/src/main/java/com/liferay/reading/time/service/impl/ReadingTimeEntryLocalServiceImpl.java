42b6787d2f80ea04dd69a21d6fb1e4e8d92e6cce
Alejandro Tardín <alejandro.tardin@liferay.com>
2017-11-14 17:02:08 +0100

    LPS-74943 Adding service methods to update and delete

--- modules/apps/collaboration/reading-time/reading-time-service/src/main/java/com/liferay/reading/time/service/impl/ReadingTimeEntryLocalServiceImpl.java
+++ modules/apps/collaboration/reading-time/reading-time-service/src/main/java/com/liferay/reading/time/service/impl/ReadingTimeEntryLocalServiceImpl.java
@@ -14,14 +14,29 @@
 
 package com.liferay.reading.time.service.impl;
 
+import com.liferay.portal.kernel.model.GroupedModel;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.spring.extender.service.ServiceReference;
 import com.liferay.reading.time.model.ReadingTimeEntry;
 import com.liferay.reading.time.service.base.ReadingTimeEntryLocalServiceBaseImpl;
+
+import java.time.Duration;
 
 /**
  * @author Alejandro Tardín
  */
 public class ReadingTimeEntryLocalServiceImpl
 	extends ReadingTimeEntryLocalServiceBaseImpl {
+
+	@Override
+	public ReadingTimeEntry addReadingTimeEntry(
+		GroupedModel model, Duration readingTime) {
+
+		return addReadingTimeEntry(
+			model.getGroupId(),
+			_classNameLocalService.getClassNameId(model.getModelClass()),
+			(Long)model.getPrimaryKeyObj(), readingTime.toMillis());
+	}
 
 	@Override
 	public ReadingTimeEntry addReadingTimeEntry(
@@ -40,11 +55,49 @@ public class ReadingTimeEntryLocalServiceImpl
 	}
 
 	@Override
+	public ReadingTimeEntry deleteReadingTimeEntry(GroupedModel model) {
+		ReadingTimeEntry readingTimeEntry = fetchReadingTimeEntry(model);
+
+		if (readingTimeEntry != null) {
+			return deleteReadingTimeEntry(readingTimeEntry);
+		}
+
+		return null;
+	}
+
+	@Override
+	public ReadingTimeEntry fetchReadingTimeEntry(GroupedModel model) {
+		return fetchReadingTimeEntry(
+			model.getGroupId(),
+			_classNameLocalService.getClassNameId(model.getModelClass()),
+			(Long)model.getPrimaryKeyObj());
+	}
+
+	@Override
 	public ReadingTimeEntry fetchReadingTimeEntry(
 		long groupId, long classNameId, long classPK) {
 
 		return readingTimeEntryPersistence.fetchByG_C_C(
 			groupId, classNameId, classPK);
 	}
+
+	@Override
+	public ReadingTimeEntry updateReadingTimeEntry(
+		GroupedModel model, Duration readingTime) {
+
+		ReadingTimeEntry readingTimeEntry = fetchReadingTimeEntry(model);
+
+		if (readingTimeEntry == null) {
+			return addReadingTimeEntry(model, readingTime);
+		}
+		else {
+			readingTimeEntry.setReadingTime(readingTime.toMillis());
+
+			return updateReadingTimeEntry(readingTimeEntry);
+		}
+	}
+
+	@ServiceReference(type = ClassNameLocalService.class)
+	private ClassNameLocalService _classNameLocalService;
 
 }
