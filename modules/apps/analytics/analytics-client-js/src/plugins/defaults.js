7f5086a0869dccd8f13c6fbc18a1c1bf47e3911b
Bruno Basto <bruno.basto@liferay.com>
2018-05-11 17:26:03 -0300

    LPS-80825 Adds tracking of Documents to Analytics Client

--- modules/apps/analytics/analytics-client-js/src/plugins/defaults.js
+++ modules/apps/analytics/analytics-client-js/src/plugins/defaults.js
@@ -1,8 +1,9 @@
 import blogs from './blogs';
+import documents from './documents';
 import forms from './forms';
 import resolution from './resolution';
 import scrolling from './scrolling';
 import timing from './timing';
 
-export {blogs, forms, resolution, scrolling, timing};
-export default [blogs, forms, resolution, scrolling, timing];
+export {blogs, documents, forms, resolution, scrolling, timing};
+export default [blogs, documents, forms, resolution, scrolling, timing];
