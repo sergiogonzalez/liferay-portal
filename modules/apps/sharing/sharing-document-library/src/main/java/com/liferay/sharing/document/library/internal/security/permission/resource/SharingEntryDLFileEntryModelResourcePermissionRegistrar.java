32ea8225faa52bb55c3df26507b27976ed8a4f09
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2018-07-17 12:23:34 +0200

    merge     LPS-83368 Apply sharing permission to DL permission check on file entries

--- modules/apps/sharing/sharing-document-library/src/main/java/com/liferay/sharing/document/library/internal/security/permission/resource/SharingEntryDLFileEntryModelResourcePermissionRegistrar.java
+++ modules/apps/sharing/sharing-document-library/src/main/java/com/liferay/sharing/document/library/internal/security/permission/resource/SharingEntryDLFileEntryModelResourcePermissionRegistrar.java
@@ -15,20 +15,23 @@
 package com.liferay.sharing.document.library.internal.security.permission.resource;
 
 import com.liferay.document.library.kernel.model.DLFileEntry;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.document.library.kernel.service.DLFileEntryLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
 import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionLogic;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portlet.documentlibrary.constants.DLConstants;
 import com.liferay.sharing.constants.SharingEntryActionKey;
 import com.liferay.sharing.service.SharingEntryLocalService;
 
-import java.lang.reflect.Field;
-
-import java.util.Iterator;
-import java.util.List;
+import java.util.Dictionary;
 
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
@@ -42,12 +45,65 @@ public class SharingEntryDLFileEntryModelResourcePermissionRegistrar {
 
 	@Activate
 	public void activate(BundleContext bundleContext) {
-		_modelResourcePermissionLogic =
-			(permissionChecker, name, dlFileEntry, actionId) -> {
-				if (!SharingEntryActionKey.isSupportedActionId(actionId)) {
-					return null;
-				}
+		Dictionary<String, Object> properties = new HashMapDictionary<>();
 
+		properties.put("component.name", _COMPONENT_NAME);
+		properties.put("model.class.name", DLFileEntry.class.getName());
+		properties.put("service.ranking", 100);
+
+		_serviceRegistration = bundleContext.registerService(
+			ModelResourcePermission.class,
+			ModelResourcePermissionFactory.create(
+				DLFileEntry.class, DLFileEntry::getFileEntryId,
+				_dlFileEntryLocalService::getDLFileEntry,
+				_portletResourcePermission,
+				(modelResourcePermission, consumer) -> {
+					consumer.accept(
+						new SharingDLFileEntryModelPermissionLogic(
+							_dlFileEntryModelResourcePermission));
+				}),
+			properties);
+	}
+
+	@Deactivate
+	public void deactivate() {
+		_serviceRegistration.unregister();
+	}
+
+	private static final String _COMPONENT_NAME =
+		"com.liferay.sharing.document.library.internal.security.permission." +
+			"resource.SharingEntryDLFileEntryModelResourcePermission";
+
+	@Reference
+	private ClassNameLocalService _classNameLocalService;
+
+	@Reference
+	private DLFileEntryLocalService _dlFileEntryLocalService;
+
+	@Reference(
+		target = "(&(model.class.name=com.liferay.document.library.kernel.model.DLFileEntry)(!(component.name=" + _COMPONENT_NAME + ")))"
+	)
+	private ModelResourcePermission<DLFileEntry>
+		_dlFileEntryModelResourcePermission;
+
+	@Reference(target = "(resource.name=" + DLConstants.RESOURCE_NAME + ")")
+	private PortletResourcePermission _portletResourcePermission;
+
+	private ServiceRegistration<ModelResourcePermission> _serviceRegistration;
+
+	@Reference
+	private SharingEntryLocalService _sharingEntryLocalService;
+
+	private class SharingDLFileEntryModelPermissionLogic
+		implements ModelResourcePermissionLogic<DLFileEntry> {
+
+		@Override
+		public Boolean contains(
+				PermissionChecker permissionChecker, String name,
+				DLFileEntry dlFileEntry, String actionId)
+			throws PortalException {
+
+			if (SharingEntryActionKey.isSupportedActionId(actionId)) {
 				SharingEntryActionKey sharingEntryActionKey =
 					SharingEntryActionKey.parseFromActionId(actionId);
 
@@ -59,81 +115,23 @@ public class SharingEntryDLFileEntryModelResourcePermissionRegistrar {
 
 					return true;
 				}
-
-				return null;
-			};
-
-		Class<? extends ModelResourcePermission> clazz =
-			_modelResourcePermission.getClass();
-
-		Field field = null;
-
-		try {
-			field = clazz.getDeclaredField("_modelResourcePermissionLogics");
-
-			field.setAccessible(true);
-
-			List<ModelResourcePermissionLogic> modelResourcePermissionLogics =
-				(List<ModelResourcePermissionLogic>)field.get(
-					_modelResourcePermission);
-
-			modelResourcePermissionLogics.add(0, _modelResourcePermissionLogic);
-		}
-		catch (IllegalAccessException | NoSuchFieldException e) {
-			_log.error(
-				"Unable to include sharing permission check for DLFileEntry");
-		}
-	}
-
-	@Deactivate
-	protected void deactivate() {
-		Class<? extends ModelResourcePermission> clazz =
-			_modelResourcePermission.getClass();
-
-		Field field = null;
-
-		try {
-			field = clazz.getDeclaredField("_modelResourcePermissionLogics");
-
-			field.setAccessible(true);
-
-			List<ModelResourcePermissionLogic> modelResourcePermissionLogics =
-				(List<ModelResourcePermissionLogic>)field.get(
-					_modelResourcePermission);
-
-			Iterator<ModelResourcePermissionLogic> it =
-				modelResourcePermissionLogics.iterator();
-
-			while (it.hasNext()) {
-				if (it.next() == _modelResourcePermissionLogic) {
-					it.remove();
-				}
 			}
-		}
-		catch (IllegalAccessException | NoSuchFieldException e) {
-			_log.error(
-				"Unable to remove sharing permission check for DLFileEntry");
+
+			return _dlFileEntryModelResourcePermission.contains(
+				permissionChecker, dlFileEntry, actionId);
 		}
 
-		_modelResourcePermissionLogic = null;
+		private SharingDLFileEntryModelPermissionLogic(
+			ModelResourcePermission<DLFileEntry>
+				dlFileEntryModelResourcePermission) {
+
+			_dlFileEntryModelResourcePermission =
+				dlFileEntryModelResourcePermission;
+		}
+
+		private final ModelResourcePermission<DLFileEntry>
+			_dlFileEntryModelResourcePermission;
+
 	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		SharingEntryDLFileEntryModelResourcePermissionRegistrar.class);
-
-	@Reference
-	private ClassNameLocalService _classNameLocalService;
-
-	@Reference(
-		target = "(model.class.name=com.liferay.document.library.kernel.model.DLFileEntry)",
-		unbind = "-"
-	)
-	private ModelResourcePermission<DLFileEntry> _modelResourcePermission;
-
-	private ModelResourcePermissionLogic<DLFileEntry>
-		_modelResourcePermissionLogic;
-
-	@Reference
-	private SharingEntryLocalService _sharingEntryLocalService;
 
 }
