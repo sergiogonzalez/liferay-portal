eda4ba0b1c19f43dd7460d2cdeb7a1c30fde1ae3
Adolfo Pérez <adolfo.perez@liferay.com>
2018-09-27 09:30:05 +0200

    LPS-85584 SF

--- portal-impl/test/unit/com/liferay/portal/jsonwebservice/JSONWebServiceTest.java
+++ portal-impl/test/unit/com/liferay/portal/jsonwebservice/JSONWebServiceTest.java
@@ -99,28 +99,6 @@ public class JSONWebServiceTest extends BaseJSONWebServiceTestCase {
 	}
 
 	@Test
-	public void testEnumReturnValue() throws Exception {
-		MockHttpServletRequest mockHttpServletRequest = createHttpRequest(
-			"/camelfoo/get-isolation");
-
-		JSONWebServiceAction jsonWebServiceAction = lookupJSONWebServiceAction(
-			mockHttpServletRequest);
-
-		Assert.assertEquals("DEFAULT", jsonWebServiceAction.invoke());
-	}
-
-	@Test
-	public void testEnumParameter() throws Exception {
-		MockHttpServletRequest mockHttpServletRequest = createHttpRequest(
-			"/camelfoo/add-isolation/isolation/DEFAULT");
-
-		JSONWebServiceAction jsonWebServiceAction = lookupJSONWebServiceAction(
-			mockHttpServletRequest);
-
-		jsonWebServiceAction.invoke();
-	}
-
-	@Test
 	public void testArgumentsMatching() throws Exception {
 		MockHttpServletRequest mockHttpServletRequest = createHttpRequest(
 			"/foo/hello-world");
@@ -293,6 +271,28 @@ public class JSONWebServiceTest extends BaseJSONWebServiceTestCase {
 
 		Assert.assertEquals(
 			ServiceContext.class.getName(), jsonWebServiceAction.invoke());
+	}
+
+	@Test
+	public void testEnumParameter() throws Exception {
+		MockHttpServletRequest mockHttpServletRequest = createHttpRequest(
+			"/camelfoo/add-isolation/isolation/DEFAULT");
+
+		JSONWebServiceAction jsonWebServiceAction = lookupJSONWebServiceAction(
+			mockHttpServletRequest);
+
+		jsonWebServiceAction.invoke();
+	}
+
+	@Test
+	public void testEnumReturnValue() throws Exception {
+		MockHttpServletRequest mockHttpServletRequest = createHttpRequest(
+			"/camelfoo/get-isolation");
+
+		JSONWebServiceAction jsonWebServiceAction = lookupJSONWebServiceAction(
+			mockHttpServletRequest);
+
+		Assert.assertEquals("DEFAULT", jsonWebServiceAction.invoke());
 	}
 
 	@Test
