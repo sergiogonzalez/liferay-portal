1021aeb6b45d4396c7ca9baec6f2692e684b9598
Adolfo Pérez <adolfo.perez@liferay.com>
2018-10-11 12:01:02 +0200

    LPS-86341 Create interface for versioning strategies

---
+++ portal-impl/src/com/liferay/portlet/documentlibrary/versioning/DefaultVersioningStrategy.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.documentlibrary.versioning;
+
+import com.liferay.document.library.kernel.model.DLFileVersion;
+import com.liferay.document.library.kernel.model.DLVersionNumberIncrease;
+import com.liferay.document.library.kernel.versioning.VersioningStrategy;
+import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
+
+import org.osgi.framework.Constants;
+
+/**
+ * @author Adolfo Pérez
+ */
+@OSGiBeanProperties(
+	property = Constants.SERVICE_RANKING + "=" + Integer.MIN_VALUE
+)
+public class DefaultVersioningStrategy implements VersioningStrategy {
+
+	@Override
+	public DLVersionNumberIncrease computeDLVersionNumberIncrease(
+		DLFileVersion previousDLFileVersion, DLFileVersion nextDLFileVersion) {
+
+		return DLVersionNumberIncrease.NONE;
+	}
+
+	@Override
+	public boolean isOverridable() {
+		return true;
+	}
+
+}
